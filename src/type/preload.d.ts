f8eb60ce655d45057e6acd7ab6a22a20a1b0d825
Riku Kanayama <68765710+k-kuroguro@users.noreply.github.com>
2021-08-05 19:28:00 +0900

    テキストファイルからセリフをインポートする (#10)
    * import from text file
    
    * Apply suggestions from code review
    
    Co-authored-by: Hiroshiba <kazuyuki_hiroshiba@dwango.co.jp>
    
    * change dialog title
    
    * run lint
    
    * run lint
    
    Co-authored-by: Hiroshiba <kazuyuki_hiroshiba@dwango.co.jp>

--- src/type/preload.d.ts
+++ src/type/preload.d.ts
@@ -3,8 +3,12 @@ export interface Sandbox {
   getOssLicenses(): Promise<Record<string, string>[]>;
   saveTempAudioFile(obj: { relativePath: string; buffer: ArrayBuffer }): void;
   loadTempFile(): Promise<string>;
-  showSaveDialog(obj: { title: string, defaultPath?: string }): Promise<string | undefined>;
+  showSaveDialog(obj: {
+    title: string;
+    defaultPath?: string;
+  }): Promise<string | undefined>;
   showOpenDirectoryDialog(obj: { title: string }): Promise<string | undefined>;
+  showImportFileDialog(obj: { title: string }): Promise<string | undefined>;
   writeFile(obj: { filePath: string; buffer: ArrayBuffer });
   readFile(obj: { filePath: string }): Promise<ArrayBuffer>;
   createHelpWindow(): void;
