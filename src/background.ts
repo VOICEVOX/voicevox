928b32e3901108900ae008ba678e5c1a7ab230f0
k-kuroguro <riku_2002_1128@i.softbank.jp>
2021-08-09 23:07:48 +0900

    add default menu when development

--- src/background.ts
+++ src/background.ts
@@ -127,6 +127,7 @@ const updateInfos = JSON.parse(
 
 // initialize menu
 const menu = MenuBuilder()
+  .configure(isDevelopment)
   .setOnLaunchModeItemClicked((useGpu) => {
     store.set("useGpu", useGpu);
 
