159e19cd4d86a9e4a4022cdfbc3736de03bbcacf
yamachu <yamachu.dev@gmail.com>
2021-08-09 13:30:10 +0900

    d.tsをskipLibCheckをfalseにしてもエラーにならない様に書き換える

--- src/type/shims-vue.d.ts
+++ src/type/shims-vue.d.ts
@@ -1,18 +1,12 @@
 /* eslint-disable */
 declare module "*.vue" {
+  // @ts-ignore
   import type { DefineComponent } from "vue";
   const component: DefineComponent<{}, {}, any>;
-  export default component;
 
-  declare global {
-    interface Window {
-      readonly electron: import("./preload").Sandbox;
-    }
-  }
+  export default component;
 }
 
-declare global {
-  interface Window {
-    readonly electron: import("./preload").Sandbox;
-  }
+interface Window {
+  readonly electron: import("./preload").Sandbox;
 }
