657458ef3dae16ad9a521732a394f22df450727f
k-kuroguro <riku_2002_1128@i.softbank.jp>
2021-08-15 22:22:52 +0900

    show error when load invalid project

--- src/store/index.ts
+++ src/store/index.ts
@@ -73,6 +73,10 @@ export const store = createStore<State>({
           const text = new TextDecoder("utf-8").decode(buf).trim();
           const obj = JSON.parse(text);
           if (!(await projectValidationCheck(obj, true))) {
+            window.electron.showErrorDialog({
+              title: "エラー",
+              message: "ファイルフォーマットが正しくありません。",
+            });
             return;
           }
           const projectData = obj as ProjectData;
