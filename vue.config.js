e4e2be25168f97d728cbe761e6e36333b19c44e6
HyodaKazuaki <kazuaki@ex-t.jp>
2021-08-17 12:11:00 +0900

    move splitResources.js to build dir

--- vue.config.js
+++ vue.config.js
@@ -29,7 +29,7 @@ module.exports = {
         copyright: "Hiroshiba Kazuyuki",
         compression: "maximum",
         extends: null,
-        afterAllArtifactBuild: path.resolve(__dirname, "splitResources.js"),
+        afterAllArtifactBuild: path.resolve(__dirname, "build", "splitResources.js"),
         win: {
           icon: "public/icon.png",
           target: [
