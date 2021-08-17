7ce10678eee6d21e02ace1d5c7693e6ee415b2b0
MT224244 <mt224244@gmail.com>
2021-08-17 23:44:25 +0900

    利用規約をMarkdownファイルを読んで表示するように

--- src/components/Policy.vue
+++ src/components/Policy.vue
@@ -8,45 +8,108 @@
       </q-toolbar>
     </q-header>
     <q-page class="relarive-absolute-wrapper scroller">
-      <div class="q-pa-md">
-        <section>
-          <p>ダミー利用規約</p>
-        </section>
-        <section>
-          <h3>禁止事項</h3>
-          <ul>
-            <li>ダミー禁止事項</li>
-          </ul>
-        </section>
-        <section>
-          <h3>免責事項</h3>
-          <p>ダミー免責事項</p>
-        </section>
-        <section>
-          <h3>その他</h3>
-          <p>ダミーその他</p>
-        </section>
-        <section>
-          <h3>謝辞</h3>
-          <p>ダミー謝辞</p>
-        </section>
-      </div>
+      <div class="q-pa-md markdown" v-html="policy"></div>
     </q-page>
   </div>
 </template>
 
-<script lang="ts"></script>
+<script lang="ts">
+import { defineComponent, onMounted, ref } from "vue";
+import { useStore, GET_POLICY_TEXT } from "@/store";
+import MarkdownIt from "markdown-it";
+
+export default defineComponent({
+  setup() {
+    const store = useStore();
+    const policy = ref("");
+
+    const md = new MarkdownIt();
+
+    // 全てのリンクに_blankを付ける
+    // https://github.com/markdown-it/markdown-it/blob/master/docs/architecture.md#renderer
+    const defaultRender =
+      md.renderer.rules.link_open ||
+      function (tokens, idx, options, env, self) {
+        return self.renderToken(tokens, idx, options);
+      };
+    md.renderer.rules.link_open = function (tokens, idx, options, env, self) {
+      const aIdx = tokens[idx].attrIndex("target");
+
+      if (aIdx < 0) {
+        tokens[idx].attrPush(["target", "_blank"]);
+      } else {
+        const attrs = tokens[idx].attrs;
+        if (attrs) attrs[aIdx][1] = "_blank";
+        tokens[idx].attrs = attrs;
+      }
+
+      return defaultRender(tokens, idx, options, env, self);
+    };
+
+    onMounted(async () => {
+      policy.value = md.render(await store.dispatch(GET_POLICY_TEXT));
+    });
+
+    return {
+      policy,
+    };
+  },
+});
+</script>
+
+<style lang="scss">
+.markdown {
+  // h1-h6のスタイルをデフォルトに戻す
+  // https://www.w3schools.com/tags/tag_hn.asp
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    display: block;
+    margin-left: 0;
+    margin-right: 0;
+    font-weight: bold;
+  }
+  h1 {
+    font-size: 2rem;
+    margin-top: 0.67rem;
+    margin-bottom: 0.67rem;
+  }
+  h2 {
+    font-size: 1.5rem;
+    margin-top: 0.83rem;
+    margin-bottom: 0.83rem;
+  }
+  h3 {
+    font-size: 1.17rem;
+    margin-top: 1rem;
+    margin-bottom: 1rem;
+  }
+  h4 {
+    font-size: 1rem;
+    margin-top: 1.33rem;
+    margin-bottom: 1.33rem;
+  }
+  h5 {
+    font-size: 0.83rem;
+    margin-top: 1.67rem;
+    margin-bottom: 1.67rem;
+  }
+  h6 {
+    font-size: 0.67rem;
+    margin-top: 2.33rem;
+    margin-bottom: 2.33rem;
+  }
+}
+</style>
 
 <style scoped lang="scss">
 .root {
   .scroller {
     width: 100%;
     overflow: auto;
-    h3 {
-      font-size: 1.3rem;
-      font-weight: bold;
-      margin: 0;
-    }
   }
 }
 </style>
