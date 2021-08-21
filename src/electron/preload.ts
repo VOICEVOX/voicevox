a79ff3d732c35dc7708f271adb2125ab2ab34cae
MT <mt224244@gmail.com>
2021-08-22 00:19:56 +0900

    ipcのラッパー関数を作成 (#131)

--- src/electron/preload.ts
+++ src/electron/preload.ts
@@ -1,77 +1,101 @@
-import { contextBridge, ipcRenderer } from "electron";
+import {
+  contextBridge,
+  ipcRenderer,
+  IpcRenderer,
+  IpcRendererEvent,
+} from "electron";
 import fs from "fs";
 import path from "path";
 
 import { Sandbox } from "@/type/preload";
 
+function ipcRendererInvoke<T extends keyof IpcIHData>(
+  channel: T,
+  ...args: IpcIHData[T]["args"]
+): Promise<IpcIHData[T]["return"]>;
+function ipcRendererInvoke(channel: string, ...args: unknown[]): unknown {
+  return ipcRenderer.invoke(channel, ...args);
+}
+
+function ipcRendererOn<T extends keyof IpcSOData>(
+  channel: T,
+  listener: (event: IpcRendererEvent, ...args: IpcSOData[T]["args"]) => void
+): IpcRenderer;
+function ipcRendererOn(
+  channel: string,
+  listener: (event: IpcRendererEvent, ...args: unknown[]) => void
+) {
+  return ipcRenderer.on(channel, listener);
+}
+
 let tempDir: string;
 
 const api: Sandbox = {
   getAppInfos: async () => {
-    return await ipcRenderer.invoke("GET_APP_INFOS");
+    return await ipcRendererInvoke("GET_APP_INFOS");
   },
 
   getCharacterInfos: async () => {
-    return await ipcRenderer.invoke("GET_CHARACTER_INFOS");
+    return await ipcRendererInvoke("GET_CHARACTER_INFOS");
   },
 
   getPolicyText: async () => {
-    return await ipcRenderer.invoke("GET_POLICY_TEXT");
+    return await ipcRendererInvoke("GET_POLICY_TEXT");
   },
 
   getOssLicenses: async () => {
-    return await ipcRenderer.invoke("GET_OSS_LICENSES");
+    return await ipcRendererInvoke("GET_OSS_LICENSES");
   },
 
   getUpdateInfos: async () => {
-    return await ipcRenderer.invoke("GET_UPDATE_INFOS");
+    return await ipcRendererInvoke("GET_UPDATE_INFOS");
   },
 
   saveTempAudioFile: async ({ relativePath, buffer }) => {
     if (!tempDir) {
-      tempDir = await ipcRenderer.invoke("GET_TEMP_DIR");
+      tempDir = await ipcRendererInvoke("GET_TEMP_DIR");
     }
     fs.writeFileSync(path.join(tempDir, relativePath), new DataView(buffer));
   },
 
   loadTempFile: async () => {
     if (!tempDir) {
-      tempDir = await ipcRenderer.invoke("GET_TEMP_DIR");
+      tempDir = await ipcRendererInvoke("GET_TEMP_DIR");
     }
     const buf = fs.readFileSync(path.join(tempDir, "hoge.txt"));
     return new TextDecoder().decode(buf);
   },
 
   showAudioSaveDialog: ({ title, defaultPath }) => {
-    return ipcRenderer.invoke("SHOW_AUDIO_SAVE_DIALOG", { title, defaultPath });
+    return ipcRendererInvoke("SHOW_AUDIO_SAVE_DIALOG", { title, defaultPath });
   },
 
   showOpenDirectoryDialog: ({ title }) => {
-    return ipcRenderer.invoke("SHOW_OPEN_DIRECTORY_DIALOG", { title });
+    return ipcRendererInvoke("SHOW_OPEN_DIRECTORY_DIALOG", { title });
   },
 
   showProjectSaveDialog: ({ title }) => {
-    return ipcRenderer.invoke("SHOW_PROJECT_SAVE_DIALOG", { title });
+    return ipcRendererInvoke("SHOW_PROJECT_SAVE_DIALOG", { title });
   },
 
   showProjectLoadDialog: ({ title }) => {
-    return ipcRenderer.invoke("SHOW_PROJECT_LOAD_DIALOG", { title });
+    return ipcRendererInvoke("SHOW_PROJECT_LOAD_DIALOG", { title });
   },
 
   showConfirmDialog: ({ title, message }) => {
-    return ipcRenderer.invoke("SHOW_CONFIRM_DIALOG", { title, message });
+    return ipcRendererInvoke("SHOW_CONFIRM_DIALOG", { title, message });
   },
 
   showWarningDialog: ({ title, message }) => {
-    return ipcRenderer.invoke("SHOW_WARNING_DIALOG", { title, message });
+    return ipcRendererInvoke("SHOW_WARNING_DIALOG", { title, message });
   },
 
   showErrorDialog: ({ title, message }) => {
-    return ipcRenderer.invoke("SHOW_ERROR_DIALOG", { title, message });
+    return ipcRendererInvoke("SHOW_ERROR_DIALOG", { title, message });
   },
 
   showImportFileDialog: ({ title }) => {
-    return ipcRenderer.invoke("SHOW_IMPORT_FILE_DIALOG", { title });
+    return ipcRendererInvoke("SHOW_IMPORT_FILE_DIALOG", { title });
   },
 
   writeFile: ({ filePath, buffer }) => {
@@ -83,23 +107,23 @@ const api: Sandbox = {
   },
 
   openTextEditContextMenu: () => {
-    return ipcRenderer.invoke("OPEN_TEXT_EDIT_CONTEXT_MENU");
+    return ipcRendererInvoke("OPEN_TEXT_EDIT_CONTEXT_MENU");
   },
 
   useGpu: (newValue) => {
-    return ipcRenderer.invoke("USE_GPU", { newValue });
+    return ipcRendererInvoke("USE_GPU", { newValue });
   },
 
   isAvailableGPUMode: () => {
-    return ipcRenderer.invoke("IS_AVAILABLE_GPU_MODE");
+    return ipcRendererInvoke("IS_AVAILABLE_GPU_MODE");
   },
 
   fileEncoding: (newValue) => {
-    return ipcRenderer.invoke("FILE_ENCODING", { newValue });
+    return ipcRendererInvoke("FILE_ENCODING", { newValue });
   },
 
   onReceivedIPCMsg: (channel, callback) => {
-    return ipcRenderer.on(channel, callback);
+    return ipcRendererOn(channel, callback);
   },
 
   closeWindow: () => {
