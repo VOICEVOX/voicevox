1951937ff19aff4b84f47737ef350a5d67961bce
Hiroshiba <hihokaruta@gmail.com>
2021-08-23 09:15:14 +0900

    to 0.4.0 (#134)
    * update version
    
    * update updateinfos
    
    * update openapi.json
    
    * update how-to-use-link

--- src/openapi/models/index.ts
+++ src/openapi/models/index.ts
@@ -2,4 +2,5 @@ export * from "./AccentPhrase";
 export * from "./AudioQuery";
 export * from "./HTTPValidationError";
 export * from "./Mora";
+export * from "./Speaker";
 export * from "./ValidationError";
