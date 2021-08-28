b92ec386dd9f45dd9f9741e235fd589f70f40de6
k-kuroguro <riku_2002_1128@i.softbank.jp>
2021-08-29 02:14:29 +0900

    delete needless variables

--- src/components/ErrorBoundary.vue
+++ src/components/ErrorBoundary.vue
@@ -1,22 +1,18 @@
 <template>
-  <slot></slot>
+  <slot />
 </template>
 
 <script lang="ts">
-import { Component, defineComponent } from "vue";
+import { defineComponent } from "vue";
 import { useStore, CAPTURE_ERROR } from "@/store";
 
 export default defineComponent({
   name: "ErrorBoundary",
   data(): {
     error: Error | null;
-    instance: Component | null;
-    info: string | null;
   } {
     return {
       error: null,
-      instance: null,
-      info: null,
     };
   },
   setup() {
@@ -26,24 +22,18 @@ export default defineComponent({
     };
     return { captureError };
   },
-  errorCaptured(error, instance, info) {
+  errorCaptured(error) {
     this.error = error instanceof Error ? error : null;
-    this.instance = instance;
-    this.info = info;
     if (this.error) this.captureError(this.error);
     return false;
   },
   mounted() {
     const handlePromiseRejectionEvent = (event: PromiseRejectionEvent) => {
       this.error = event.reason;
-      this.instance = null;
-      this.info = null;
       if (this.error) this.captureError(this.error);
     };
     window.addEventListener("error", (event: ErrorEvent) => {
       this.error = event.error;
-      this.instance = null;
-      this.info = null;
       if (this.error) this.captureError(this.error);
     });
     window.addEventListener("unhandledrejection", handlePromiseRejectionEvent);
