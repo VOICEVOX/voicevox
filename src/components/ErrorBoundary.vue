e0c5df345a2abaaf63674eab11b457637f50a54f
k-kuroguro <riku_2002_1128@i.softbank.jp>
2021-08-29 13:16:17 +0900

    use Composition API

--- src/components/ErrorBoundary.vue
+++ src/components/ErrorBoundary.vue
@@ -3,41 +3,35 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, onErrorCaptured, onMounted } from "vue";
 import { useStore, CAPTURE_ERROR } from "@/store";
 
 export default defineComponent({
   name: "ErrorBoundary",
-  data(): {
-    error: Error | null;
-  } {
-    return {
-      error: null,
-    };
-  },
   setup() {
     const store = useStore();
     const captureError = (error: Error): void => {
       store.dispatch(CAPTURE_ERROR, { error, stack: error.stack });
     };
-    return { captureError };
-  },
-  errorCaptured(error) {
-    this.error = error instanceof Error ? error : null;
-    if (this.error) this.captureError(this.error);
-    return false;
-  },
-  mounted() {
-    const handlePromiseRejectionEvent = (event: PromiseRejectionEvent) => {
-      this.error = event.reason;
-      if (this.error) this.captureError(this.error);
-    };
-    window.addEventListener("error", (event: ErrorEvent) => {
-      this.error = event.error;
-      if (this.error) this.captureError(this.error);
+
+    onMounted(() => {
+      const handlePromiseRejectionEvent = (event: PromiseRejectionEvent) => {
+        captureError(event.reason);
+      };
+      window.addEventListener("error", (event: ErrorEvent) => {
+        captureError(event.error);
+      });
+      window.addEventListener(
+        "unhandledrejection",
+        handlePromiseRejectionEvent
+      );
+      window.addEventListener("rejectionhandled", handlePromiseRejectionEvent);
     });
-    window.addEventListener("unhandledrejection", handlePromiseRejectionEvent);
-    window.addEventListener("rejectionhandled", handlePromiseRejectionEvent);
+
+    onErrorCaptured((error) => {
+      if (error instanceof Error) captureError(error);
+      return false;
+    });
   },
 });
 </script>
