062f7d4ddde607aea4c7c20e6e6cdf4aa4341374
MT224244 <mt224244@gmail.com>
2021-09-02 00:22:30 +0900

    音声ライブラリの利用規約をMarkdownファイルで書けるように

--- src/plugins/ipcMessageReceiverPlugin.ts
+++ src/plugins/ipcMessageReceiverPlugin.ts
@@ -1,11 +1,12 @@
 import { LOAD_PROJECT_FILE } from "@/store/project";
-import { App } from "vue";
+import { Plugin } from "vue";
 import { Store } from "vuex";
 import { DETECT_MAXIMIZED, DETECT_UNMAXIMIZED } from "@/store/ui";
 import { DETECTED_ENGINE_ERROR, START_WAITING_ENGINE } from "@/store/audio";
+import { State } from "@/store/type";
 
-export const ipcMessageReceiver = {
-  install: (_: App, options: { store: Store<unknown> }): void => {
+export const ipcMessageReceiver: Plugin = {
+  install: (_, options: { store: Store<State> }) => {
     window.electron.onReceivedIPCMsg(
       "LOAD_PROJECT_FILE",
       (_, { filePath, confirm } = {}) =>
