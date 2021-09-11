3de1fcac7263370cf0ec4fe27e5b80d0c532e832
k-kuroguro <riku_2002_1128@i.softbank.jp>
2021-09-11 23:23:45 +0900

    pyenv local -> global

--- scripts/gitpod_init.sh
+++ scripts/gitpod_init.sh
@@ -8,5 +8,5 @@ LATEST_TAG=$(curl -w "%{redirect_url}" -s -o /dev/null https://github.com/Hirosh
 git clone -b ${LATEST_TAG} --depth 1 https://github.com/Hiroshiba/voicevox_engine.git ./voicevox_engine
 
 pyenv install 3.7.9
-pyenv local 3.7.9
+pyenv global 3.7.9
 pip install -r ./voicevox_engine/requirements.txt
