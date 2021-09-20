35482b0ee7d537cdca404eae32bc025989756389
Hiroshiba Kazuyuki <hihokaruta@gmail.com>
2021-09-20 20:56:30 +0900

    update openapi

--- src/openapi/models/index.ts
+++ src/openapi/models/index.ts
@@ -2,5 +2,6 @@ export * from "./AccentPhrase";
 export * from "./AudioQuery";
 export * from "./HTTPValidationError";
 export * from "./Mora";
+export * from "./ParseKanaBadRequest";
 export * from "./Speaker";
 export * from "./ValidationError";
