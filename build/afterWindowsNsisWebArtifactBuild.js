d996d744b08e2eb7e12a775cd214ea475ced3e7a
aoirint <aoirint@gmail.com>
2021-09-24 06:03:34 +0900

    rename splitResources to splitNsisArchive

--- build/afterWindowsNsisWebArtifactBuild.js
+++ build/afterWindowsNsisWebArtifactBuild.js
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const splitResources = require("./splitResources").default;
+const splitNsisArchive = require("./splitNsisArchive").default;
 
 // target: electron-builder.Target
 exports.default = async function (target) {
-  splitResources(target);
+  splitNsisArchive(target);
 };
