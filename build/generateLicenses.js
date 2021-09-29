052d100855c4bbdf81527576ae58b1d05a7b2955
aoirint <aoirint@gmail.com>
2021-09-29 12:53:04 +0900

    license: use packageName if name is undefined

--- build/generateLicenses.js
+++ build/generateLicenses.js
@@ -38,7 +38,7 @@ const licenseJson = execSync(
 const checkerLicenses = JSON.parse(licenseJson);
 
 const licenses = Object.entries(checkerLicenses).map(([packageName, license]) => ({
-  name: license.name,
+  name: license.name ?? packageName,
   version: license.version,
   license: license.licenses,
   text: license.licenseText,
