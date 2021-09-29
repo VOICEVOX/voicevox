72021dfc572e0cbd60e64a316c0f0a7b4c8ef0c7
aoirint <aoirint@gmail.com>
2021-09-30 05:51:14 +0900

    use log level for logging

--- src/background.ts
+++ src/background.ts
@@ -13,10 +13,23 @@ import path from "path";
 import { textEditContextMenu } from "./electron/contextMenu";
 import { hasSupportedGpu } from "./electron/device";
 import { ipcMainHandle, ipcMainSend } from "@/electron/ipc";
-import { logError } from "./electron/log";
 
 import fs from "fs";
 import { CharacterInfo, SavingSetting } from "./type/preload";
+
+import log from "electron-log";
+import dayjs from "dayjs";
+
+// silly以上のログをコンソールに出力
+log.transports.console.format = '[{h}:{i}:{s}.{ms}] [{level}] {text}';
+log.transports.console.level = 'silly';
+
+// warn以上のログをファイルに出力
+const prefix = dayjs().format('YYYYMMDD_HHmmss');
+log.transports.file.format = '[{h}:{i}:{s}.{ms}] [{level}] {text}';
+log.transports.file.level = 'warn';
+log.transports.file.fileName = `${prefix}_error.log`;
+
 
 const isDevelopment = process.env.NODE_ENV !== "production";
 
@@ -26,10 +39,10 @@ let win: BrowserWindow;
 if (!isDevelopment && !app.requestSingleInstanceLock()) app.quit();
 
 process.on("uncaughtException", (error) => {
-  logError(error);
+  log.error(error);
 });
 process.on("unhandledRejection", (reason) => {
-  logError(reason);
+  log.error(reason);
 });
 
 // 設定
@@ -330,7 +343,7 @@ ipcMainHandle("MAXIMIZE_WINDOW", () => {
 });
 
 ipcMainHandle("LOG_ERROR", (_, ...params) => {
-  logError(...params);
+  log.error(...params);
 });
 
 /**
@@ -363,7 +376,7 @@ ipcMainHandle(
       treeKill(engineProcess.pid, (error) => {
         // error変数の値がnull以外であればkillコマンドが失敗したことを意味します。
         if (error !== null) {
-          console.log(error);
+          log.error(error);
 
           // 再起動用に設定したclose listenerを削除。
           engineProcess.removeListener("close", closeListenerCallBack);
@@ -414,8 +427,9 @@ app.on("quit", () => {
   willQuitEngine = true;
   try {
     engineProcess.pid != undefined && treeKill(engineProcess.pid);
-  } catch {
-    logError("engine kill error");
+  } catch (e: unknown) {
+    log.error("engine kill error");
+    log.error(e);
   }
 });
 
@@ -429,7 +443,7 @@ app.on("ready", async () => {
       await installExtension(VUEJS3_DEVTOOLS);
     } catch (e: unknown) {
       if (e instanceof Error) {
-        logError("Vue Devtools failed to install:", e.toString());
+        log.error("Vue Devtools failed to install:", e.toString());
       }
     }
   }
