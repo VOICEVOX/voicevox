fc33f3dd087cacb97b5a4f11fd2583d7c75473a8
aoirint <aoirint@gmail.com>
2021-10-01 01:01:36 +0900

    fix process status check in RESTART_ENGINE

--- src/background.ts
+++ src/background.ts
@@ -376,8 +376,11 @@ ipcMainHandle(
       );
 
       // エンジンのプロセスが存在しない（すでに終了している）場合
-      if (engineProcess.exitCode !== null) {
-        log.info("ENGINE process is not started yet. Starting ENGINE...");
+      const engineExited = engineProcess.exitCode !== null;
+      const engineKilled = engineProcess.signalCode !== null;
+
+      if (engineExited || engineKilled) {
+        log.info("ENGINE process is not started yet or killed. Starting ENGINE...");
 
         runEngine();
         resolve();
