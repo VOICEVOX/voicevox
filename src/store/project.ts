9815664b7fce91bc79b59f5b74b7327d1e05cad2
raa0121 <raa0121@gmail.com>
2021-10-09 15:24:35 +0900

    あまあまの数値を間違えたのを修正

--- src/store/project.ts
+++ src/store/project.ts
@@ -180,12 +180,12 @@ export const projectStore: VoiceVoxStoreOptions<
               const audioItem = obj.audioItems[audioItemsKey];
               if (audioItem.characterIndex != null) {
                 if (audioItem.characterIndex == 0) {
-                  // 四国めたん 0 -> 四国めたん(あまあま) 1
-                  audioItem.speaker = 1;
+                  // 四国めたん 0 -> 四国めたん(あまあま) 0
+                  audioItem.speaker = 0;
                 }
                 if (audioItem.characterIndex == 1) {
-                  // ずんだもん 1 -> ずんだもん(あまあま) 2
-                  audioItem.speaker = 2;
+                  // ずんだもん 1 -> ずんだもん(あまあま) 1
+                  audioItem.speaker = 1;
                 }
                 delete audioItem.characterIndex;
               }
