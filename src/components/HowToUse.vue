e96cf1a5d2f87728a8d6b46692868b90d5c44b5a
Hiroshiba Kazuyuki <hihokaruta@gmail.com>
2021-10-10 14:26:12 +0900

    to 0.7.1

--- src/components/HowToUse.vue
+++ src/components/HowToUse.vue
@@ -11,7 +11,7 @@
           <p>ソフトウェアの使い方は以下をご参照ください。</p>
           <p>
             <a
-              href="https://docs.google.com/document/d/1AIiqFftlGvBp_ZOc9h1kCM8vIW_gkgPu49aUKMRtvFs/edit?usp=sharing"
+              href="https://docs.google.com/document/d/10kS3Jh12BmoRZPnrVhlVoWVv8_LW3VYBuZuVExwPaJI/edit?usp=sharing"
               target="_blank"
               >使い方</a
             >
