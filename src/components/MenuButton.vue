0a331569790a832db8db7ada674b4ddf6291aea2
MT224244 <mt224244@gmail.com>
2021-10-13 12:38:28 +0900

    メニューボタンをq-badgeにしていた原因が解消されたためq-btnを使用するよう変更

--- src/components/MenuButton.vue
+++ src/components/MenuButton.vue
@@ -1,19 +1,16 @@
 <template>
-  <q-badge
-    v-if="disable"
-    transparent
-    color="transparent"
+  <q-btn
+    flat
     text-color="secondary"
-    class="full-height cursor-not-allowed no-border-radius menu-disable"
-    >{{ menudata.label }}</q-badge
-  >
-  <!-- q-btnにすると謎のエラーが発生するためq-badgeを代用 -->
-  <!-- https://github.com/Hiroshiba/voicevox/pull/84#discussion_r689164447 -->
-  <q-badge
-    v-else
-    text-color="secondary"
-    class="full-height cursor-pointer no-border-radius"
+    class="
+      full-height
+      cursor-pointer
+      no-border-radius
+      text-no-wrap
+      q-py-none q-px-sm
+    "
     :class="selected ? 'active-menu' : 'bg-transparent'"
+    :disable="disable"
     @click="menudata.type === 'button' && menudata.onClick()"
   >
     {{ menudata.label }}
@@ -34,7 +31,7 @@
         />
       </q-list>
     </q-menu>
-  </q-badge>
+  </q-btn>
 </template>
 
 <script lang="ts">
@@ -111,13 +108,14 @@ export default defineComponent({
 <style lang="scss" scoped>
 @use '@/styles' as global;
 
-.q-badge {
-  font-size: 0.8rem !important;
-  &:not(.menu-disable):hover {
-    background-color: rgba(global.$primary, 0.3) !important;
-  }
-
+.q-btn {
+  min-height: 0;
   overflow: hidden;
-  text-overflow: ellipsis;
+  :deep(.q-btn__content) {
+    display: inline;
+    line-height: global.$menubar-height;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
 }
 </style>
