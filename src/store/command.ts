b3c8fc8572831ec065ea17fc6b5da03ff87d2281
segu_g <segusegment@gmail.com>
2021-10-19 00:31:59 +0900

    add comment

--- src/store/command.ts
+++ src/store/command.ts
@@ -1,9 +1,10 @@
 import { toRaw } from "vue";
 import { enablePatches, enableMapSet, Immer } from "immer";
-
+// immerの内部関数であるgetPlugin("Patches").applyPatches_はexportされていないので
+// ビルド前のsrcからソースコードを読み込んで使う必要がある
 import { enablePatches as enablePatchesImpl } from "immer/src/plugins/patches";
 import { enableMapSet as enableMapSetImpl } from "immer/src/plugins/mapset";
-import { getPlugin } from "immer/src/internal";
+import { getPlugin } from "immer/src/utils/plugins";
 
 import {
   Command,
@@ -14,10 +15,13 @@ import {
 } from "./type";
 import { Mutation, MutationsBase, MutationTree } from "@/store/vuex";
 
+// ビルド後のモジュールとビルド前のモジュールは別のスコープで変数を持っているので
+// enable * も両方叩く必要がある。
 enablePatches();
 enableMapSet();
 enablePatchesImpl();
 enableMapSetImpl();
+// immerのPatchをmutableに適応する内部関数
 const applyPatchesImpl = getPlugin("Patches").applyPatches_;
 
 const immer = new Immer();
