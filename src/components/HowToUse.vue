84d5137ac59657b90e542395a4a42f84b508a2cd
Hiroshiba <hihokaruta@gmail.com>
2021-10-21 17:58:27 +0900

    to 0.7.4　VOICEVOX ENGINE 0.7.4への対応 (#376)

--- src/components/HowToUse.vue
+++ src/components/HowToUse.vue
@@ -11,7 +11,7 @@
           <p>ソフトウェアの使い方は以下をご参照ください。</p>
           <p>
             <a
-              href="https://docs.google.com/document/d/1-lE7zS9M3HuXTndAq8e7HLa8FAxuB4Po2RtFf66pmAY/edit?usp=sharing"
+              href="https://docs.google.com/document/d/1_g-HZPvNII-_UahWcvfK08apVjsMsHxcw7zBJHAztiQ/edit?usp=sharing"
               target="_blank"
               >使い方</a
             >
