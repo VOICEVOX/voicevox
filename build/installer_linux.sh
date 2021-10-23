a53501def09fb88a3b64b982d3c3d976857502f0
aoirint <aoirint@gmail.com>
2021-10-24 04:31:20 +0900

    add comment

--- build/installer_linux.sh
+++ build/installer_linux.sh
@@ -209,6 +209,7 @@ done
 FIRST_ARCHIVE=${ARCHIVE_NAME_LIST[0]}
 ${COMMAND_7Z} x "${FIRST_ARCHIVE}" -y
 
+# Get AppImage filename from 7z archive
 APPIMAGE=$(${COMMAND_7Z} l -slt -ba "${FIRST_ARCHIVE}" | grep 'Path = ' | head -n1 | sed 's/Path = \(.*\)/\1/')
 chmod +x "${APPIMAGE}"
 
