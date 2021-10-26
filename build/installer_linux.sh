5ed7e3e29db4aa60193d64d06c86b60d95ebe0af
eggplants <w10776e8w@yahoo.co.jp>
2021-10-26 22:51:16 +0900

    fix: message

--- build/installer_linux.sh
+++ build/installer_linux.sh
@@ -33,7 +33,7 @@ ICON_INSTALL_DIR=${ICON_INSTALL_DIR:-$HOME/.local/share/icons}
 if [ -f "${APP_DIR}/VOICEVOX.AppImage" ]; then
     echo "[*] VOICEVOX already installed in '${APP_DIR}/VOICEVOX.AppImage'."
     while true; do
-        read -r -p "[*] Reinstall?(y/n): " yn
+        read -r -p "[*] Reinstall/Update?(y/n): " yn
         case "$yn" in
             [Yy]*)
                 break
