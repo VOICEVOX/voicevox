3a6f6c557b104bba090d6b5abfe2e40ad2940ca5
Segu <51497552+Segu-g@users.noreply.github.com>
2021-10-28 04:45:30 +0900

    スライダーのスクロール操作におけるコマンド発火タイミング制御 (#368)
    * add PreviewSlider.vue
    
    * fix wheel bug
    
    * Bugfix clicking a slider
    
    * change emit name
    
    * improve precision
    
    * Change previewSlider from a Vue component to a usePreviewSlider hook
    
    * use usePreviewSlider in AudioInfo.vue
    
    * use usePreviewSlider  in AudioParameter.vue
    
    * use usePreviewSlider on AudioAccent.vue
    
    * Fixed to change when the value is the same as modelValue.
    
    * remove isUpdated flag
    
    * Change the function name from "usePreviewSlider" to "previewSliderHelper".
    
    * delete funcUtils
    
    * expand qSliderProps
    
    * add comment
    
    * update comment
    
    * fix rollback
    
    * Revert "fix rollback"
    
    This reverts commit 0c9842deab2ec3ca23f2292d136cf900f543d6a4.
    
    * await change
    
    * cancelable change promise
    
    * fix step value and digits
    
    * fix pre/postPhoemeLength digits
    
    * Update previewSliderHelper comment
    
    Co-authored-by: Hiroshiba <hihokaruta@gmail.com>
    
    * Add a wait-for-change flag
    
    * fix step digits
    
    Co-authored-by: Hiroshiba <hihokaruta@gmail.com>

---
+++ src/helpers/previewSliderHelper.ts
@@ -0,0 +1,177 @@
+import { ref, computed, Ref, Events } from "vue";
+import { QSliderProps, debounce } from "quasar";
+export type Props = {
+  onPan?: QSliderProps["onPan"];
+  onChange?: QSliderProps["onChange"];
+  modelValue: () => number | null;
+  min?: () => number;
+  max?: () => number;
+  disable?: () => boolean;
+  step?: () => number;
+  scrollStep?: () => number;
+  scrollMinStep?: () => number;
+  disableScroll?: () => boolean;
+};
+
+export type PreviewSliderHelper = {
+  state: {
+    currentValue: Ref<number | null>;
+    isPanning: Ref<boolean>;
+    isScrolling: Ref<boolean>;
+    isAwaiting: Ref<boolean>;
+  };
+  qSliderProps: {
+    min: Ref<number>;
+    max: Ref<number>;
+    step: Ref<number>;
+    disable: Ref<boolean>;
+    "onUpdate:modelValue": (value: number) => void;
+    onChange: (value: number) => void;
+    onWheel: (event: Events["onWheel"]) => void;
+    onPan: QSliderProps["onPan"];
+  };
+};
+
+class CancelableFinary {
+  isCanceled: boolean;
+
+  constructor(promise: Promise<void>, func: (...args: unknown[]) => unknown) {
+    this.isCanceled = false;
+    promise.finally(() => {
+      if (!this.isCanceled) func();
+    });
+  }
+
+  cancel() {
+    this.isCanceled = true;
+  }
+}
+
+/**
+ * スライダー用のヘルパー関数。ホイール操作やプレビュー値表示の機能がある。
+ * @param props
+ * q-sliderの描画に用いる引数
+ * reactiveにする為にcomputedの引数の様に算出関数を渡す。
+ * @returns QSliderProps
+ * + state
+ *   currentValue等、Slider外での描画の為に用いることができる変数
+ * + qSliderProps
+ *   q-sliderに渡すべきprops
+ */
+export const previewSliderHelper = (props: Props): PreviewSliderHelper => {
+  const modelValue = computed(props.modelValue);
+  const min = computed(() => (props.min && props.min()) ?? 0);
+  const max = computed(() => (props.max && props.max()) ?? 100);
+  const disable = computed(() => (props.disable && props.disable()) ?? false);
+  const step = computed(() => (props.step && props.step()) ?? 1);
+  const scrollStep = computed(
+    () => (props.scrollStep && props.scrollStep()) ?? step.value
+  );
+  const scrollMinStep = computed(
+    () => (props.scrollMinStep && props.scrollMinStep()) ?? scrollStep.value
+  );
+  const disableScroll = computed(
+    () => (props.disableScroll && props.disableScroll()) ?? false
+  );
+
+  const previewValue = ref(modelValue.value);
+  const isPanning = ref(false);
+  const isScrolling = ref(false);
+  const isAwaiting = ref(false);
+
+  const currentValue = computed(() => {
+    if (isPanning.value || isScrolling.value || isAwaiting.value) {
+      return previewValue.value;
+    } else {
+      return modelValue.value;
+    }
+  });
+
+  const updatePreviewValue = (value: number) => {
+    previewValue.value = value;
+  };
+  const changePreviewValue = async () => {
+    if (previewValue.value === null)
+      throw new Error("previewValue.value === null");
+    if (modelValue.value !== previewValue.value && props.onChange) {
+      const ret: unknown = props.onChange(previewValue.value);
+      if (ret instanceof Promise) await ret;
+    }
+  };
+
+  let awaitingChange: CancelableFinary | null = null;
+  const endAwaiting = () => {
+    isAwaiting.value = false;
+  };
+  const fireChange = () => {
+    if (awaitingChange !== null) awaitingChange.cancel();
+    isAwaiting.value = true;
+    awaitingChange = new CancelableFinary(changePreviewValue(), endAwaiting);
+  };
+
+  const onPan: QSliderProps["onPan"] = (phase) => {
+    if (phase == "start") {
+      // start panning
+      isPanning.value = true;
+    } else {
+      // end panning
+      isPanning.value = false;
+    }
+    if (props.onPan) props.onPan(phase);
+  };
+
+  const debounceScroll = debounce(() => {
+    fireChange();
+    isScrolling.value = false;
+  }, 300);
+
+  const scrollStepDecimals = computed(() => {
+    return String(scrollStep.value).split(".")[1]?.length ?? 0;
+  });
+  const scrollMinStepDecimals = computed(() => {
+    return String(scrollMinStep.value).split(".")[1]?.length ?? 0;
+  });
+
+  const onWheel = (event: Events["onWheel"]) => {
+    if (disableScroll.value || disable.value || currentValue.value === null)
+      return;
+    event.preventDefault();
+    const deltaY = event.deltaY;
+    const ctrlKey = event.ctrlKey;
+    const step = ctrlKey ? scrollMinStep.value : scrollStep.value;
+    const decimals = ctrlKey
+      ? scrollMinStepDecimals.value
+      : scrollStepDecimals.value;
+    const diff = -step * Math.sign(deltaY);
+    const nextValue = Math.min(
+      Math.max(currentValue.value + diff, min.value),
+      max.value
+    );
+    updatePreviewValue(Number.parseFloat(nextValue.toFixed(decimals)));
+    // start scroll
+    isScrolling.value = true;
+    debounceScroll();
+  };
+
+  const qSliderProps = {
+    min: min,
+    max: max,
+    step: step,
+    disable: disable,
+    modelValue: currentValue,
+    "onUpdate:modelValue": updatePreviewValue,
+    onChange: fireChange,
+    onWheel,
+    onPan,
+  };
+
+  return {
+    state: {
+      currentValue,
+      isPanning,
+      isScrolling,
+      isAwaiting,
+    },
+    qSliderProps,
+  };
+};
