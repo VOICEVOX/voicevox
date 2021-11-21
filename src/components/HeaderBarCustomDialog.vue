1e144f219bfd6777ece71c7fea006fe7bb66d2d9
y-chan <y-chan@y-chan.dev>
2021-11-22 04:24:16 +0900

    move comment

--- src/components/HeaderBarCustomDialog.vue
+++ src/components/HeaderBarCustomDialog.vue
@@ -253,6 +253,7 @@ export default defineComponent({
     );
     const removable = computed(() => selectedButton.value !== undefined);
 
+    // 配列の比較は出来ないので、文字列として結合したものを比較する
     const changedOrNotFlag = computed(() => {
       const nowSetting = store.state.toolbarSetting;
       return toolbarButtons.value.join("") !== nowSetting.join("");
@@ -325,7 +326,6 @@ export default defineComponent({
     };
 
     const finishOrNotDialog = () => {
-      // 配列の比較は出来ないので、文字列として結合したものを比較する
       if (changedOrNotFlag.value) {
         $q.dialog({
           title: "カスタマイズを終了しますか？",
