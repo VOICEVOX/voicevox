b9f76b50dd4765e5a92daada9a573d97e7d7be6a
y-chan <y-chan@y-chan.dev>
2021-12-02 02:37:02 +0900

    add generate and connect and save all audio to menubar

--- src/components/MenuBar.vue
+++ src/components/MenuBar.vue
@@ -126,6 +126,46 @@ export default defineComponent({
       }
     };
 
+    const generateAndConnectAndSaveAllAudio = async () => {
+      if (!uiLocked.value) {
+        const result = await store.dispatch(
+          "GENERATE_AND_CONNECT_AND_SAVE_AUDIO",
+          {
+            encoding: store.state.savingSetting.fileEncoding,
+          }
+        );
+
+        if (
+          result === undefined ||
+          result.result === "SUCCESS" ||
+          result.result === "CANCELED"
+        )
+          return;
+
+        let msg = "";
+        switch (result.result) {
+          case "WRITE_ERROR":
+            msg =
+              "書き込みエラーによって失敗しました。空き容量があることや、書き込み権限があることをご確認ください。";
+            break;
+          case "ENGINE_ERROR":
+            msg =
+              "エンジンのエラーによって失敗しました。エンジンの再起動をお試しください。";
+            break;
+        }
+
+        $q.dialog({
+          title: "書き出しに失敗しました。",
+          message: msg,
+          ok: {
+            label: "閉じる",
+            flat: true,
+            textColor: "secondary",
+          },
+        });
+      }
+    };
+
     const generateAndSaveOneAudio = async () => {
       if (uiLocked.value) return;
 
@@ -245,6 +285,13 @@ export default defineComponent({
             label: "一つだけ書き出し",
             onClick: () => {
               generateAndSaveOneAudio();
+            },
+          },
+          {
+            type: "button",
+            label: "音声を繋げて書き出し",
+            onClick: () => {
+              generateAndConnectAndSaveAllAudio();
             },
           },
           {
