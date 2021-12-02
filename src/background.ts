c4958bd3f8039f93bff7e333a4baa3cd095248c6
raa0121 <raa0121@gmail.com>
2021-12-02 23:52:27 +0900

    fix for lint

--- src/background.ts
+++ src/background.ts
@@ -25,7 +25,7 @@ import {
   StyleInfo,
 } from "./type/preload";
 
-import { autoUpdater } from "electron-updater"
+import { autoUpdater } from "electron-updater";
 import log from "electron-log";
 import dayjs from "dayjs";
 
@@ -828,34 +828,35 @@ if (isDevelopment) {
 // 自動アップデート関連のイベント処理
 //-------------------------------------------
 // アップデートをチェック開始
-autoUpdater.on('checking-for-update', () => {
-  log.info(process.pid, 'checking-for-update...');
+autoUpdater.on("checking-for-update", () => {
+  log.info(process.pid, "checking-for-update...");
 });
 // アップデートが見つかった
-autoUpdater.on('update-available', (ev, info) => {
-  log.info(process.pid, 'Update available.');
+autoUpdater.on("update-available", () => {
+  log.info(process.pid, "Update available.");
 });
 // アップデートがなかった（最新版だった）
-autoUpdater.on('update-not-available', (ev, info) => {
-  log.info(process.pid, 'Update not available.');
+autoUpdater.on("update-not-available", () => {
+  log.info(process.pid, "Update not available.");
 });
 // アップデートのダウンロードが完了
-autoUpdater.on('update-downloaded', (info) => {
+autoUpdater.on("update-downloaded", () => {
   const dialogOpts = {
-    type: 'info',
-    buttons: ['更新して再起動', 'あとで'],
-    message: 'アップデート',
-    detail: '新しいバージョンをダウンロードしました。再起動して更新を適用しますか？'
-  }
+    type: "info",
+    buttons: ["更新して再起動", "あとで"],
+    message: "アップデート",
+    detail:
+      "新しいバージョンをダウンロードしました。再起動して更新を適用しますか？",
+  };
 
   // ダイアログを表示しすぐに再起動するか確認
   dialog.showMessageBox(win, dialogOpts).then((returnValue) => {
-    if (returnValue.response === 0){
-      autoUpdater.quitAndInstall()
+    if (returnValue.response === 0) {
+      autoUpdater.quitAndInstall();
     }
-  })
+  });
 });
 // エラーが発生
-autoUpdater.on('error', (err) => {
+autoUpdater.on("error", (err) => {
   log.error(process.pid, err);
 });
