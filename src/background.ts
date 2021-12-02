bb597851df8e81190eb453134872eaeda001a370
raa0121 <raa0121@gmail.com>
2021-12-02 23:37:53 +0900

    add electron-updater

--- src/background.ts
+++ src/background.ts
@@ -25,6 +25,7 @@ import {
   StyleInfo,
 } from "./type/preload";
 
+import { autoUpdater } from "electron-updater"
 import log from "electron-log";
 import dayjs from "dayjs";
 
@@ -46,6 +47,8 @@ if (isDevelopment) {
     path.join(app.getPath("appData"), `${app.getName()}-dev`)
   );
 }
+
+autoUpdater.logger = log;
 
 let win: BrowserWindow;
 
@@ -797,6 +800,7 @@ app.on("ready", async () => {
   }
 
   createWindow().then(() => runEngine());
+  autoUpdater.checkForUpdatesAndNotify();
 });
 
 app.on("second-instance", () => {
@@ -819,3 +823,39 @@ if (isDevelopment) {
     });
   }
 }
+
+//-------------------------------------------
+// 自動アップデート関連のイベント処理
+//-------------------------------------------
+// アップデートをチェック開始
+autoUpdater.on('checking-for-update', () => {
+  log.info(process.pid, 'checking-for-update...');
+});
+// アップデートが見つかった
+autoUpdater.on('update-available', (ev, info) => {
+  log.info(process.pid, 'Update available.');
+});
+// アップデートがなかった（最新版だった）
+autoUpdater.on('update-not-available', (ev, info) => {
+  log.info(process.pid, 'Update not available.');
+});
+// アップデートのダウンロードが完了
+autoUpdater.on('update-downloaded', (info) => {
+  const dialogOpts = {
+    type: 'info',
+    buttons: ['更新して再起動', 'あとで'],
+    message: 'アップデート',
+    detail: '新しいバージョンをダウンロードしました。再起動して更新を適用しますか？'
+  }
+
+  // ダイアログを表示しすぐに再起動するか確認
+  dialog.showMessageBox(win, dialogOpts).then((returnValue) => {
+    if (returnValue.response === 0){
+      autoUpdater.quitAndInstall()
+    }
+  })
+});
+// エラーが発生
+autoUpdater.on('error', (err) => {
+  log.error(process.pid, err);
+});
