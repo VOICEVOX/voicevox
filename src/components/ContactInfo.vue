4b8aebb5ea353b874523015efd5a341084cdffb9
Yuki <91584669+Apple-Yuki@users.noreply.github.com>
2021-12-26 01:20:28 +0900

    お問い合わせ画面を追加（VOICEVOX#615）

---
+++ src/components/ContactInfo.vue
@@ -0,0 +1,33 @@
+<template>
+  <q-page class="relative-absolute-wrapper scroller bg-background">
+    <div class="q-pa-md markdown markdown-body" v-html="Contact"></div>
+  </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted, ref } from "vue";
+import { useStore } from "@/store";
+import { useMarkdownIt } from "@/plugins/markdownItPlugin";
+export default defineComponent({
+  setup() {
+    const store = useStore();
+    const policy = ref("");
+    const md = useMarkdownIt();
+    onMounted(async () => {
+      policy.value = md.render(await store.dispatch("GET_POLICY_TEXT"));
+    });
+    return {
+      policy,
+    };
+  },
+});
+</script>
+
+<style scoped lang="scss">
+.root {
+  .scroller {
+    width: 100%;
+    overflow: auto;
+  }
+}
+</style>
