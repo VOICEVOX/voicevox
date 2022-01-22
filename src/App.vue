5ea0b2450c77f8696a54bdd478b83f2652c18a76
aoirint <aoirint@gmail.com>
2022-01-23 07:21:36 +0900

    revert src/App.vue

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,32 @@
 </template>
 
 <script lang="ts">
+import { useStore } from "@/store";
 import ErrorBoundary from "@/components/ErrorBoundary.vue";
-import { defineComponent } from "vue";
+import { defineComponent, watch } from "vue";
+import { useGtm } from "@gtm-support/vue-gtm";
 
 export default defineComponent({
   name: "App",
 
   components: {
     ErrorBoundary,
+  },
+
+  setup() {
+    const store = useStore();
+    store.dispatch("INIT_VUEX");
+    store.dispatch("START_WAITING_ENGINE");
+
+    // Google Tag Manager
+    const gtm = useGtm();
+    watch(
+      () => store.state.acceptRetrieveTelemetry,
+      (acceptRetrieveTelemetry) => {
+        gtm?.enable(acceptRetrieveTelemetry === "Accepted");
+      },
+      { immediate: true }
+    );
   },
 });
 </script>
