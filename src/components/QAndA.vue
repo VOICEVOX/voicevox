5f21a0356b767f170a3f0df062720db2fa3d3503
Hiroshiba <hihokaruta@gmail.com>
2022-01-26 19:01:24 +0900

    よくある質問を追加 (#666)

---
+++ src/components/QAndA.vue
@@ -0,0 +1,41 @@
+<template>
+  <q-page class="relative-absolute-wrapper scroller bg-background">
+    <div class="q-pa-md markdown markdown-body" v-html="qAndA"></div>
+  </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted, ref } from "vue";
+import { useStore } from "@/store";
+import { useMarkdownIt } from "@/plugins/markdownItPlugin";
+
+export default defineComponent({
+  setup() {
+    const store = useStore();
+    const qAndA = ref("");
+
+    const md = useMarkdownIt();
+
+    onMounted(async () => {
+      qAndA.value = md.render(await store.dispatch("GET_Q_AND_A_TEXT"));
+    });
+
+    return {
+      qAndA,
+    };
+  },
+});
+</script>
+
+<style scoped lang="scss">
+.root {
+  .scroller {
+    width: 100%;
+    overflow: auto;
+  }
+}
+
+.markdown :deep(img) {
+  border: 1px solid #333;
+}
+</style>
