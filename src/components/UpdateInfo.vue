54b6ace138cda6633cfebab47a72f52edf5f4e10
牛蒡 <gobosan@outlook.jp>
2022-02-06 17:04:26 +0900

    add the ability to check for updates

--- src/components/UpdateInfo.vue
+++ src/components/UpdateInfo.vue
@@ -21,10 +21,39 @@ export default defineComponent({
     const infos = ref<UpdateInfo[]>();
     store.dispatch("GET_UPDATE_INFOS").then((obj) => (infos.value = obj));
 
+    const currentVersion = ref("");
+    window.electron.getAppInfos().then((obj) => {
+      currentVersion.value = obj.version;
+    });
+
+    const latestVersion = ref("");
+    let tags: string[] = [];
+    fetch("https://api.github.com/repos/VOICEVOX/voicevox/releases")
+      .then((res) => res.json())
+      .then((obj) => {
+        obj.map((item: { prerelease: boolean; tag_name: string }) => {
+          if (item.prerelease == false) {
+            tags.push(item.tag_name);
+          }
+        });
+        latestVersion.value = tags[0];
+      });
+
     const html = computed(() => {
       if (!infos.value) return "";
 
       let html = "";
+      if (currentVersion.value === latestVersion.value) {
+        html += `<h3>お使いの VOICEBOX は最新です！</h3>`;
+        html += "<hr />";
+      } else {
+        html += `<h3>アップデートがあります！</h3>`;
+        html += "<p>現在のバージョン：" + currentVersion.value + "</p>";
+        html += "<p>最新のバージョン：" + latestVersion.value + "</p>";
+        html += "<h4>ダウンロードページ</h4>";
+        html += `<a href="https://voicevox.hiroshiba.jp/" target="_blank">https://voicevox.hiroshiba.jp/</a>`;
+        html += "<hr />";
+      }
       for (const info of infos.value) {
         const version: string = info.version;
         const descriptions: string[] = info.descriptions;
