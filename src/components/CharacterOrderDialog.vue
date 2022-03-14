b461f7081c2d2182f0b307047d247d63c45db4d3
aoirint <aoirint@gmail.com>
2022-03-14 12:13:02 +0900

    CharacterOrderDialog: change icon per style

--- src/components/CharacterOrderDialog.vue
+++ src/components/CharacterOrderDialog.vue
@@ -71,7 +71,9 @@
                 <div class="character-item-inner">
                   <img
                     :src="
-                      characterInfosMap[speakerUuid].metas.styles[0].iconPath
+                      characterInfosMap[speakerUuid].metas.styles[
+                        selectedStyleIndexes[speakerUuid] ?? 0
+                      ].iconPath
                     "
                     class="style-icon"
                   />
