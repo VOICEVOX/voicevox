b49667c520c874359aba022c13b9a3e9916e8423
Yuki <91584669+Apple-Yuki@users.noreply.github.com>
2022-03-15 20:46:37 +0900

    Update DictionaryManageDialog.vue

--- src/components/DictionaryManageDialog.vue
+++ src/components/DictionaryManageDialog.vue
@@ -90,7 +90,9 @@
 
           <!-- 右側のpane -->
           <div v-if="wordEditing" class="col-8 no-wrap text-no-wrap">
-          <div class="row q-pl-md q-pt-sm text-h6">単語登録</div><div class="row q-pl-md accent-desc">全角表示となりますが単語は全角と半角は区別しません。</div>
+          <div class="row q-pl-md q-mt-md accent-desc">
+            全角表示となりますが単語は全角と半角は区別しません。
+            </div>
             <div class="row q-pl-md q-mt-md">
               <div class="text-h6">単語</div>
               <q-input
