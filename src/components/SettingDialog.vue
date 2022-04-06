72aca0a6a027a6143e3dcf35904c91ce6ccb3ce7
jdkfx <amateurranch@gmail.com>
2022-04-06 22:27:21 +0900

    文字コードの詳細表示アイコンを追加

--- src/components/SettingDialog.vue
+++ src/components/SettingDialog.vue
@@ -163,6 +163,24 @@
               </q-card-actions>
               <q-card-actions class="q-px-md q-py-sm bg-setting-item">
                 <div>文字コード</div>
+                <div>
+                  <q-icon
+                    name="help_outline"
+                    color="grey-8"
+                    size="sm"
+                    class="help-hover-icon"
+                  >
+                    <q-tooltip
+                      :delay="500"
+                      anchor="center left"
+                      self="center right"
+                      transition-show="jump-left"
+                      transition-hide="jump-right"
+                    >
+                      文字コードを選ぶことができます
+                    </q-tooltip>
+                  </q-icon>
+                </div>
                 <q-space />
                 <q-btn-toggle
                   padding="xs md"
