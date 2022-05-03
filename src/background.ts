ca234029fc247593b58779e431357792fb58a425
PickledChair <ubatamamoon@gmail.com>
2022-05-03 13:08:36 +0900

    ビルド後はappDirPathにある.envを読みに行く

--- src/background.ts
+++ src/background.ts
@@ -79,7 +79,12 @@ process.on("unhandledRejection", (reason) => {
 
 // .envから設定をprocess.envに読み込み
 const appDirPath = path.dirname(app.getPath("exe"));
-dotenv.config({ override: true });
+if (isDevelopment) {
+  dotenv.config({ override: true });
+} else {
+  const envPath = path.join(appDirPath, ".env");
+  dotenv.config({ path: envPath });
+}
 
 protocol.registerSchemesAsPrivileged([
   { scheme: "app", privileges: { secure: true, standard: true, stream: true } },
