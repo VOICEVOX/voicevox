5947a587469f7ab70350740ea5a6f046eb674db0
Hiroshiba <hihokaruta@gmail.com>
2022-05-31 03:39:19 +0900

    DirectML版をGPUありのデフォルトにし、文言をすべて合わせる

--- src/electron/device.ts
+++ src/electron/device.ts
@@ -1,13 +1,21 @@
 import si from "systeminformation";
 
-export function hasSupportedGpu(): Promise<boolean> {
+/**
+ * GPUモードがサポートされているかどうか。
+ * windowsの場合は全GPUに対応。
+ * linuxの場合はNVIDIAのみ対応。
+ * なんとなく1GB以上の場合は対応していることにする。
+ */
+export function hasSupportedGpu(platform: string): Promise<boolean> {
   return si
     .graphics()
     .then((data) =>
       data.controllers.some(
         (datum) =>
-          datum.vendor.toUpperCase().indexOf("NVIDIA") !== -1 &&
-          datum.vram >= 3072
+          (platform === "win32" ||
+            (platform === "linux" &&
+              datum.vendor.toUpperCase().indexOf("NVIDIA") !== -1)) &&
+          datum.vram >= 1024
       )
     )
     .catch(() => false);
