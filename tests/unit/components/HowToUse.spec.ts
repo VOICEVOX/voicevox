bab66dca4f705a286a8def4d6017643bbeb26724
madosuki <yurikasamaaikatsu@gmail.com>
2022-06-28 20:18:39 +0900

    [FIX] npm run test:unit時にHowToUse.spec.tsで起きるエラーを修正 (#850)
    * add detectNvidia function
    
    * follow to menu bar
    
    * remove no longer a variable
    
    * run fmt
    
    * fix typo
    
    * remove utils.ts and some changes in background.ts
    
    * change message box type and detail, and change behaviour of setOnLaunchModeItemClicked
    
    * recreate package-lock.json using node 12.18.2
    
    * re recreate package-lock.json using node 12.18.2 and npm 7.20.5
    
    * recreate package-lock.json using node 12.18.2 and npm 6.14.5
    
    * recreate package-lock.json using node 12.18.2 and npm 7.20.3
    
    * fix conflict
    
    * experimentaly fix
    
    * remove no longer need lines
    
    * fix failed resolve q-page

--- tests/unit/components/HowToUse.spec.ts
+++ tests/unit/components/HowToUse.spec.ts
@@ -1,8 +1,27 @@
 import { shallowMount } from "@vue/test-utils";
 import HowToUse from "@/components/HowToUse.vue";
+import { markdownItPlugin } from "@/plugins/markdownItPlugin";
+import { createStore } from "vuex";
+import { storeKey } from "@/store";
+import { QPage } from "quasar";
+
+const store = createStore({
+  actions: {
+    GET_HOW_TO_USE_TEXT: async () => {
+      return "test string";
+    },
+  },
+});
 
 describe("HowToUse.vue", () => {
   it("can mount", () => {
-    shallowMount(HowToUse);
+    shallowMount(HowToUse, {
+      global: {
+        plugins: [markdownItPlugin, [store, storeKey]],
+        components: {
+          QPage: QPage,
+        },
+      },
+    });
   });
 });
