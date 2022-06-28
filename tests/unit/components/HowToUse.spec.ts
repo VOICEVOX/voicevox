16d7097ac9f83186878fb49cca2ec677f82251e4
madosuki <yurikasamaaikatsu@gmail.com>
2022-06-28 13:25:39 +0900

    experimentaly fix

--- tests/unit/components/HowToUse.spec.ts
+++ tests/unit/components/HowToUse.spec.ts
@@ -1,8 +1,23 @@
 import { shallowMount } from "@vue/test-utils";
 import HowToUse from "@/components/HowToUse.vue";
+import { markdownItPlugin } from "@/plugins/markdownItPlugin";
+import { createStore } from "vuex";
+import { storeKey } from "@/store";
+
+const store = createStore({
+  actions: {
+    GET_HOW_TO_USE_TEXT: async () => {
+      return "test string";
+    },
+  },
+});
 
 describe("HowToUse.vue", () => {
   it("can mount", () => {
-    shallowMount(HowToUse);
+    shallowMount(HowToUse, {
+      global: {
+        plugins: [markdownItPlugin, [store, storeKey]],
+      },
+    });
   });
 });
