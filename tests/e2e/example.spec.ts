9cb02cd6fb8c1bbbcb0c730aac9ec45e82267dd0
so-c <so_c321@hotmail.co.jp>
2022-07-15 16:11:03 +0900

    eslintの自動修正

--- tests/e2e/example.spec.ts
+++ tests/e2e/example.spec.ts
@@ -1,15 +1,15 @@
-import { test } from '@playwright/test';
+import { test } from "@playwright/test";
 import { assert } from "chai";
-import { testWithPlaywright } from 'vue-cli-plugin-electron-builder';
+import { testWithPlaywright } from "vue-cli-plugin-electron-builder";
 
-test('launch app', async () => {
-    const { app, stop } = await testWithPlaywright();
+test("launch app", async () => {
+  const { app, stop } = await testWithPlaywright();
 
-    // Get the first window that the app opens, wait if necessary.
-    const win = await app.firstWindow()
-    await win.waitForLoadState('domcontentloaded')
+  // Get the first window that the app opens, wait if necessary.
+  const win = await app.firstWindow();
+  await win.waitForLoadState("domcontentloaded");
 
-    assert.equal(await win.title(), 'voicevox')
+  assert.equal(await win.title(), "voicevox");
 
-    await stop()
-})
+  await stop();
+});
