d44d38e91e9e2084cad6b6cf84e2e5ab5464f96d
so-c <so_c321@hotmail.co.jp>
2022-07-17 06:34:29 +0900

    compare vue cli mode.

--- tests/e2e/example.spec.ts
+++ tests/e2e/example.spec.ts
@@ -2,8 +2,20 @@ import { test } from "@playwright/test";
 import { assert } from "chai";
 import { testWithPlaywright } from "vue-cli-plugin-electron-builder";
 
-test("launch app", async () => {
-  const { app, stop } = await testWithPlaywright();
+test("launch app in production mode", async () => {
+  const {app, stop } = await testWithPlaywright({mode: "production"});
+
+  // Get the first window that the app opens, wait if necessary.
+  const win = await app.firstWindow();
+  await win.waitForLoadState("domcontentloaded");
+
+  assert.equal(await win.title(), "voicevox");
+
+  await stop();
+});
+
+test("launch app in development mode", async () => {
+  const {app, stop } = await testWithPlaywright({mode: "development"});
 
   // Get the first window that the app opens, wait if necessary.
   const win = await app.firstWindow();
