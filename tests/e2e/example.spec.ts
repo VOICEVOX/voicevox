38414411e3f84467501e0e49f1a3a2684a14807a
so-c <so_c321@hotmail.co.jp>
2022-07-18 08:35:45 +0900

    #7 コードフォーマットの類

--- tests/e2e/example.spec.ts
+++ tests/e2e/example.spec.ts
@@ -1,15 +1,14 @@
-import { Page, test } from "@playwright/test";
-import { assert } from "chai";
+import { test } from "@playwright/test";
 import { testWithPlaywright } from "vue-cli-plugin-electron-builder";
 import dotenv from "dotenv";
 
 test("起動したら「利用規約に関するお知らせ」が表示される", async () => {
-  dotenv.config();  // FIXME: エンジンの設定直読み
+  dotenv.config(); // FIXME: エンジンの設定直読み
   const { app, stop } = await testWithPlaywright();
   const sut = await app.firstWindow();
 
   // エンジンが起動し「利用規約に関するお知らせ」が表示されるのを待つ
-  await sut.waitForSelector('text=利用規約に関するお知らせ');
+  await sut.waitForSelector("text=利用規約に関するお知らせ");
 
   await stop();
 });
