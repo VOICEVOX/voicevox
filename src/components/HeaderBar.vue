967544353a279b94d0b4f8409f7c9129d3e095e1
sevenc-nanashi <sevenc7c@sevenc7c.com>
2022-07-31 13:43:52 +0900

    Delete: 不要になったクラスを削除

--- src/components/HeaderBar.vue
+++ src/components/HeaderBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-header class="q-py-sm header-bar">
+  <q-header class="q-py-sm">
     <q-toolbar>
       <template v-for="button in headerButtons" :key="button.text">
         <q-space v-if="button.text === null" />
@@ -218,11 +218,3 @@ export default defineComponent({
   },
 });
 </script>
-
-<style scoped lang="scss">
-@use "@/styles/colors" as colors;
-
-.header-bar {
-  background: colors.$background-header;
-}
-</style>
