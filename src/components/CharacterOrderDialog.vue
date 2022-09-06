ccc336137fcad0079e64c06e46d878cdfb06e9ae
sevenc-nanashi <sevenc7c@sevenc7c.com>
2022-09-07 05:30:30 +0900

    Add: FIXMEを追加

--- src/components/CharacterOrderDialog.vue
+++ src/components/CharacterOrderDialog.vue
@@ -280,6 +280,7 @@ export default defineComponent({
           selectedCharacter.value = sampleCharacterOrder.value[0];
 
           // 保存済みのキャラクターリストを取得
+          // FIXME: 並び替えると不明なキャラがリセットされる。
           characterOrder.value = store.state.userCharacterOrder
             .map((speakerUuid) => characterInfosMap.value[speakerUuid])
             .filter((info) => info !== undefined) as CharacterInfo[];
