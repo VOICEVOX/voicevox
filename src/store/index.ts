6815a34307c9eacf8d2a346765a9d4f8b8c0efe0
sevenc-nanashi <sevenc7c@sevenc7c.com>
2022-09-20 06:16:26 +0900

    Add: コメントを追加

--- src/store/index.ts
+++ src/store/index.ts
@@ -39,6 +39,7 @@ export const indexStore = createPartialStore<IndexStoreTypes>({
   GET_FLATTEN_CHARACTER_INFOS: {
     /**
      * すべてのエンジンのキャラクター情報のリスト。
+     * 同じspeakerUuidのキャラクター情報は、登録順が速いエンジンの情報を元に統合される。
      * キャラクター情報が読み出されていないときは、空リストを返す。
      */
     getter(state) {
