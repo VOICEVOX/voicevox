b0155951f8462f2714b8c54975290ea6a65e319b
Hiroshiba <hihokaruta@gmail.com>
2022-09-30 15:55:00 +0900

    初回起動時のデフォルトスタイルID選択のとき、「ノーマル」を最初から選ばれている状態にする

--- src/store/type.ts
+++ src/store/type.ts
@@ -712,10 +712,6 @@ type IndexStoreTypes = {
     action(): Promise<string>;
   };
 
-  IS_UNSET_DEFAULT_STYLE_ID: {
-    action(payload: { speakerUuid: string }): Promise<boolean>;
-  };
-
   LOAD_DEFAULT_STYLE_IDS: {
     action(): Promise<void>;
   };
