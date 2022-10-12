2160a9d19f6ed9581c7a06ca05319026c40ddf58
しごぶ <32050537+shigobu@users.noreply.github.com>
2022-10-12 10:44:18 +0900

    メッセージを表示する条件をわかりやすいものに変更
    Co-authored-by: Hiroshiba <hihokaruta@gmail.com>

--- src/store/project.ts
+++ src/store/project.ts
@@ -325,14 +325,15 @@ export const projectStore = createPartialStore<ProjectStoreTypes>({
             return;
           }
           filePath = ret;
-          if (context.state.projectFilePath) {
-            window.electron.showMessageDialog({
-              type: "info",
-              title: "保存",
-              message: `編集中のプロジェクトが ${filePath} に切り替わりました。`,
-            });
-          }
         }
+        if (context.state.projectFilePath && context.state.projectFilePath != filePath) {
+          window.electron.showMessageDialog({
+            type: "info",
+            title: "保存",
+            message: `編集中のプロジェクトが ${filePath} に切り替わりました。`,
+          });
+        }
+
         const appInfos = await window.electron.getAppInfos();
         const { audioItems, audioKeys } = context.state;
         const projectData: ProjectType = {
