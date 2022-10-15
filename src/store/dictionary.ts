678c08ebd007cab30406be1d8d3feb1ea9987dee
sevenc-nanashi <sevenc7c@sevenc7c.com>
2022-10-15 23:57:36 +0900

    Refactor: マージ処理のコードの順序を変更

--- src/store/dictionary.ts
+++ src/store/dictionary.ts
@@ -125,6 +125,21 @@ export const dictionaryStore = createPartialStore<DictionaryStoreTypes>({
     async action({ dispatch, state }) {
       const mergedDict = await dispatch("LOAD_ALL_USER_DICT");
       for (const engineId of state.engineIds) {
+        await dispatch("INSTANTIATE_ENGINE_CONNECTOR", {
+          engineId,
+        }).then((instance) =>
+          // マージした辞書をエンジンにインポートする。
+          instance.invoke("importUserDictWordsImportUserDictPost")({
+            override: true,
+            requestBody: Object.fromEntries(
+              Object.entries(mergedDict).map(([k, v]) => [
+                k,
+                UserDictWordToJSON(v),
+              ])
+            ),
+          })
+        );
+
         // エンジンの辞書のIDリストを取得する。
         const removedDictIdSet = await dispatch(
           "INSTANTIATE_ENGINE_CONNECTOR",
@@ -147,21 +162,10 @@ export const dictionaryStore = createPartialStore<DictionaryStoreTypes>({
           }
         }
 
-        dispatch("INSTANTIATE_ENGINE_CONNECTOR", { engineId }).then(
-          async (instance) => {
-            // マージした辞書をエンジンにインポートする。
-            await instance.invoke("importUserDictWordsImportUserDictPost")({
-              override: true,
-              requestBody: Object.fromEntries(
-                Object.entries(mergedDict).map(([k, v]) => [
-                  k,
-                  UserDictWordToJSON(v),
-                ])
-              ),
-            });
-
+        await dispatch("INSTANTIATE_ENGINE_CONNECTOR", { engineId }).then(
+          (instance) => {
             // マージ処理で削除された項目をエンジンから削除する。
-            await Promise.all(
+            Promise.all(
               [...removedDictIdSet].map((id) =>
                 instance.invoke("deleteUserDictWordUserDictWordWordUuidDelete")(
                   {
