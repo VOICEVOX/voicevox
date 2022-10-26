dcb4cd11bb30ae68293098694ce7469cf15b05d5
Sig <sigprogrammer@gmail.com>
2022-10-26 17:59:47 +0900

    project-s: 初期化とシンガー・楽譜を設定するaction/mutationを追加 (#988)
    * 初期化とシンガー・楽譜を設定するaction/mutationを追加

--- src/store/singing.ts
+++ src/store/singing.ts
@@ -1,4 +1,4 @@
-import { SingingStoreState, SingingStoreTypes } from "./type";
+import { Score, SingingStoreState, SingingStoreTypes } from "./type";
 import { createPartialStore } from "./vuex";
 
 export const singingStoreState: SingingStoreState = {
@@ -19,6 +19,76 @@ export const singingStore = createPartialStore<SingingStoreTypes>({
       commit("SET_SHOW_SINGER", {
         isShowSinger,
       });
+    },
+  },
+
+  SET_SINGER: {
+    mutation(
+      state,
+      { engineId, styleId }: { engineId: string; styleId: number }
+    ) {
+      state.engineId = engineId;
+      state.styleId = styleId;
+    },
+    async action(
+      { state, getters, dispatch, commit },
+      payload: { engineId?: string; styleId?: number }
+    ) {
+      if (state.defaultStyleIds == undefined)
+        throw new Error("state.defaultStyleIds == undefined");
+      if (getters.USER_ORDERED_CHARACTER_INFOS == undefined)
+        throw new Error("state.characterInfos == undefined");
+      const userOrderedCharacterInfos = getters.USER_ORDERED_CHARACTER_INFOS;
+
+      const engineId = payload.engineId ?? state.engineIds[0];
+
+      // FIXME: engineIdも含めて探査する
+      const styleId =
+        payload.styleId ??
+        state.defaultStyleIds[
+          state.defaultStyleIds.findIndex(
+            (x) =>
+              x.speakerUuid === userOrderedCharacterInfos[0].metas.speakerUuid // FIXME: defaultStyleIds内にspeakerUuidがない場合がある
+          )
+        ].defaultStyleId;
+
+      try {
+        // 指定されたstyleIdに対して、エンジン側の初期化を行う
+        const isInitialized = await dispatch("IS_INITIALIZED_ENGINE_SPEAKER", {
+          engineId,
+          styleId,
+        });
+        if (!isInitialized) {
+          await dispatch("INITIALIZE_ENGINE_SPEAKER", {
+            engineId,
+            styleId,
+          });
+        }
+      } finally {
+        commit("SET_SINGER", { engineId, styleId });
+      }
+    },
+  },
+
+  GET_DEFAULT_SCORE: {
+    async action() {
+      const score: Score = {
+        resolution: 480,
+        // テンポの初期値は120で良いでしょうか？
+        tempos: [{ position: 0, tempo: 120 }],
+        timeSignatures: [{ position: 0, beats: 4, beatType: 4 }],
+        notes: [],
+      };
+      return score;
+    },
+  },
+
+  SET_SCORE: {
+    mutation(state, { score }: { score: Score }) {
+      state.score = score;
+    },
+    async action({ commit }, { score }: { score: Score }) {
+      commit("SET_SCORE", { score });
     },
   },
 });
