3023a71fbc38965db4d3324232903ad163dbe536
sabonerune <102559104+sabonerune@users.noreply.github.com>
2022-12-13 07:40:34 +0900

    FIX: lint

--- src/components/CharacterButton.vue
+++ src/components/CharacterButton.vue
@@ -29,13 +29,17 @@
           v-if="selectedStyleInfo == undefined"
           class="row no-wrap items-center"
         >
-          <span class="text-warning vertical-middle">有効なスタイルが選択されていません</span>
+          <span class="text-warning vertical-middle"
+            >有効なスタイルが選択されていません</span
+          >
         </q-item>
         <q-item
           v-if="characterInfos.length === 0"
           class="row no-wrap items-center"
         >
-          <span class="text-warning vertical-middle">選択可能なスタイルがありません</span>
+          <span class="text-warning vertical-middle"
+            >選択可能なスタイルがありません</span
+          >
         </q-item>
         <q-item v-if="emptiable" class="q-pa-none">
           <q-btn
