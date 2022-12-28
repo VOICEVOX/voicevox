8d8467e713cbb0db8871a481b57110cf519c44ac
wappon_28_dev <wappon_28_dev@ruru.be>
2022-12-28 16:09:28 +0900

    Add: 許可しないライセンス群の追加

--- build/generateLicenses.js
+++ build/generateLicenses.js
@@ -4,49 +4,66 @@ const process = require("process");
 const yargs = require("yargs/yargs");
 const { hideBin } = require("yargs/helpers");
 const argv = yargs(hideBin(process.argv))
-  .option("output_path", {
-    alias: "o",
-    demandOption: true,
-    type: "string"
-  })
-  .help()
-  .parse();
+    .option("output_path", {
+        alias: "o",
+        demandOption: true,
+        type: "string",
+    })
+    .help()
+    .parse();
 
 const { execSync } = require("child_process");
 const fs = require("fs");
 const tmp = require("tmp");
 
 const customFormat = {
-  name: "",
-  version: "",
-  description: "",
-  licenses: "",
-  copyright: "",
-  licenseFile: "none",
-  licenseText: "none",
-  licenseModified: "no"
+    name: "",
+    version: "",
+    description: "",
+    licenses: "",
+    copyright: "",
+    licenseFile: "none",
+    licenseText: "none",
+    licenseModified: "no",
 };
 
 const customFormatFile = tmp.fileSync();
 fs.writeFileSync(customFormatFile.name, JSON.stringify(customFormat));
 
+const disallowLicenses = [
+  "GPL",
+  "GPL-2.0",
+  "GPL-3.0",
+  "AGPL",
+  "LGPL",
+  "LGPL-3.0",
+  "NGPL",
+];
+
 // https://github.com/davglass/license-checker
 // npm install -g license-checker
 const licenseJson = execSync(
-  `license-checker --production --excludePrivatePackages --json --customPath ${customFormatFile.name}`,
+  `license-checker \
+  --production \
+  --excludePrivatePackages \
+  --json \
+  --customPath ${customFormatFile.name} \
+  --failOn '${disallowLicenses.join(";")}'`,
   {
-    encoding: "utf-8"
+    encoding: "utf-8",
   }
 );
 
 const checkerLicenses = JSON.parse(licenseJson);
 
-const licenses = Object.entries(checkerLicenses).map(([packageName, license]) => ({
-  name: license.name,
-  version: license.version,
-  license: license.licenses,
-  text: license.licenseText,
-}));
+const licenses = Object.entries(checkerLicenses).map(
+    ([packageName, license]) => ({
+        name: license.name,
+        version: license.version,
+        license: license.licenses,
+        text: license.licenseText,
+    }),
+);
 
 const outputPath = argv.output_path;
 fs.writeFileSync(outputPath, JSON.stringify(licenses));
