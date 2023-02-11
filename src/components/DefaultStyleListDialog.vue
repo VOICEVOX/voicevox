d746e7f89b5cfc353a1f2e107d66836c466daf4b
sevenc-nanashi <sevenc7c@sevenc7c.com>
2023-02-11 23:02:10 +0900

    Fix: デフォルトスタイルのダイアログが閉じれないバグを修正

--- src/components/DefaultStyleListDialog.vue
+++ src/components/DefaultStyleListDialog.vue
@@ -163,17 +163,29 @@ watch([() => props.modelValue], async ([newValue]) => {
       .filter(
         (characterInfo) => characterInfo.metas.styles.length > 1
       ) as CharacterInfo[];
-    selectedStyleIndexes.value = Object.fromEntries([
-      ...store.state.userCharacterOrder.map((speakerUuid) => [speakerUuid, 0]),
-      ...store.state.defaultStyleIds.map((defaultStyle) => [
-        defaultStyle.speakerUuid,
-        characterInfosMap.value[
-          defaultStyle.speakerUuid
-        ]?.metas.styles.findIndex(
-          (style) => style.styleId === defaultStyle.defaultStyleId
-        ) ?? 0,
-      ]),
-    ]);
+    selectedStyleIndexes.value = Object.fromEntries(
+      [
+        ...store.state.userCharacterOrder.map(
+          (speakerUuid) => [speakerUuid, 0] as const
+        ),
+        ...store.state.defaultStyleIds.map(
+          (defaultStyle) =>
+            [
+              defaultStyle.speakerUuid,
+              characterInfosMap.value[
+                defaultStyle.speakerUuid
+              ]?.metas.styles.findIndex(
+                (style) => style.styleId === defaultStyle.defaultStyleId
+              ),
+            ] as const
+        ),
+      ].filter(([speakerUuid, styleIndex]) => {
+        return (
+          characterInfosMap.value[speakerUuid]?.metas.styles[styleIndex] !==
+          undefined
+        );
+      })
+    );
   }
 });
 
