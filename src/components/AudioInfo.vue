4d85ff12f5667053b7a37a9af697e9812d5d9108
k-chop <whelmaze@gmail.com>
2023-02-12 13:11:31 +0900

    新規作成時の候補にデフォルトプリセットが出てこないようにする

--- src/components/AudioInfo.vue
+++ src/components/AudioInfo.vue
@@ -952,6 +952,7 @@ const filterPresetOptionsList: QSelectProps["onFilter"] = (
   doneFn
 ) => {
   const presetNames = presetKeys.value
+    .filter((presetKey) => !presetItems.value[presetKey]?.isDefault)
     .map((presetKey) => presetItems.value[presetKey]?.name)
     .filter((value) => value != undefined);
   doneFn(() => {
@@ -965,6 +966,7 @@ const checkRewritePreset = async () => {
   if (presetList.value.find((e) => e.label === presetName.value)) {
     showsPresetRewriteDialog.value = true;
   } else {
+    // FIXME: デフォルトプリセットと同名のプリセットが作成できないようにする
     const audioPresetKey = await addPreset();
     changePreset(audioPresetKey);
   }
