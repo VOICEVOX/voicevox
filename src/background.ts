c4b17c26d2d9cc1efa1772adb61095b59050e538
raa0121 <raa0121@gmail.com>
2023-02-22 11:09:57 +0900

    fix: config.json の validation 失敗時にわかりやすいログを出すように

--- src/background.ts
+++ src/background.ts
@@ -125,55 +125,63 @@ const electronStoreJsonSchema = zodToJsonSchema(electronStoreSchema);
 if (!("properties" in electronStoreJsonSchema)) {
   throw new Error("electronStoreJsonSchema must be object");
 }
-const store = new Store<ElectronStoreType>({
-  schema: electronStoreJsonSchema.properties as Schema<ElectronStoreType>,
-  migrations: {
-    ">=0.13": (store) => {
-      // acceptTems -> acceptTerms
-      const prevIdentifier = "acceptTems";
-      const prevValue = store.get(prevIdentifier, undefined) as
-        | AcceptTermsStatus
-        | undefined;
-      if (prevValue) {
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        store.delete(prevIdentifier as any);
-        store.set("acceptTerms", prevValue);
-      }
-    },
-    ">=0.14": (store) => {
-      // FIXME: できるならEngineManagerからEnginIDを取得したい
-      if (process.env.DEFAULT_ENGINE_INFOS == undefined)
-        throw new Error("DEFAULT_ENGINE_INFOS == undefined");
-      const engineId = EngineId(
-        JSON.parse(process.env.DEFAULT_ENGINE_INFOS)[0].uuid
-      );
-      if (engineId == undefined)
-        throw new Error("DEFAULT_ENGINE_INFOS[0].uuid == undefined");
-      const prevDefaultStyleIds = store.get("defaultStyleIds");
-      store.set(
-        "defaultStyleIds",
-        prevDefaultStyleIds.map((defaultStyle) => ({
-          engineId,
-          speakerUuid: defaultStyle.speakerUuid,
-          defaultStyleId: defaultStyle.defaultStyleId,
-        }))
-      );
+let store: Store<ElectronStoreType>;
+try {
+  store = new Store<ElectronStoreType>({
+    schema: electronStoreJsonSchema.properties as Schema<ElectronStoreType>,
+    migrations: {
+      ">=0.13": (store) => {
+        // acceptTems -> acceptTerms
+        const prevIdentifier = "acceptTems";
+        const prevValue = store.get(prevIdentifier, undefined) as
+          | AcceptTermsStatus
+          | undefined;
+        if (prevValue) {
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          store.delete(prevIdentifier as any);
+          store.set("acceptTerms", prevValue);
+        }
+      },
+      ">=0.14": (store) => {
+        // FIXME: できるならEngineManagerからEnginIDを取得したい
+        if (process.env.DEFAULT_ENGINE_INFOS == undefined)
+          throw new Error("DEFAULT_ENGINE_INFOS == undefined");
+        const engineId = EngineId(
+          JSON.parse(process.env.DEFAULT_ENGINE_INFOS)[0].uuid
+        );
+        if (engineId == undefined)
+          throw new Error("DEFAULT_ENGINE_INFOS[0].uuid == undefined");
+        const prevDefaultStyleIds = store.get("defaultStyleIds");
+        store.set(
+          "defaultStyleIds",
+          prevDefaultStyleIds.map((defaultStyle) => ({
+            engineId,
+            speakerUuid: defaultStyle.speakerUuid,
+            defaultStyleId: defaultStyle.defaultStyleId,
+          }))
+        );
 
-      const outputSamplingRate: number =
+        const outputSamplingRate: number =
+          // @ts-expect-error 削除されたパラメータ。
+          store.get("savingSetting").outputSamplingRate;
+        store.set(`engineSettings.${engineId}`, {
+          useGpu: store.get("useGpu"),
+          outputSamplingRate:
+            outputSamplingRate === 24000 ? "engineDefault" : outputSamplingRate,
+        });
         // @ts-expect-error 削除されたパラメータ。
-        store.get("savingSetting").outputSamplingRate;
-      store.set(`engineSettings.${engineId}`, {
-        useGpu: store.get("useGpu"),
-        outputSamplingRate:
-          outputSamplingRate === 24000 ? "engineDefault" : outputSamplingRate,
-      });
-      // @ts-expect-error 削除されたパラメータ。
-      store.delete("savingSetting.outputSamplingRate");
-      // @ts-expect-error 削除されたパラメータ。
-      store.delete("useGpu");
+        store.delete("savingSetting.outputSamplingRate");
+        // @ts-expect-error 削除されたパラメータ。
+        store.delete("useGpu");
+      },
     },
-  },
-});
+  });
+} catch (e) {
+  log.error(
+    "config ロードエラーです。config.json を消してみてください(設定がすべてリセットされます)"
+  );
+  log.error(e);
+}
 
 // engine
 const vvppEngineDir = path.join(app.getPath("userData"), "vvpp-engines");
