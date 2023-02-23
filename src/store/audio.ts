5da06ad1c7cdfc0f1dcd332354e0e19dca128fc4
k-chop <whelmaze@gmail.com>
2023-02-23 15:17:14 +0900

    空文字かもしれないので既存の処理に合わせておく

--- src/store/audio.ts
+++ src/store/audio.ts
@@ -389,7 +389,7 @@ export const audioStore = createPartialStore<AudioStoreTypes>({
       );
       if (style === undefined) throw new Error("assert style !== undefined");
 
-      const styleName = style.styleName ?? "ノーマル";
+      const styleName = style.styleName || "ノーマル";
       return `${characterInfo.metas.speakerName} (${styleName})`;
     },
   },
