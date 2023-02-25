0fffb291d3a0e6d24db229e6780a4ef5479eaba8
sevenc-nanashi <sevenc7c@sevenc7c.com>
2023-02-26 00:27:08 +0900

    Code: ダミー画像を変更

--- tests/unit/dummyImage.ts
+++ tests/unit/dummyImage.ts
@@ -1,4 +1,4 @@
 // https://gist.github.com/ondrek/7413434?permalink_comment_id=3914686#gistcomment-3914686
-const dummyImage = `data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAIAQMAAAD+wSzIAAAABlBMVEX///+/v7+jQ3Y5AAAADklEQVQI12P4AIX8EAgALgAD/aNpbtEAAAAASUVORK5CYII`;
+const dummyImage = "data:image/png;base64,DUMMY_IMAGE";
 
 export default dummyImage;
