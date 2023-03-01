cb7203df19b407ca9c7d086ea02cf7748cff2ee4
sabonerune <102559104+sabonerune@users.noreply.github.com>
2023-03-01 20:38:50 +0900

    FIX: 一部の型エラーの修正

--- src/components/EngineManageDialog.vue
+++ src/components/EngineManageDialog.vue
@@ -153,7 +153,11 @@
                   </template>
                   <template v-slot:error>
                     {{
-                      getEngineDirValidationMessage(newEngineDirValidationState)
+                      newEngineDirValidationState
+                        ? getEngineDirValidationMessage(
+                            newEngineDirValidationState
+                          )
+                        : undefined
                     }}
                   </template>
                 </q-input>
@@ -168,7 +172,7 @@
                   dense
                   readonly
                   :error="
-                    newEngineDirValidationState &&
+                    newEngineDirValidationState != undefined &&
                     newEngineDirValidationState !== 'ok'
                   "
                   placeholder="エンジンフォルダの場所"
@@ -190,7 +194,11 @@
                   </template>
                   <template v-slot:error>
                     {{
-                      getEngineDirValidationMessage(newEngineDirValidationState)
+                      newEngineDirValidationState
+                        ? getEngineDirValidationMessage(
+                            newEngineDirValidationState
+                          )
+                        : undefined
                     }}
                   </template>
                 </q-input>
