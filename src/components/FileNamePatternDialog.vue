bf48e8cd310824982886692bc014cc2c974498b3
Nanashi <sevenc7c@sevenc7c.com>
2023-03-05 21:52:01 +0900

    script setup：残り全部を移行 (#1241)
    Migrate: 残り全部を移行

--- src/components/FileNamePatternDialog.vue
+++ src/components/FileNamePatternDialog.vue
@@ -1,6 +1,6 @@
 <template>
   <q-dialog
-    :model-value="openDialog"
+    :model-value="props.openDialog"
     @update:model-value="updateOpenDialog"
     @before-show="initializeInput"
   >
@@ -75,8 +75,8 @@
   </q-dialog>
 </template>
 
-<script lang="ts">
-import { defineComponent, computed, ref, nextTick } from "vue";
+<script setup lang="ts">
+import { computed, ref, nextTick } from "vue";
 import { QInput } from "quasar";
 import { useStore } from "@/store";
 import {
@@ -86,150 +86,124 @@ import {
   sanitizeFileName,
 } from "@/store/utility";
 
-export default defineComponent({
-  name: "FileNamePatternDialog",
+const props =
+  defineProps<{
+    openDialog: boolean;
+  }>();
 
-  props: {
-    openDialog: Boolean,
-  },
+const emit =
+  defineEmits<{
+    (e: "update:openDialog", val: boolean): void;
+  }>();
 
-  emits: ["update:openDialog"],
+const updateOpenDialog = (isOpen: boolean) => emit("update:openDialog", isOpen);
 
-  setup(props, context) {
-    const updateOpenDialog = (isOpen: boolean) =>
-      context.emit("update:openDialog", isOpen);
+const store = useStore();
+const patternInput = ref<QInput>();
+const maxLength = 128;
+const tagStrings = Object.values(replaceTagIdToTagString);
 
-    const store = useStore();
-    const patternInput = ref<QInput>();
-    const maxLength = 128;
-    const tagStrings = Object.values(replaceTagIdToTagString);
+const savingSetting = computed(() => store.state.savingSetting);
 
-    const savingSetting = computed(() => store.state.savingSetting);
+const currentFileNamePattern = ref(savingSetting.value.fileNamePattern);
+const sanitizedFileNamePattern = computed(() =>
+  sanitizeFileName(currentFileNamePattern.value)
+);
 
-    const currentFileNamePattern = ref(savingSetting.value.fileNamePattern);
-    const sanitizedFileNamePattern = computed(() =>
-      sanitizeFileName(currentFileNamePattern.value)
-    );
+const hasInvalidChar = computed(
+  () => currentFileNamePattern.value !== sanitizedFileNamePattern.value
+);
+const hasNotIndexTagString = computed(
+  () => !currentFileNamePattern.value.includes(replaceTagIdToTagString["index"])
+);
+const invalidChar = computed(() => {
+  if (!hasInvalidChar.value) return "";
 
-    const hasInvalidChar = computed(
-      () => currentFileNamePattern.value !== sanitizedFileNamePattern.value
-    );
-    const hasNotIndexTagString = computed(
-      () =>
-        !currentFileNamePattern.value.includes(replaceTagIdToTagString["index"])
-    );
-    const invalidChar = computed(() => {
-      if (!hasInvalidChar.value) return "";
+  const a = currentFileNamePattern.value;
+  const b = sanitizedFileNamePattern.value;
 
-      const a = currentFileNamePattern.value;
-      const b = sanitizedFileNamePattern.value;
+  let diffAt = "";
+  for (let i = 0; i < a.length; i++) {
+    if (b[i] !== a[i]) {
+      diffAt = a[i];
+      break;
+    }
+  }
 
-      let diffAt = "";
-      for (let i = 0; i < a.length; i++) {
-        if (b[i] !== a[i]) {
-          diffAt = a[i];
-          break;
-        }
-      }
-
-      return diffAt;
-    });
-    const errorMessage = computed(() => {
-      if (currentFileNamePattern.value.length === 0) {
-        return "何か入力してください";
-      }
-
-      const result: string[] = [];
-      if (invalidChar.value !== "") {
-        result.push(
-          `使用できない文字が含まれています：「${invalidChar.value}」`
-        );
-      }
-      if (previewFileName.value.includes("$")) {
-        result.push(`不正なタグが存在するか、$が単体で含まれています`);
-      }
-      if (hasNotIndexTagString.value) {
-        result.push(`$${replaceTagIdToTagString["index"]}$は必須です`);
-      }
-      return result.join(", ");
-    });
-    const hasError = computed(() => errorMessage.value !== "");
-
-    const previewFileName = computed(() =>
-      buildFileNameFromRawData(currentFileNamePattern.value + ".wav")
-    );
-
-    const removeExtension = (str: string) => {
-      return str.replace(/\.wav$/, "");
-    };
-
-    const initializeInput = () => {
-      const pattern = savingSetting.value.fileNamePattern;
-      currentFileNamePattern.value = removeExtension(pattern);
-
-      if (currentFileNamePattern.value.length === 0) {
-        currentFileNamePattern.value = removeExtension(
-          DEFAULT_FILE_NAME_TEMPLATE
-        );
-      }
-    };
-    const resetToDefault = () => {
-      currentFileNamePattern.value = removeExtension(
-        DEFAULT_FILE_NAME_TEMPLATE
-      );
-      patternInput.value?.focus();
-    };
-
-    const insertTagToCurrentPosition = (tag: string) => {
-      const elem = patternInput.value?.getNativeElement() as HTMLInputElement;
-      if (elem) {
-        const text = elem.value;
-
-        if (text.length + tag.length > maxLength) {
-          return;
-        }
-
-        const from = elem.selectionStart ?? 0;
-        const to = elem.selectionEnd ?? 0;
-        const newText = text.substring(0, from) + tag + text.substring(to);
-        currentFileNamePattern.value = newText;
-
-        // キャレットの位置を挿入した後の位置にずらす
-        nextTick(() => {
-          elem.selectionStart = from + tag.length;
-          elem.selectionEnd = from + tag.length;
-          elem.focus();
-        });
-      }
-    };
-
-    const submit = async () => {
-      await store.dispatch("SET_SAVING_SETTING", {
-        data: {
-          ...savingSetting.value,
-          fileNamePattern: currentFileNamePattern.value + ".wav",
-        },
-      });
-      updateOpenDialog(false);
-    };
-
-    return {
-      patternInput,
-      tagStrings,
-      maxLength,
-      updateOpenDialog,
-      resetToDefault,
-      insertTagToCurrentPosition,
-      submit,
-      initializeInput,
-      savingSetting,
-      currentFileNamePattern,
-      errorMessage,
-      hasError,
-      previewFileName,
-    };
-  },
+  return diffAt;
 });
+const errorMessage = computed(() => {
+  if (currentFileNamePattern.value.length === 0) {
+    return "何か入力してください";
+  }
+
+  const result: string[] = [];
+  if (invalidChar.value !== "") {
+    result.push(`使用できない文字が含まれています：「${invalidChar.value}」`);
+  }
+  if (previewFileName.value.includes("$")) {
+    result.push(`不正なタグが存在するか、$が単体で含まれています`);
+  }
+  if (hasNotIndexTagString.value) {
+    result.push(`$${replaceTagIdToTagString["index"]}$は必須です`);
+  }
+  return result.join(", ");
+});
+const hasError = computed(() => errorMessage.value !== "");
+
+const previewFileName = computed(() =>
+  buildFileNameFromRawData(currentFileNamePattern.value + ".wav")
+);
+
+const removeExtension = (str: string) => {
+  return str.replace(/\.wav$/, "");
+};
+
+const initializeInput = () => {
+  const pattern = savingSetting.value.fileNamePattern;
+  currentFileNamePattern.value = removeExtension(pattern);
+
+  if (currentFileNamePattern.value.length === 0) {
+    currentFileNamePattern.value = removeExtension(DEFAULT_FILE_NAME_TEMPLATE);
+  }
+};
+const resetToDefault = () => {
+  currentFileNamePattern.value = removeExtension(DEFAULT_FILE_NAME_TEMPLATE);
+  patternInput.value?.focus();
+};
+
+const insertTagToCurrentPosition = (tag: string) => {
+  const elem = patternInput.value?.getNativeElement() as HTMLInputElement;
+  if (elem) {
+    const text = elem.value;
+
+    if (text.length + tag.length > maxLength) {
+      return;
+    }
+
+    const from = elem.selectionStart ?? 0;
+    const to = elem.selectionEnd ?? 0;
+    const newText = text.substring(0, from) + tag + text.substring(to);
+    currentFileNamePattern.value = newText;
+
+    // キャレットの位置を挿入した後の位置にずらす
+    nextTick(() => {
+      elem.selectionStart = from + tag.length;
+      elem.selectionEnd = from + tag.length;
+      elem.focus();
+    });
+  }
+};
+
+const submit = async () => {
+  await store.dispatch("SET_SAVING_SETTING", {
+    data: {
+      ...savingSetting.value,
+      fileNamePattern: currentFileNamePattern.value + ".wav",
+    },
+  });
+  updateOpenDialog(false);
+};
 </script>
 
 <style scoped lang="scss">
