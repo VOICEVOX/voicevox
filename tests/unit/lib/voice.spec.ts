ad3847d22d9bc051019cf9f55543154b0747c136
k-chop <whelmaze@gmail.com>
2023-03-09 00:03:09 +0900

    VoiceIdのstringが変わってないテスト

--- tests/unit/lib/voice.spec.ts
+++ tests/unit/lib/voice.spec.ts
@@ -12,4 +12,17 @@ describe("voiceIdToVoice", () => {
 
     expect(voiceIdToVoice(VoiceId(voice))).toEqual(voice);
   });
+
+  it("VoiceIdを再変換しても文字列表現は変わらない", () => {
+    const voice: Voice = {
+      engineId: EngineId("57959e44-0c80-4c21-984d-7ba58224e834"),
+      speakerId: SpeakerId("acb2b256-ead4-43b4-b738-84c0f0feb36d"),
+      styleId: StyleId(0),
+    };
+    const voiceId = VoiceId(voice);
+    const voice2 = voiceIdToVoice(voiceId);
+    const voiceId2 = VoiceId(voice2);
+
+    expect(voiceId2).toEqual(voiceId);
+  });
 });
