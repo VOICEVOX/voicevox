8f9a830e0c0df7577b07192fc65c3723789d183b
k-chop <whelmaze@gmail.com>
2023-03-09 09:58:26 +0900

    移した

--- tests/unit/lib/voice.spec.ts
+++ tests/unit/lib/voice.spec.ts
@@ -1,6 +1,14 @@
 import { describe, expect, it } from "vitest";
-import { voiceIdToVoice } from "@/lib/voice";
 import { EngineId, SpeakerId, StyleId, Voice, VoiceId } from "@/type/preload";
+
+export const voiceIdToVoice = (voiceId: VoiceId): Voice => {
+  const [engineId, speakerId, styleId] = voiceId.split(":");
+  return {
+    engineId: EngineId(engineId),
+    speakerId: SpeakerId(speakerId),
+    styleId: StyleId(Number(styleId)),
+  };
+};
 
 describe("voiceIdToVoice", () => {
   it("VoiceとVoiceIdは互いに変換可能", () => {
