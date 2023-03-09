4d4f7e5a383cef2853461943c8151947aa154d31
kchop <whelmaze@gmail.com>
2023-03-10 08:53:13 +0900

    Update tests/unit/lib/voice.spec.ts
    Co-authored-by: Hiroshiba <hihokaruta@gmail.com>

--- tests/unit/lib/voice.spec.ts
+++ tests/unit/lib/voice.spec.ts
@@ -10,6 +10,18 @@ export const voiceIdToVoice = (voiceId: VoiceId): Voice => {
   };
 };
 
+describe("voiceToVoiceId", () => {
+  it("Voice→VoiceId変換処理が変わっていない", () => {
+    const voice: Voice = {
+      engineId: EngineId("57959e44-0c80-4c21-984d-7ba58224e834"),
+      speakerId: SpeakerId("acb2b256-ead4-43b4-b738-84c0f0feb36d"),
+      styleId: StyleId(0),
+    };
+
+    expect(VoiceId(voice)).toEqual(`${voice.engineId}:${voice.speakerId}:${voice.styleId}`);
+  });
+}
+
 describe("voiceIdToVoice", () => {
   it("VoiceとVoiceIdは互いに変換可能", () => {
     const voice: Voice = {
