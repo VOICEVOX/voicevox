093efad014a5394d512ec3e77430d6a9126f5ae4
sevenc-nanashi <sevenc7c@sevenc7c.com>
2023-04-03 11:11:27 +0900

    Fix: DevToolsを開かないように

--- src/background.ts
+++ src/background.ts
@@ -477,7 +477,7 @@ async function createWindow() {
     });
     win.loadURL("app://./index.html" + parameter);
   }
-  if (isDevelopment) win.webContents.openDevTools();
+  if (isDevelopment && !isTest) win.webContents.openDevTools();
 
   win.on("maximize", () => win.webContents.send("DETECT_MAXIMIZED"));
   win.on("unmaximize", () => win.webContents.send("DETECT_UNMAXIMIZED"));
