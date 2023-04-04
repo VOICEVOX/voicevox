15b558be848f03e0d61970d4cdec2b6eb9c7d11f
yamachu <yamachu.dev@gmail.com>
2023-04-04 22:53:03 +0900

    onReceivedIPCMsg の event や戻り値を使用していないため、frontendからelectronの型を減らすべく関連する型を削除

--- src/type/preload.ts
+++ src/type/preload.ts
@@ -1,4 +1,4 @@
-import { IpcRenderer, IpcRendererEvent, nativeTheme } from "electron";
+import { nativeTheme } from "electron";
 import { z } from "zod";
 import { IpcSOData } from "./ipc";
 
@@ -187,8 +187,8 @@ export interface Sandbox {
   isMaximizedWindow(): Promise<boolean>;
   onReceivedIPCMsg<T extends keyof IpcSOData>(
     channel: T,
-    listener: (event: IpcRendererEvent, ...args: IpcSOData[T]["args"]) => void
-  ): IpcRenderer;
+    listener: (event: unknown, ...args: IpcSOData[T]["args"]) => void
+  ): void;
   closeWindow(): void;
   minimizeWindow(): void;
   maximizeWindow(): void;
