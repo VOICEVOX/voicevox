ff2000b5736ece4b307f9ed5ebf0132c5837707a
finict <finict@proton.me>
2023-04-26 07:40:16 +0900

    fix dictionary spacing

--- src/components/DictionaryManageDialog.vue
+++ src/components/DictionaryManageDialog.vue
@@ -378,7 +378,7 @@ const accentPhraseTable = ref<HTMLElement>();
 
 const convertHankakuToZenkaku = (text: string) => {
   // " "などの目に見えない文字をまとめて全角スペース(0x3000)に置き換える
-  text = text.replace(/\p{Z}/u, () => String.fromCharCode(0x3000));
+  text = text.replace(/\p{Z}/gu, () => String.fromCharCode(0x3000));
 
   // "!"から"~"までの範囲の文字(数字やアルファベット)を全角に置き換える
   return text.replace(/[\u0021-\u007e]/g, (s) => {
