bb81c5d2b3c46a9c90e8db229a8ef048010b7bb0
yamachu <yamachu.dev@gmail.com>
2023-06-13 01:07:48 +0900

    resourcesを取得する処理の実装

--- src/browser/background.ts
+++ src/browser/background.ts
@@ -1,4 +1,14 @@
-import { getAppInfosImpl } from "./backgroundImpl";
+import {
+  getAppInfosImpl,
+  getContactTextImpl,
+  getHowToUseTextImpl,
+  getOssCommunityInfosImpl,
+  getOssLicensesImpl,
+  getPolicyTextImpl,
+  getPrivacyPolicyTextImpl,
+  getQAndATextImpl,
+  getUpdateInfosImpl,
+} from "./backgroundImpl";
 import type { MainToWorkerMessage } from "./type";
 import type { IpcIHData } from "@/type/ipc";
 
@@ -16,7 +26,85 @@ onmessage = (e: MessageEvent<MainToWorkerMessage>) => {
       return getAppInfosImpl(e.data.args).then((v) =>
         typedPostMessage<typeof e.data.type>(v)
       );
-    default:
+    case "GET_TEMP_DIR":
+      console.dir(e.data);
+      postMessage({ type: e.data.type, return: [], eventId: e.data.eventId });
+      break;
+    case "GET_HOW_TO_USE_TEXT":
+      return getHowToUseTextImpl(e.data.args).then((v) =>
+        typedPostMessage<typeof e.data.type>(v)
+      );
+    case "GET_POLICY_TEXT":
+      return getPolicyTextImpl(e.data.args).then((v) =>
+        typedPostMessage<typeof e.data.type>(v)
+      );
+    case "GET_OSS_LICENSES":
+      return getOssLicensesImpl(e.data.args).then((v) =>
+        typedPostMessage<typeof e.data.type>(v)
+      );
+    case "GET_UPDATE_INFOS":
+      return getUpdateInfosImpl(e.data.args).then((v) =>
+        typedPostMessage<typeof e.data.type>(v)
+      );
+    case "GET_OSS_COMMUNITY_INFOS":
+      return getOssCommunityInfosImpl(e.data.args).then((v) =>
+        typedPostMessage<typeof e.data.type>(v)
+      );
+    case "GET_CONTACT_TEXT":
+      return getContactTextImpl(e.data.args).then((v) =>
+        typedPostMessage<typeof e.data.type>(v)
+      );
+    case "GET_Q_AND_A_TEXT":
+      return getQAndATextImpl(e.data.args).then((v) =>
+        typedPostMessage<typeof e.data.type>(v)
+      );
+    case "GET_PRIVACY_POLICY_TEXT":
+      return getPrivacyPolicyTextImpl(e.data.args).then((v) =>
+        typedPostMessage<typeof e.data.type>(v)
+      );
+    case "GET_ALT_PORT_INFOS":
+    case "SHOW_AUDIO_SAVE_DIALOG":
+    case "SHOW_TEXT_SAVE_DIALOG":
+    case "SHOW_VVPP_OPEN_DIALOG":
+    case "SHOW_OPEN_DIRECTORY_DIALOG":
+    case "SHOW_IMPORT_FILE_DIALOG":
+    case "SHOW_PROJECT_SAVE_DIALOG":
+    case "SHOW_PROJECT_LOAD_DIALOG":
+    case "SHOW_MESSAGE_DIALOG":
+    case "SHOW_QUESTION_DIALOG":
+    case "SHOW_WARNING_DIALOG":
+    case "SHOW_ERROR_DIALOG":
+    case "OPEN_TEXT_EDIT_CONTEXT_MENU":
+    case "IS_AVAILABLE_GPU_MODE":
+    case "IS_MAXIMIZED_WINDOW":
+    case "CLOSE_WINDOW":
+    case "MINIMIZE_WINDOW":
+    case "MAXIMIZE_WINDOW":
+    case "LOG_ERROR":
+    case "LOG_WARN":
+    case "LOG_INFO":
+    case "ENGINE_INFOS":
+    case "RESTART_ENGINE_ALL":
+    case "RESTART_ENGINE":
+    case "OPEN_ENGINE_DIRECTORY":
+    case "CHECK_FILE_EXISTS":
+    case "CHANGE_PIN_WINDOW":
+    case "HOTKEY_SETTINGS":
+    case "GET_DEFAULT_HOTKEY_SETTINGS":
+    case "GET_DEFAULT_TOOLBAR_SETTING":
+    case "THEME":
+    case "ON_VUEX_READY":
+    case "GET_SETTING":
+    case "SET_SETTING":
+    case "SET_ENGINE_SETTING":
+    case "SET_NATIVE_THEME":
+    case "INSTALL_VVPP_ENGINE":
+    case "UNINSTALL_VVPP_ENGINE":
+    case "VALIDATE_ENGINE_DIR":
+    case "RESTART_APP":
+    case "JOIN_PATH":
+    case "WRITE_FILE":
+    case "READ_FILE":
       console.dir(e.data);
       postMessage({ type: e.data.type, return: [], eventId: e.data.eventId });
       break;
--- src/browser/backgroundImpl.ts
+++ src/browser/backgroundImpl.ts
@@ -1,4 +1,14 @@
 import type { IpcIHData } from "@/type/ipc";
+import {
+  ContactTextFileName,
+  HowToUseTextFileName,
+  OssCommunityInfosFileName,
+  OssLicensesJsonFileName,
+  PolicyTextFileName,
+  PrivacyPolicyTextFileName,
+  QAndATextFileName,
+  UpdateInfosJsonFileName,
+} from "@/type/staticResources";
 
 type SandboxImpl = {
   [K in keyof IpcIHData]: (
@@ -14,4 +24,41 @@ export const getAppInfosImpl: SandboxImpl["GET_APP_INFOS"] = () => {
   };
   /* eslint-enable @typescript-eslint/no-non-null-assertion */
   return Promise.resolve(appInfo);
+};
+
+// TODO: base pathを設定できるようにするか、ビルド時埋め込みにする
+const toStaticPath = (fileName: string) => `/${fileName}`;
+
+export const getHowToUseTextImpl: SandboxImpl["GET_HOW_TO_USE_TEXT"] = () => {
+  return fetch(toStaticPath(HowToUseTextFileName)).then((v) => v.text());
+};
+
+export const getPolicyTextImpl: SandboxImpl["GET_POLICY_TEXT"] = () => {
+  return fetch(toStaticPath(PolicyTextFileName)).then((v) => v.text());
+};
+
+export const getOssCommunityInfosImpl: SandboxImpl["GET_OSS_COMMUNITY_INFOS"] =
+  () => {
+    return fetch(toStaticPath(OssCommunityInfosFileName)).then((v) => v.text());
+  };
+
+export const getContactTextImpl: SandboxImpl["GET_CONTACT_TEXT"] = () => {
+  return fetch(toStaticPath(ContactTextFileName)).then((v) => v.text());
+};
+
+export const getQAndATextImpl: SandboxImpl["GET_Q_AND_A_TEXT"] = () => {
+  return fetch(toStaticPath(QAndATextFileName)).then((v) => v.text());
+};
+
+export const getPrivacyPolicyTextImpl: SandboxImpl["GET_PRIVACY_POLICY_TEXT"] =
+  () => {
+    return fetch(toStaticPath(PrivacyPolicyTextFileName)).then((v) => v.text());
+  };
+
+export const getOssLicensesImpl: SandboxImpl["GET_OSS_LICENSES"] = () => {
+  return fetch(toStaticPath(OssLicensesJsonFileName)).then((v) => v.json());
+};
+
+export const getUpdateInfosImpl: SandboxImpl["GET_UPDATE_INFOS"] = () => {
+  return fetch(toStaticPath(UpdateInfosJsonFileName)).then((v) => v.json());
 };
