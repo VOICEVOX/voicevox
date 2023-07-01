f60102c1da013b4b9cdc984a4a59346807546b98
Romot <romot@romotmbp16.local>
2023-07-01 16:45:50 +0900

    #1114 spec

--- tests/unit/store/Vuex.spec.ts
+++ tests/unit/store/Vuex.spec.ts
@@ -123,7 +123,7 @@ describe("store/vuex.js test", () => {
         sequencerScrollX: 0,
         sequencerScrollY: 60,
         sequencerSnapSize: 120,
-        selectedNotes: new Set(),
+        selectedNoteIds: [],
         nowPlaying: false,
         volume: 0,
         leftLocatorPosition: 0,
