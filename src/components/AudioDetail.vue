173b9af8b9895996a60bf4823d25fd88001d2013
tiramisu_oTATo <xxx.partyo@gmail.com>
2023-07-03 22:57:53 +0900

    UIとドキュメントのMac版対応

--- src/components/AudioDetail.vue
+++ src/components/AudioDetail.vue
@@ -55,8 +55,10 @@
             スライダーを微調整できます。
           </p>
           ホイール: ±0.1<br />
-          Ctrl + ホイール: ±0.01<br />
-          Alt + ホイール: 一括調整
+          <span v-if="_isMac">Command</span><span v-else>Ctrl</span> + ホイール:
+          ±0.01<br />
+          <span v-if="_isMac">Option</span><span v-else>Alt</span> + ホイール:
+          一括調整
         </tool-tip>
         <div
           v-for="(accentPhrase, accentPhraseIndex) in accentPhrases"
@@ -277,6 +279,7 @@ import {
   AudioKey,
   HotkeyAction,
   HotkeyReturnType,
+  isMac,
   MoraDataType,
 } from "@/type/preload";
 import { setHotkeyFunctions } from "@/store/setting";
@@ -289,6 +292,8 @@ const props =
 
 const store = useStore();
 const $q = useQuasar();
+
+const _isMac = computed(() => isMac);
 
 const supportedFeatures = computed(
   () =>
