6554f5c74f4b8e5fb0c69ae6718d4504b608fccd
sevenc-nanashi <sevenc7c@sevenc7c.com>
2023-07-05 19:20:41 +0900

    Add: ResultにE引数を追加

--- src/type/result.ts
+++ src/type/result.ts
@@ -1,20 +1,56 @@
-export type Result<T> = SuccessResult<T> | FailureResult;
+export type Result<T, E extends string = string> =
+  | SuccessResult<T>
+  | FailureResult<E>;
 export const success = <T>(value: T): SuccessResult<T> => ({ ok: true, value });
 export type SuccessResult<T> = { ok: true; value: T };
 type Failure = {
-  (error: Error): FailureResult;
-  (code: string | undefined, error: Error): FailureResult;
+  (code: undefined, error: Error): FailureResult<typeof unknownCode>;
+  (error: Error): FailureResult<typeof unknownCode>;
+  <C extends string>(code: C, error: Error): FailureResult<C>;
 };
-export const failure: Failure = (codeOrError, error?) => {
-  const code = typeof codeOrError === "object" ? undefined : codeOrError;
-  const err = typeof codeOrError === "object" ? codeOrError : error;
+const unknownCode = "UNKNOWN" as const;
+export const failure: Failure = <C extends string>(
+  codeOrError: C | undefined | Error,
+  error?: Error
+) => {
+  switch (typeof codeOrError) {
+    case "undefined": {
+      if (typeof error === "undefined") {
+        throw new Error("Error must be specified");
+      }
 
-  return { ok: false, code: code || "UNKNOWN", error: err as Error };
+      return {
+        ok: false as const,
+        code: unknownCode,
+        error: error,
+      } as FailureResult<typeof unknownCode>;
+    }
+    case "string": {
+      if (error === undefined) {
+        throw new Error("Error must be specified");
+      }
+
+      return { ok: false as const, code: codeOrError, error };
+    }
+    case "object": {
+      if (codeOrError === null) {
+        throw new Error("Error must be specified");
+      }
+      return {
+        ok: false as const,
+        code: unknownCode,
+        error: codeOrError,
+      } as FailureResult<typeof unknownCode>;
+    }
+    default: {
+      throw new Error("Error must be specified");
+    }
+  }
 };
-export class ResultError extends Error {
-  public code: string;
+export class ResultError<E extends string = string> extends Error {
+  public code: E;
 
-  constructor(public readonly result: FailureResult) {
+  constructor(public readonly result: FailureResult<E>) {
     super(`${result.code}: ${result.error.message}`, { cause: result.error });
     this.code = result.code;
   }
@@ -30,8 +66,8 @@ export const getValueOrThrow = <T>(result: Result<T>): T | never => {
     throw new ResultError(result);
   }
 };
-export type FailureResult = {
+export type FailureResult<E> = {
   ok: false;
-  code: string;
+  code: E;
   error: Error;
 };
