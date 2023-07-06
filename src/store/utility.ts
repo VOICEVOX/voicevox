7942e9c7fdb1ec92ba29ac673540e63ca9f72b0b
weweweok <combination.kyoukyou@gmail.com>
2023-07-07 06:32:18 +0900

    関数名と関数の定義場所の修正

--- src/store/utility.ts
+++ src/store/utility.ts
@@ -25,6 +25,18 @@ export function sanitizeFileName(fileName: string): string {
   return fileName.replace(sanitizer, "");
 }
 
+export function textExportToText(targettext: string): string {
+  let resolvedText = targettext.replace(/\{.*?\}/g, "");
+  resolvedText = resolvedText.replace(/\[(.*?)\]/g, "$1");
+  return resolvedText;
+}
+
+export function textExportToEngine(targettext: string): string {
+  let resolvedText = targettext.replace(/\[.*?\]/g, "");
+  resolvedText = resolvedText.replace(/\{(.*?)\}/g, "$1");
+  return resolvedText;
+}
+
 export function buildProjectFileName(state: State, extension?: string): string {
   const headItemText = state.audioItems[state.audioKeys[0]].text;
 
