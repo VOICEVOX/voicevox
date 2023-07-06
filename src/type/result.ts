96df41e3ffb7b327a24a8e02a58573c1dd3df3e6
sevenc-nanashi <sevenc7c@sevenc7c.com>
2023-07-06 18:53:09 +0900

    Add: 軽いドキュメントを追加
    Co-Authored-By: Hiroshiba <Hiroshiba@users.noreply.github.com>

--- src/type/result.ts
+++ src/type/result.ts
@@ -1,3 +1,42 @@
+/**
+ * Result型。新しいErrorクラスを作らなくてもエラーハンドリングできる。
+ *
+ * 成功時はsuccess関数を使ってSuccessResultを返し、
+ * エラー時はfailure関数を使ってFailureResultを返せば良い。
+ *
+ * 例：成功時はstringを返し、エラー時はErrorCodeA・ErrorCodeBを返す場合
+ *
+ * // 関数定義
+ * function generateString(): Result<string, "ErrorCodeA" | "ErrorCodeB"> {
+ *   // 成功時
+ *   return success("ok string");
+ *
+ *  // エラー時
+ *   return failure("ErrorCodeA", new Error("error message A"));
+ *   return failure("ErrorCodeB", new Error("error message B"));
+ *   return failure(new Error("other error"));
+ * }
+ *
+ * // 使う側
+ * const result = generateString();
+ * if (result.ok) {
+ *  // 成功時の処理
+ *   console.log(result.value); // -> "ok string"
+ * }
+ * else {
+ *   // エラー時の処理
+ *   console.log(result.code); // -> "ErrorCodeA" | "ErrorCodeB" | undefined
+ *   console.log(result.error); // -> Error
+ * }
+ *
+ * // getValueOrThrowを使えばエラー時にResultErrorをthrowし直す
+ * const value = getValueOrThrow(result);
+ * console.log(value); // -> "ok string"
+ *
+ * // Promise#thenと組み合わせると綺麗に書ける。
+ * const value = await asyncGenerateString().then(getValueOrThrow);
+ * console.log(value); // -> "ok string"
+ */
 export type Result<T, E extends string = string> =
   | SuccessResult<T>
   | FailureResult<E>;
