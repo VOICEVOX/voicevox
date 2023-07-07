f5ad963c3046f00b4eabd8b8e8babb5078f476c0
sabonerune <102559104+sabonerune@users.noreply.github.com>
2023-07-08 02:25:04 +0900

    fix: 他のプロセスが使用中以外の理由でポートが使えない場合に起動できない問題を修正 (#1373)

--- src/background/portManager.ts
+++ src/background/portManager.ts
@@ -1,4 +1,5 @@
 import { execFileSync } from "child_process";
+import { createServer } from "net";
 import log from "electron-log";
 
 const isWindows = process.platform === "win32";
@@ -173,15 +174,45 @@ export async function getProcessNameFromPid(
 }
 
 /**
+ * ポートが割り当て可能かどうか実際にlistenして接続したポート番号を返します。
+ * 0番ポートを指定した場合はランダムな接続可能ポート番号を返します。
+ * @param port 確認するポート番号
+ * @param hostname 確認するホスト名
+ * @returns 割り当て不能だった場合`undefined`を返します。割り当て可能だった場合ポート番号を返します。
+ */
+function findOrCheckPort(
+  port: number,
+  hostname: string
+): Promise<number | undefined> {
+  return new Promise((resolve, reject) => {
+    const server = createServer();
+    server.on("error", () => {
+      server.close();
+      resolve(undefined);
+    });
+    server.on("listening", () => {
+      const address = server.address();
+      server.close();
+      if (address == undefined || typeof address === "string") {
+        reject(new Error(`'address' is null or string: ${address}`));
+        return;
+      }
+      resolve(address.port);
+    });
+    server.listen(port, hostname);
+  });
+}
+
+/**
  * 割り当て可能な他のポートを探します
- *
- * @param hostInfo ホスト情報
+ * @param basePort 元のポート番号
+ * @param hostname 割り当てるホスト名
  * @returns 割り当て可能なポート番号 or `undefined` (割り当て可能なポートが見つからなかったとき)
  */
 export async function findAltPort(
-  hostInfo: HostInfo
+  basePort: number,
+  hostname: string
 ): Promise<number | undefined> {
-  const basePort = hostInfo.port;
   portLog(basePort, `Find another assignable port from ${basePort}...`);
 
   // エンジン指定のポート + 100番までを探索  エフェメラルポートの範囲の最大は超えないようにする
@@ -189,19 +220,27 @@ export async function findAltPort(
 
   for (let altPort = basePort + 1; altPort <= altPortMax; altPort++) {
     portLog(basePort, `Trying whether port ${altPort} is assignable...`);
-    const altPid = await getPidFromPort(
-      {
-        protocol: hostInfo.protocol,
-        hostname: hostInfo.hostname,
-        port: altPort,
-      },
-      true
-    );
-
-    // ポートを既に割り当てられているプロセスidの取得: undefined → ポートが空いている
-    if (altPid == undefined) return altPort;
+    if (await isAssignablePort(altPort, hostname)) {
+      return altPort;
+    }
   }
 
+  // 指定のポート + 100番まで見つからなかった場合ランダムなポートを使用する
   portWarn(basePort, `No alternative port found! ${basePort}...${altPortMax}`);
-  return undefined;
+  const altPort = await findOrCheckPort(0, hostname);
+  if (altPort == undefined) {
+    portWarn(basePort, "No alternative port found!");
+  } else {
+    portLog(altPort, "Assignable");
+  }
+  return altPort;
+}
+
+/**
+ * ポートが割り当て可能か確認します
+ * @param port 確認するポート番号
+ * @param hostname 確認するホスト名
+ */
+export async function isAssignablePort(port: number, hostname: string) {
+  return (await findOrCheckPort(port, hostname)) != undefined;
 }
