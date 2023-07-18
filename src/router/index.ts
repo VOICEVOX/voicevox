7e23cda4e8ca1054873d5d1165ec554b339cee1d
Hiroshiba Kazuyuki <kazuyuki_hiroshiba@dwango.co.jp>
2023-07-18 18:54:05 +0900

    とりあえずdistから表示可能に

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,5 @@
-import {
-  createRouter,
-  createWebHashHistory,
-  createWebHistory,
-  RouteRecordRaw,
-} from "vue-router";
+import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 import EditorHome from "../views/EditorHome.vue";
-
-import { isElectron } from "@/type/preload";
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -17,9 +10,7 @@ const routes: Array<RouteRecordRaw> = [
 ];
 
 const router = createRouter({
-  history: isElectron
-    ? createWebHashHistory(import.meta.env.BASE_URL)
-    : createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes,
 });
 
