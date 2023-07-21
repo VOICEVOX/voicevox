3fa05aeaa68aea23d78ecd4b394fd2fdcd0d2c41
Hiroshiba <hihokaruta@gmail.com>
2023-07-22 00:47:25 +0900

    nemo

--- tests/e2e/example.spec.ts
+++ tests/e2e/example.spec.ts
@@ -39,7 +39,7 @@ test.beforeAll(async () => {
 test("起動したら「利用規約に関するお知らせ」が表示される", async () => {
   const app = await electron.launch({
     args: ["."],
-    timeout: process.env.CI ? 0 : 180000,
+    timeout: process.env.CI ? 0 : 60000,
     env: {
       ...process.env,
       VITE_DEV_SERVER_URL: "http://localhost:5173",
