2c626fc479eabfbd6340b3e164e507d506a47d52
tiramisu_oTATo <xxx.partyo@gmail.com>
2023-07-22 16:03:39 +0900

    コメントアウト修正

--- src/helpers/contextMenu.ts
+++ src/helpers/contextMenu.ts
@@ -2,7 +2,7 @@
  * コンテキストメニューの開閉由来のFocusやBlurの判別に使う。
  * no-focusがついていないメニューを開く場合には機能しないため注意。
  */
-// no-focuがついていないメニューを開く時はrelatedTarget === nullになるため機能しない。
+// no-focusがついていないメニューを開く時はrelatedTarget === nullになるため機能しない。
 export const isRelatedToContextMenu = (event?: FocusEvent) => {
   return event !== undefined && isMenuItemOrChild(event.relatedTarget);
 };
