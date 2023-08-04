dc7772e6dd4bec8c3fa366fbb64e8fa8e5c754c4
Hiroshiba <hihokaruta@gmail.com>
2023-08-04 09:42:30 +0900

    timeoutを全体で30 * 1000に

--- tests/e2e/navigators.ts
+++ tests/e2e/navigators.ts
@@ -4,9 +4,7 @@ import { expect, Page } from "@playwright/test";
  * 初回起動時の確認を完了してメイン画面に移動
  */
 export async function navigateToMain(page: Page) {
-  await expect(page.getByText("利用規約に関するお知らせ")).toBeVisible({
-    timeout: 30 * 1000,
-  });
+  await expect(page.getByText("利用規約に関するお知らせ")).toBeVisible();
   await page.waitForTimeout(100);
   await page.getByRole("button", { name: "同意して使用開始" }).click();
   await page.waitForTimeout(100);
