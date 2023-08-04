effd247bb37ee153e616894882318197db2f447b
Motoki Saito <stmtk13044032@gmail.com>
2023-08-04 20:29:52 +0900

    エンターを押したあとに100ms待つ処理を追加

--- tests/e2e/browser/アクセント分割.spec.ts
+++ tests/e2e/browser/アクセント分割.spec.ts
@@ -13,6 +13,7 @@ test("アクセント分割したらアクセント区画が増える", async ({
   await expect(page.locator(".audio-cell").first()).toBeVisible();
   await page.locator(".audio-cell input").first().fill("こんにちは");
   await page.locator(".audio-cell input").first().press("Enter");
+  await page.waitForTimeout(100);
   expect(await page.locator(".mora-table").count()).toBe(1);
   await (await page.locator(".splitter-cell").all())[1].click();
   await page.waitForTimeout(100);
