8836e216f3dd565984f048c174e0b5386c4fd74e
Motoki saito <stmtk1@users.noreply.github.com>
2023-08-05 11:08:52 +0900

    アクセント分割のe2eテスト追加 (#1479)
    Co-authored-by: Hiroshiba <hihokaruta@gmail.com>

---
+++ tests/e2e/browser/アクセント.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect } from "@playwright/test";
+
+import { navigateToMain } from "../navigators";
+
+test.beforeEach(async ({ page }) => {
+  const BASE_URL = "http://localhost:5173/#/home";
+  await page.setViewportSize({ width: 800, height: 600 });
+  await page.goto(BASE_URL);
+});
+
+test("アクセント分割したらアクセント区間が増える", async ({ page }) => {
+  await navigateToMain(page);
+  await expect(page.locator(".audio-cell").first()).toBeVisible();
+  await page.locator(".audio-cell input").first().fill("こんにちは");
+  await page.locator(".audio-cell input").first().press("Enter");
+  await page.waitForTimeout(100);
+  expect(await page.locator(".mora-table").count()).toBe(1);
+  await (await page.locator(".splitter-cell").all())[1].click();
+  await page.waitForTimeout(100);
+  expect(await page.locator(".mora-table").count()).toBe(2);
+});
