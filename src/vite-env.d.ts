398197e5677376182d339c4bef35b87ae3d82b7d
sabonerune <102559104+sabonerune@users.noreply.github.com>
2023-08-11 12:34:04 +0900

    type: VITE_*環境変数の型付けを追加

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_7Z_BIN_NAME: string;
+  readonly VITE_APP_NAME: string;
+  readonly VITE_GTM_CONTAINER_ID: string;
+  readonly VITE_TARGET: "electron" | "browser";
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
