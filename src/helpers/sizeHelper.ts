15e3d33605506a60fc5c47f008f73d41a2ef7b02
y-chan <y-chan@y-chan.dev>
2023-08-18 20:32:07 +0900

    remove size helper

--- src/helpers/sizeHelper.ts
+++
@@ -1,6 +0,0 @@
-export function bytesToSize(bytes: number): string {
-  const sizes = ["B", "KB", "MB", "GB", "TB"];
-  if (bytes === 0) return "0 B";
-  const i = parseInt(String(Math.floor(Math.log(bytes) / Math.log(1024))));
-  return (bytes / Math.pow(1024, i)).toFixed(2) + " " + sizes[i];
-}
