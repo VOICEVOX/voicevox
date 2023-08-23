dab9eb65f40c2f5f7af729cefba10c0e010fe492
Motoki Saito <stmtk13044032@gmail.com>
2023-08-23 16:20:34 +0900

    処理の共通化、コメントの追加をしました

--- tests/e2e/browser/テキスト追加・削除・入れ替え.spec.ts
+++ tests/e2e/browser/テキスト追加・削除・入れ替え.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from "@playwright/test";
+import { test, expect, Locator, Page } from "@playwright/test";
 
 import { navigateToMain } from "../navigators";
 
@@ -8,66 +8,67 @@ test.beforeEach(async ({ page }) => {
   await page.goto(BASE_URL);
 });
 
+async function fillInput(page: Page, locator: Locator, text: string) {
+  await locator.fill(text);
+  await locator.press("Enter");
+  await page.waitForTimeout(100);
+  await validateInput(locator, text);
+}
+
+async function validateInput(locator: Locator, expectedText: string) {
+  expect(await locator.inputValue()).toBe(expectedText);
+}
+
+async function getCenter(locator: Locator) {
+  const box = (await locator.boundingBox()) || {
+    x: 0,
+    y: 0,
+    width: 0,
+    height: 0,
+  };
+  return { x: box.x + box.width / 2, y: box.y + box.height / 2 };
+}
+
 test("テキストの追加・入れ替え・削除", async ({ page }) => {
+  // デフォルトでaudioCellは一つなのを確認
   await navigateToMain(page);
   await expect(
     page.getByRole("button").filter({ hasText: "add" })
   ).toBeVisible();
   expect(await page.locator(".audio-cell").count()).toBe(1);
-  await page.locator(".audio-cell input").fill("foo");
-  await page.locator(".audio-cell input").press("Enter");
-  expect(await page.locator(".audio-cell input").inputValue()).toBe("foo");
+  // ３つAudioCellを追加したらAudioCellが４つになるのを確認
   await page.getByRole("button").filter({ hasText: "add" }).click();
   await page.getByRole("button").filter({ hasText: "add" }).click();
   await page.getByRole("button").filter({ hasText: "add" }).click();
-  await page.locator(".audio-cell input").nth(1).fill("bar");
-  await page.locator(".audio-cell input").nth(1).press("Enter");
-  await page.locator(".audio-cell input").nth(2).fill("baz");
-  await page.locator(".audio-cell input").nth(2).press("Enter");
+  await page.waitForTimeout(100);
+  await fillInput(page, page.locator(".audio-cell input").first(), "foo");
+  await fillInput(page, page.locator(".audio-cell input").nth(1), "bar");
+  await fillInput(page, page.locator(".audio-cell input").nth(2), "baz");
   expect(await page.locator(".audio-cell").count()).toBe(4);
+  // 一番上のAudioCellを削除しもともと２番めだったものが一番上に来てAudioCellが３つになることを確認
   await page.locator(".audio-cell").first().hover();
   await page
     .getByRole("button")
     .filter({ hasText: "delete_outline" })
     .first()
     .click();
+  await page.waitForTimeout(100);
   expect(await page.locator(".audio-cell").count()).toBe(3);
-  expect(await page.locator(".audio-cell input").first().inputValue()).toBe(
-    "bar"
+  await validateInput(page.locator(".audio-cell input").first(), "bar");
+  // ドラッグして一番上と２番めに上のものを入れ替えて、入れ替わってることを確認
+  const dragFrom = await getCenter(
+    page.locator(".audio-cell .icon-container").first()
   );
-  const dragFrom = (await page
-    .locator(".audio-cell .icon-container")
-    .first()
-    .boundingBox()) || {
-    x: 0,
-    y: 0,
-    width: 0,
-    height: 0,
-  };
-  const dragTo = (await page
-    .locator(".audio-cell .icon-container")
-    .nth(1)
-    .boundingBox()) || {
-    x: 0,
-    y: 0,
-    width: 0,
-    height: 0,
-  };
-  await page.mouse.move(
-    dragFrom.x + dragFrom.width / 2,
-    dragFrom.y + dragFrom.height / 2
+  const dragTo = await getCenter(
+    page.locator(".audio-cell .icon-container").nth(1)
   );
+  await page.mouse.move(dragFrom.x, dragFrom.y);
   await page.mouse.down();
-  await page.mouse.move(
-    dragTo.x + dragTo.width / 2,
-    dragTo.y + dragTo.height / 2
-  );
+  await page.mouse.move(dragTo.x, dragTo.y);
   await page.mouse.up();
 
-  expect(await page.locator(".audio-cell input").first().inputValue()).toBe(
-    "baz"
-  );
-  expect(await page.locator(".audio-cell input").nth(1).inputValue()).toBe(
-    "bar"
-  );
+  await page.waitForTimeout(100);
+  await validateInput(page.locator(".audio-cell input").first(), "baz");
+  await page.waitForTimeout(100);
+  await validateInput(page.locator(".audio-cell input").nth(1), "bar");
 });
