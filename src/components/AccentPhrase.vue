f6c5426e993426934e9d94d977e5b2bdc2192a59
tiramisu_oTATo <xxx.partyo@gmail.com>
2023-09-17 14:45:15 +0900

    refactor(lastPitches): 定義

--- src/components/AccentPhrase.vue
+++ src/components/AccentPhrase.vue
@@ -325,6 +325,10 @@ const toggleAccentPhraseSplit = (isPause: boolean, moraIndex?: number) => {
   });
 };
 
+const lastPitches = computed(() =>
+  props.accentPhrase.moras.map((mora) => mora.pitch)
+);
+
 const maxPitch = 6.5;
 const minPitch = 3;
 const maxMoraLength = 0.3;
@@ -337,7 +341,7 @@ const changeMoraData = (
   const accentPhraseIndex = props.index;
   if (!props.altKeyFlag) {
     if (type == "pitch") {
-      lastPitches.value[accentPhraseIndex][moraIndex] = data;
+      lastPitches.value[moraIndex] = data;
     }
     return store.dispatch("COMMAND_SET_AUDIO_MORA_DATA", {
       audioKey: props.audioKey,
@@ -364,11 +368,11 @@ const handleChangeVoicing = (mora: Mora, moraIndex: number) => {
   ) {
     let data = 0;
     if (mora.pitch == 0) {
-      if (lastPitches.value[props.index][moraIndex] == 0) {
+      if (lastPitches.value[moraIndex] == 0) {
         // 元々無声だった場合、適当な値を代入
         data = 5.5;
       } else {
-        data = lastPitches.value[props.index][moraIndex];
+        data = lastPitches.value[moraIndex];
       }
     }
     changeMoraData(moraIndex, data, "voicing");
