2f5418ab295db4a47cca5ace408133a586f999a2
sevenc-nanashi <sevenc7c@sevenc7c.com>
2023-09-26 17:34:26 +0900

    Fix: 表記揺れを修正

--- src/components/SettingDialog.vue
+++ src/components/SettingDialog.vue
@@ -896,7 +896,7 @@
                       self="center left"
                       transition-show="jump-right"
                       transition-hide="jump-left"
-                      >テキスト変更時、同じ読みのアクセント区間内の調整結果を保持します。</q-tooltip
+                      >ON の場合、テキスト変更時、同じ読みのアクセント区間内の調整結果を保持します。</q-tooltip
                     >
                   </q-icon>
                 </div>
