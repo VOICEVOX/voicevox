c1020e7e12d9d27b5bac2c7f71d1c7e07e8f7919
sevenc-nanashi <sevenc7c@sevenc7c.com>
2023-10-26 17:31:11 +0900

    Change: タイムアウトを緩和

--- tests/e2e/browser/初回起動時.spec.ts
+++ tests/e2e/browser/初回起動時.spec.ts
@@ -5,6 +5,6 @@ test.beforeEach(gotoHome);
 
 test("起動したら「利用規約に関するお知らせ」が表示される", async ({ page }) => {
   await expect(page.getByText("利用規約に関するお知らせ")).toBeVisible({
-    timeout: 60 * 1000,
+    timeout: 90 * 1000,
   });
 });
--- tests/e2e/navigators.ts
+++ tests/e2e/navigators.ts
@@ -15,7 +15,7 @@ export async function gotoHome({ page }: { page: Page }) {
  */
 export async function navigateToMain(page: Page) {
   await expect(page.getByText("利用規約に関するお知らせ")).toBeVisible({
-    timeout: 60 * 1000,
+    timeout: 90 * 1000,
   });
   await page.waitForTimeout(100);
   await page.getByRole("button", { name: "同意して使用開始" }).click();
@@ -23,6 +23,7 @@ export async function navigateToMain(page: Page) {
   await page.getByRole("button", { name: "完了" }).click();
   await page.waitForTimeout(100);
   await page.getByRole("button", { name: "許可" }).click();
+  await page.waitForTimeout(100);
 }
 
 /**
