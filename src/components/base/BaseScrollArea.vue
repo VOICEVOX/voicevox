c3c0eb96dc32ccc13c21f5e984b82c0eb4f5f4d3
takusea <takusea826@gmail.com>
2023-12-03 19:00:19 +0900

    BaseScrollAreaコンポーネントを追加

---
+++ src/components/base/BaseScrollArea.vue
@@ -0,0 +1,67 @@
+<template>
+  <ScrollAreaRoot class="scroll-area" type="auto">
+    <ScrollAreaViewport>
+      <slot />
+    </ScrollAreaViewport>
+    <ScrollAreaScrollbar class="scrollbar" orientation="horizontal">
+      <ScrollAreaThumb class="thumb-container">
+        <div class="thumb"></div>
+      </ScrollAreaThumb>
+    </ScrollAreaScrollbar>
+    <ScrollAreaScrollbar class="scrollbar" orientation="vertical">
+      <ScrollAreaThumb class="thumb-container">
+        <div class="thumb"></div>
+      </ScrollAreaThumb>
+    </ScrollAreaScrollbar>
+  </ScrollAreaRoot>
+</template>
+
+<script setup lang="ts">
+import {
+  ScrollAreaRoot,
+  ScrollAreaScrollbar,
+  ScrollAreaThumb,
+  ScrollAreaViewport,
+} from "radix-vue";
+</script>
+
+<style scoped lang="scss">
+@use '@/styles/variables' as vars;
+.scroll-area {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+}
+
+.scrollbar {
+  user-select: none;
+  touch-action: none;
+}
+
+.scrollbar[data-orientation="vertical"] {
+  width: vars.$size-scrollbar;
+}
+
+.scrollbar[data-orientation="horizontal"] {
+  height: vars.$size-scrollbar;
+}
+
+.thumb-container {
+  padding: 4px;
+}
+
+.thumb {
+  width: 100%;
+  height: 100%;
+  background-color: #0002;
+  border-radius: vars.$size-scrollbar;
+  position: relative;
+  transition: background-color vars.$transition-duration;
+}
+
+.scrollbar:hover .thumb {
+  background-color: #0003;
+}
+</style>
