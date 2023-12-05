8b40e920428dabaaae21db2859afdc45b3370aab
sevenc-nanashi <sevenc7c@sevenc7c.com>
2023-12-05 20:29:49 +0900

    Change: 記号を統一

--- src/store/audio.ts
+++ src/store/audio.ts
@@ -2118,7 +2118,7 @@ export const audioCommandStore = transformCommandStore(
         if (Object.keys(errors).length > 0) {
           throw new Error(
             `話者の変更に失敗しました：\n${Object.entries(errors)
-              .map(([audioKey, error]) => `${audioKey}: ${error}`)
+              .map(([audioKey, error]) => `${audioKey}：${error}`)
               .join("\n")}`
           );
         }
