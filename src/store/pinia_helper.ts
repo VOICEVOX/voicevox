a9ee9ee20fae851fdcc6dbf742566eb6df88b215
Segu_g <segusegment@gmail.com>
2023-12-13 02:54:36 +0900

    add jsDoc to pinia_helper

--- src/store/pinia_helper.ts
+++ src/store/pinia_helper.ts
@@ -29,22 +29,57 @@ export class StateController<Id extends string, S extends StateTree> {
   }
 
   /**
-   * getter, mutation, actionを定義するためのcontext (defGet, defMut, defAct) を管理するクラス
-   * */
+   * getter, mutation, actionを定義するためのcontextヘルパー (defGet, defMut, defAct) を得る関数
+   **/
   public useControllerContext(...args: Parameters<typeof this._useStore>) {
     const store = this._useStore(...args);
     return {
+      /**
+       * `Getter`を定義する為の関数
+       * @param getterDef - `getter`の定義関数
+       * @example
+       * const getter1 = defGet(({state}) => state.val1);
+       * const getter2 = defGet(({state, get}) => get(getter1) && state.val2);
+       */
       defGet: <Ret>(
         getterDef: GetterDefinition<Id, S, Ret>
       ): Getter<Id, S, Ret> => ({
         [GETTER_TAG]: getterDef,
         [STORE_TAG]: store,
       }),
+      /**
+       * `Mutation`を定義する為の関数
+       * @param mutationDef - `mutation`の定義関数
+       * @example
+       * const mutation1 = defMut(({state}, arg1: string) => {
+       *   state.val1 = arg1;
+       * });
+       * const mutation2 = defMut(({state, commit}, arg1: string, arg2: boolean) => {
+       *   commit(mutation1, arg1);
+       *   state.val2 = arg2;
+       * });
+       * const mutation3 = defMut(({state, get}) => {
+       *   state.val2 = !get(getter2);
+       * });
+       */
       defMut: <Payloads extends unknown[]>(
         mutationDef: MutationDefinition<Id, S, Payloads>
       ): Mutation<Id, S, Payloads> => ({
         [MUTATION_TAG]: mutationDef,
       }),
+      /**
+       * `Action`を定義する為の関数
+       * @param actionDef - `action`の定義関数
+       * @example
+       * const action1 = defAct(({commit}, arg1: string) => {
+       *   commit(mutation1, arg1);
+       * });
+       * const action2 = defAct(({state, get, dispatch}, arg1: string) => {
+       *   if (get(getter2))
+       *     dispatch(action1, arg1);
+       *   return state.val1;
+       * });
+       */
       defAct: <Payloads extends unknown[], Ret>(
         actionDef: ActionDefinition<Id, S, Payloads, Ret>
       ): Action<Id, S, Payloads, Ret> => ({
@@ -68,12 +103,29 @@ export class StateController<Id extends string, S extends StateTree> {
   }
 }
 
-// .vueや他モジュールからimportしたgetter, actionを呼び出すための補助関数
+/**
+ * .vueファイルや他モジュールでimportした`getter`, `action`を呼び出すための関数
+ * @example
+ * const store = useStore();
+ * const { get, dispatch } = useController();
+ * const ref1 = computed(() => get(store.getter1));
+ * const onChange = (text: string) => dispatch(store.action1, text);
+ */
 export function useController() {
   const dispatch = templateDispatchWithoutStore();
   const get = templateGetWithoutStore();
   return {
+    /**
+     * `Action`を呼び出す為の関数
+     * @example
+     * const ret1 = dispatch(store.action1, text);
+     */
     dispatch,
+    /**
+     * `Getter`を呼び出す為の関数
+     * @example
+     * const ref = computed(() => get(store.getter1));
+     */
     get,
   };
 }
@@ -97,7 +149,7 @@ export function templateDispatchWithoutStore(): Dispatch {
   return dispatch;
 }
 
-export function templateGetWithoutStore() {
+export function templateGetWithoutStore(): GlobalGet {
   function get<Id extends string, S extends StateTree, Ret>(
     getter: Getter<Id, S, Ret>
   ) {
@@ -166,15 +218,34 @@ export type Dispatch = <
   action: Action<Id, S, Payloads, Ret>,
   ...payloads: Payloads
 ) => Ret;
+export type GlobalGet = <Id extends string, S extends StateTree, Ret>(
+  getter: Getter<Id, S, Ret>
+) => Ret;
 
 // Getter
 export type GetterContext<Id extends string, S extends StateTree> = {
+  /**
+   * 対象の`Store`が持っている`state` (readonly)
+   */
   state: DeepReadonly<UnwrapRef<S>>;
+  /**
+   * 他のgetterを呼ぶための関数
+   **/
   get: Get<Id, S>;
 };
+/**
+ * Getterを実態を定義する為の関数
+ */
 export type GetterDefinition<Id extends string, S extends StateTree, Ret> = (
+  /**
+   * `state`, `getter`を参照する為のコンテキストが渡される引数
+   */
   context: GetterContext<Id, S>
 ) => Ret;
+/**
+ * Getterの実態
+ * `context`として渡される`get`関数を用いて呼び出すことができる。
+ */
 export type Getter<Id extends string, S extends StateTree, Ret> = {
   [GETTER_TAG]: GetterDefinition<Id, S, Ret>;
   [STORE_TAG]: Store<Id, S>;
@@ -182,15 +253,33 @@ export type Getter<Id extends string, S extends StateTree, Ret> = {
 
 // Mutation
 export type MutationContext<Id extends string, S extends StateTree> = {
+  /**
+   * 対象の`Store`が持っている`state` (writable)
+   */
   state: UnwrapRef<S>;
+  /**
+   * getterを呼ぶための関数
+   **/
   get: Get<Id, S>;
+  /**
+   * 他のmutationを呼ぶための関数
+   */
   commit: Commit<Id, S>;
 };
 export type MutationDefinition<
   Id extends string,
   S extends StateTree,
   Payloads extends unknown[]
-> = (context: MutationContext<Id, S>, ...payloads: Payloads) => undefined;
+> = (
+  // `state`, `getter`, `mutation`を参照する為のコンテキストが渡される引数
+  context: MutationContext<Id, S>,
+  // Mutationが持つ引数
+  ...payloads: Payloads
+) => undefined;
+/**
+ * `Mutation`の実態
+ * `context`として渡される`commit`関数を用いて呼び出すことができる。
+ */
 export type Mutation<
   Id extends string,
   S extends StateTree,
@@ -201,9 +290,21 @@ export type Mutation<
 
 // Action
 export type ActionContext<Id extends string, S extends StateTree> = {
+  /**
+   * 対象の`Store`が持っている`state` (readonly)
+   */
   state: DeepReadonly<UnwrapRef<S>>;
+  /**
+   * getterを呼ぶための関数
+   **/
   get: Get<Id, S>;
+  /**
+   * mutationを呼ぶための関数
+   */
   commit: Commit<Id, S>;
+  /**
+   * 他のactionを呼ぶための関数
+   */
   dispatch: Dispatch;
 };
 export type ActionDefinition<
@@ -211,7 +312,16 @@ export type ActionDefinition<
   S extends StateTree,
   Payloads extends unknown[],
   Ret
-> = (context: ActionContext<Id, S>, ...payloads: Payloads) => Ret;
+> = (
+  // `state`, `getter`, `mutation`, `action`を参照する為のコンテキストが渡される引数
+  context: ActionContext<Id, S>,
+  // actionの引数
+  ...payloads: Payloads
+) => Ret;
+/**
+ * `Action`の実態
+ * `context`として渡される`dispatch`関数を用いて呼び出すことができる。
+ */
 export type Action<
   Id extends string,
   S extends StateTree,
