4ebd4090d379ef355bb22747c4b3a471f717e189
Segu_g <segusegment@gmail.com>
2023-12-13 03:48:29 +0900

    add jsdoc to pinia_command.ts

--- src/store/pinia_command.ts
+++ src/store/pinia_command.ts
@@ -112,6 +112,10 @@ type CommandPatches = Record<
   }
 >;
 
+/**
+ * Stateに対してController(`mutation`, `action`, `command`)を用いた操作を定義するための関数
+ * Stateが`Command`に対応しており`mutation`の変更を記録してundo/redoに用いることができる。
+ */
 export class CommandableStateController<
   Id extends string,
   S extends StateTree
@@ -120,6 +124,9 @@ export class CommandableStateController<
     super(id, useStore);
   }
 
+  /**
+   * getter, mutation, action, commandを定義するためのcontextヘルパー (defGet, defMut, defAct, defCmd) を得る関数
+   **/
   public useControllerContext(...args: Parameters<StoreDefinition>) {
     const contexts = super.useControllerContext(...args);
     const commandStore = useCommand(...args);
@@ -136,18 +143,48 @@ export class CommandableStateController<
 
     return {
       ...contexts,
+      /**
+       * `Command`を定義する為の関数
+       * @param commandDef - `command`の定義関数
+       * @example
+       * const command1 = defCmd(({recordCommit}) => recordCommit(mutation1));
+       * const command2 = defCmd(async ({get, recordCommit}) => {
+       *   const arg = await preProcess(get(getter1));
+       *   recordCommit(mutation2, arg);
+       *   return await postProcess(get(getter2));
+       * });
+       */
       defCmd,
+      /**
+       * `Command`の`recordCommit`を無効化し, 記録を行わない通常の`Action`に変換する関数
+       * @example
+       * const action3 = invalidateRecord(command3);
+       */
       invalidateRecord: invalidateRecordInternal,
     };
   }
 }
 
+/**
+ * .vueファイルや他モジュールでimportした`getter`, `action`, `command`を呼び出すための関数
+ * @example
+ * const store = useStore();
+ * const { get, dispatch, commandDispatch } = useController();
+ * const ref1 = computed(() => get(store.getter1));
+ * const onChange = (text: string) => dispatch(store.action1, text);
+ * const onChangeRecord = (text: string) => commandDispatch(store.command1, text);
+ */
 export function useController(...args: Parameters<StoreDefinition>) {
   const commandStore = useCommand(...args);
   const callers = useControllerOrig();
   const commandDispatch = templateCommandDispatchWithoutStore(commandStore);
   return {
     ...callers,
+    /**
+     * `Command`を呼び出す為の関数
+     * @example
+     * const ret1 = commandDispatch(store.command1, text);
+     */
     commandDispatch,
   };
 }
@@ -275,11 +312,36 @@ export type CommandDispatch = <
 
 // Command
 export type CommandContext<Id extends string, S extends StateTree> = {
+  /**
+   * 対象の`Store`が持っている`state` (readonly)
+   */
   state: DeepReadonly<UnwrapRef<S>>;
+  /**
+   * getterを呼ぶための関数
+   **/
   get: Get<Id, S>;
+  /**
+   * mutationを呼ぶための関数
+   */
   commit: Commit<Id, S>;
+  /**
+   * mutationを呼び、その変更を記録してundo/redoを可能にする関数
+   * @desc
+   * この関数を用いてmutationを呼んだ場合、mutationの変更記録がimmerによって記録される。
+   * undo/redoではこの変更の記録を用いてstateのundoやredoを行っている。
+   * そのため、undo/redoの変更単位は`mutation`の変更単位と一致するため、
+   * commandの中で`recordCommit`を複数回叩くと、叩いた数だけ履歴が積まれる。
+   * したがって非同期処理などのmutationに含むことができない処理は事前に行い、
+   * `mutation`を纏め`recordCommit`を叩く回数を1回にする等の工夫が必要になる。
+   */
   recordCommit: Commit<Id, S>;
+  /**
+   * actionを呼ぶための関数
+   */
   dispatch: Dispatch;
+  /**
+   * 他のcommandを呼ぶための関数
+   */
   commandDispatch: CommandDispatch;
 };
 export type CommandDefinition<
@@ -287,7 +349,16 @@ export type CommandDefinition<
   S extends StateTree,
   Payloads extends unknown[],
   Ret
-> = (context: CommandContext<Id, S>, ...payloads: Payloads) => Ret;
+> = (
+  // `state`, `getter`, `mutation`, `action`, `command`を参照する為のコンテキストが渡される引数
+  context: CommandContext<Id, S>,
+  // commandの引数
+  ...payloads: Payloads
+) => Ret;
+/**
+ * `Command`の実態
+ * `context`として渡される`commandDispatch`関数を用いて呼び出すことができる。
+ */
 export type Command<
   Id extends string,
   S extends StateTree,
