2846c77cb59456ec387c5a8577c32bc7b5636828
takusea <takusea826@gmail.com>
2023-12-30 23:52:34 +0900

    ヘルプダイアログのドキュメントの表示を行うセクションのリデザイン

---
+++ src/components/template/HelpOssCommunityInfoSection.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="container">
+    <BaseScrollArea>
+      <div class="inner">
+        <BaseDocumentView>
+          <!-- eslint-disable-next-line vue/no-v-html -->
+          <div v-html="ossCommunityInfos"></div>
+        </BaseDocumentView>
+      </div>
+    </BaseScrollArea>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+import BaseDocumentView from "../base/BaseDocumentView.vue";
+import BaseScrollArea from "../base/BaseScrollArea.vue";
+import { useStore } from "@/store";
+import { useMarkdownIt } from "@/plugins/markdownItPlugin";
+
+const store = useStore();
+const ossCommunityInfos = ref("");
+
+const md = useMarkdownIt();
+
+onMounted(async () => {
+  ossCommunityInfos.value = md.render(
+    await store.dispatch("GET_OSS_COMMUNITY_INFOS")
+  );
+});
+</script>
+
+<style scoped lang="scss">
+@use '@/styles/variables' as vars;
+
+.container {
+  // TODO: 親コンポーネントからheightを取得できないため一時的にcalcを使用、HelpDialogの構造を再設計後100%に変更する
+  // height: 100%;
+  height: calc(100vh - 90px);
+  background-color: #fff;
+}
+
+.inner {
+  padding: vars.$padding-2;
+}
+</style>
---
+++ src/components/template/HelpPolicySection.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="container">
+    <BaseScrollArea>
+      <div class="inner">
+        <BaseDocumentView>
+          <!-- eslint-disable-next-line vue/no-v-html -->
+          <div v-html="policyHtml"></div>
+        </BaseDocumentView>
+      </div>
+    </BaseScrollArea>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+import BaseDocumentView from "../base/BaseDocumentView.vue";
+import BaseScrollArea from "../base/BaseScrollArea.vue";
+import { useMarkdownIt } from "@/plugins/markdownItPlugin";
+
+const props =
+  defineProps<{
+    policy: string;
+  }>();
+
+const policyHtml = ref("");
+
+const md = useMarkdownIt();
+
+onMounted(async () => {
+  policyHtml.value = md.render(props.policy);
+});
+</script>
+
+<style scoped lang="scss">
+@use '@/styles/variables' as vars;
+
+.container {
+  // TODO: 親コンポーネントからheightを取得できないため一時的にcalcを使用、HelpDialogの構造を再設計後100%に変更する
+  // height: 100%;
+  height: calc(100vh - 90px);
+  background-color: #fff;
+}
+
+.inner {
+  padding: vars.$padding-2;
+}
+</style>
