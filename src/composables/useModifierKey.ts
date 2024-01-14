21c9444c4fb4392e52a2c06ea4cd83af76f10969
sevenc-nanashi <sevenc7c@sevenc7c.com>
2024-01-14 23:56:23 +0900

    Refactor: 修飾キーのcomposableを追加

---
+++ src/composables/useModifierKey.ts
@@ -0,0 +1,39 @@
+import { Ref, ref } from "vue";
+import { isMac } from "@/type/preload";
+
+export const createUseKey = (key: string) => (): Ref<boolean> => {
+  const isActive = ref(false);
+  const onKeydown = (e: KeyboardEvent) => {
+    if (e.key === key) {
+      isActive.value = true;
+    }
+  };
+  const onKeyup = (e: KeyboardEvent) => {
+    if (e.key === key) {
+      isActive.value = false;
+    }
+  };
+  const onBlur = () => {
+    isActive.value = false;
+  };
+  window.addEventListener("keydown", onKeydown);
+  window.addEventListener("keyup", onKeyup);
+  window.addEventListener("blur", onBlur);
+  return isActive;
+};
+
+/** Shiftキーが押されているかどうかを返す */
+export const useShiftKey = createUseKey("Shift");
+/** Altキー（MacではOptionキー）が押されているかどうかを返す */
+export const useAltKey = createUseKey("Alt");
+
+const useMetaKey = createUseKey("Meta");
+const useControlKey = createUseKey("Control");
+
+/** Ctrlキー（MacではCommandキー）が押されているかどうかを返す */
+export const useCommandOrControlKey = () => {
+  if (isMac) {
+    return useMetaKey();
+  }
+  return useControlKey();
+};
