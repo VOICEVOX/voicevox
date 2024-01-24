e6f9cf503342ed39cc2ef52c9764e263016f44f9
cm-ayf <cm.ayf2734@gmail.com>
2024-01-24 22:21:38 +0900

    add rule no-strict-nullable

--- eslint-plugin/index.js
+++ eslint-plugin/index.js
@@ -3,7 +3,12 @@ module.exports = {
   configs: {
     all: {
       plugins: ["@voicevox"],
+      rules: {
+        "@voicevox/no-strict-nullable": "error",
+      },
     },
   },
-  rules: {},
+  rules: {
+    "no-strict-nullable": require("./no-strict-nullable"),
+  },
 };
---
+++ eslint-plugin/no-strict-nullable.js
@@ -0,0 +1,61 @@
+// @ts-check
+const { ESLintUtils, AST_NODE_TYPES } = require("@typescript-eslint/utils");
+
+const createRule = ESLintUtils.RuleCreator(
+  (name) => `https://github.com/VOICEVOX/voicevox/blob/main/rules/${name}.md`
+);
+
+/**
+ * @param {import("@typescript-eslint/types").TSESTree.BinaryExpression["left"]} node
+ */
+function isNull(node) {
+  return node.type === AST_NODE_TYPES.Literal && node.value == null;
+}
+
+/**
+ * @param {import("@typescript-eslint/types").TSESTree.BinaryExpression["right"]} node
+ */
+function isUndefined(node) {
+  return node.type === AST_NODE_TYPES.Identifier && node.name === "undefined";
+}
+
+module.exports = createRule({
+  create(context) {
+    return {
+      BinaryExpression(node) {
+        if (node.operator !== "===" && node.operator !== "!==") return;
+        if (!isNull(node.right) && !isUndefined(node.right)) return;
+
+        context.report({
+          node,
+          messageId: "report",
+          data: {
+            operator: node.operator.slice(0, 2),
+            expression: context.getSourceCode().getText(node.right),
+          },
+          fix(fixer) {
+            return fixer.replaceTextRange(
+              [node.left.range[1], node.right.range[0]],
+              node.operator.slice(0, 2) + " "
+            );
+          },
+        });
+      },
+    };
+  },
+  name: "no-strict-nullable",
+  meta: {
+    type: "problem",
+    docs: {
+      description: "undefinedとnullと比較する際に厳密等価演算子を使わない",
+      recommended: "error",
+    },
+    messages: {
+      report:
+        "`{{ operator }}= {{ expression }}'ではなく'{{ operator }} {{ expression }}'を使用してください。詳細: https://github.com/VOICEVOX/voicevox/issues/1513",
+    },
+    schema: [],
+    fixable: "code",
+  },
+  defaultOptions: [],
+});
