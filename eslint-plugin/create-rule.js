50179270986c79e75e11630d29a42d3bcc1cbaa9
cm-ayf <cm.ayf2734@gmail.com>
2024-01-24 22:45:01 +0900

    fix rule documentation url

--- eslint-plugin/create-rule.js
+++ eslint-plugin/create-rule.js
@@ -1,5 +1,6 @@
 const { ESLintUtils } = require("@typescript-eslint/utils");
 
 exports.createRule = ESLintUtils.RuleCreator(
-  (name) => `https://github.com/VOICEVOX/voicevox/blob/main/rules/${name}.md`
+  (name) =>
+    `https://github.com/VOICEVOX/voicevox/blob/main/eslint-plugin/${name}.md`
 );
