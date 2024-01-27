ce56cd7a1c963b0be7f2e0d2a5b457729db1a0a8
wewewe-ok <combination.kyoukyou@gmail.com>
2024-01-27 22:43:51 +0900

    ↑修正箇所を間違えていた(このコミットが正しい)

--- tests/e2e/browser/labファイル書き出し.spec.ts
+++ tests/e2e/browser/labファイル書き出し.spec.ts
@@ -55,12 +55,12 @@ test("「labファイルを書き出し」のトグルがオンの場合のみ�
   // labファイルを生成しない
   await navigateToMain(page);
   await commonAction(page, false, "おはようございます");
-  const downloadWav = await downloadAction(page, 5100); // 「おはようございます」が既に入力されているため、空文字列にする
+  const downloadWav = await downloadAction(page, 5100); // labファイル生成時より長く待つ(ダウンロード漏れがないことを保証したい)
   await expect(downloadWav.length).toBe(1);
   await expect(downloadWav[0]).toBe("おはようございます.wav");
 
   // labファイルを生成する
-  await commonAction(page, true, ""); // おはようございます」がすでに入力されているためから文字列にする
+  await commonAction(page, true, ""); // 「おはようございます」が既に入力されているため、空文字列にする
   const downloads = await downloadAction(page, 5000);
   await expect(downloads.length).toBe(2);
   await expect(downloads[0]).toBe("おはようございます.wav");
