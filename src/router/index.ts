7d1772b166132dae881fd4179ea446c050d9e677
Hiroshiba <hihokaruta@gmail.com>
2024-01-29 02:07:43 +0900

    [refactor] Talk系移動 (#1762)
    * Talk系移動
    
    * TalkのEditorHome移動
    
    * 対応早見表の更新
    
    * 辞書からの依存更新忘れ

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 import SingerHome from "../views/SingerHome.vue";
-import EditorHome from "../views/EditorHome.vue";
+import EditorHome from "@/components/Talk/EditorHome.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
