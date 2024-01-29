23fe2593458110deb4dba2420e6c290c0944a4e2
Nanashi <sevenc7c@sevenc7c.com>
2024-01-30 04:43:38 +0900

    [ソング] script setup構文に移行 (#1789)
    * Migrate: SingerHomeを移行
    
    * Delete: 不要なimportを削除
    
    * Migrate: SequenserKeysを移行
    
    * Migrate: CharacterPortraitを移行
    
    * Migrate: Sing/ToolVarを移行
    
    * Migrate: ScoreSequenserを移行
    
    * Migrate: SequencerNoteを移行
    
    * Migrate: SequencerRulerを移行
    
    * Migrate: Sing/CharacterMenuButtonを移行
    
    * Migrate: SequencerPhraseIndicatorを移行
    
    * Delete: 消し忘れを削除
    
    * Delete: zoomXを削除
    
    * いくつかワーニング出ていたので解消
    
    ---------
    
    Co-authored-by: Hiroshiba <hihokaruta@gmail.com>

--- src/components/Sing/SequencerKeys.vue
+++ src/components/Sing/SequencerKeys.vue
@@ -51,138 +51,121 @@
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent, computed, ref, onMounted, onUnmounted } from "vue";
+<script setup lang="ts">
+import { computed, ref, onMounted, onUnmounted } from "vue";
 import { useStore } from "@/store";
 import { keyInfos, getKeyBaseHeight } from "@/sing/viewHelper";
 
-export default defineComponent({
-  name: "SingSequencerKeys",
-  props: {
-    offset: { type: Number, default: 0 },
-    blackKeyWidth: { type: Number, default: 30 },
-  },
-  setup(props) {
-    const store = useStore();
-    const width = ref(48);
-    const zoomX = computed(() => store.state.sequencerZoomX);
-    const zoomY = computed(() => store.state.sequencerZoomY);
-    const keyBaseHeight = getKeyBaseHeight();
-    const whiteKeyInfos = keyInfos.filter((value) => value.color === "white");
-    const blackKeyInfos = keyInfos.filter((value) => value.color === "black");
-    const whiteKeyBasePositions = whiteKeyInfos.map((value) => {
-      const noteNumber = value.noteNumber;
-      const n = noteNumber % 12;
-      const o = Math.floor(noteNumber / 12);
-      if (n < 5) {
-        return keyBaseHeight * (128 - 12 * o - (5 / 3) * (n / 2 + 1));
-      } else {
-        return keyBaseHeight * (128 - 12 * o - 5 - (7 / 4) * ((n - 5) / 2 + 1));
-      }
-    });
-    const blackKeyBasePositions = blackKeyInfos.map((value) => {
-      return keyBaseHeight * (127 - value.noteNumber);
-    });
-    const height = computed(() => {
-      return keyBaseHeight * 128 * zoomY.value + 1;
-    });
-    const whiteKeyRects = computed(() => {
-      return whiteKeyBasePositions.map((value, index, array) => {
-        const isLast = index === array.length - 1;
-        const nextValue = isLast ? keyBaseHeight * 128 : array[index + 1];
-        return {
-          x: -2,
-          y: Math.floor(value * zoomY.value),
-          width: width.value + 2,
-          height:
-            Math.floor(nextValue * zoomY.value) -
-            Math.floor(value * zoomY.value),
-        };
-      });
-    });
-    const blackKeyRects = computed(() => {
-      return blackKeyBasePositions.map((value) => {
-        return {
-          x: -2,
-          y: Math.floor(value * zoomY.value),
-          width: props.blackKeyWidth + 2,
-          height: Math.floor(keyBaseHeight * zoomY.value),
-        };
-      });
-    });
-    const noteNumberOfKeyBeingPressed = ref<number | undefined>();
-
-    const sequencerKeys = ref<HTMLElement | null>(null);
-    let resizeObserver: ResizeObserver | undefined;
-
-    const onMouseDown = (noteNumber: number) => {
-      noteNumberOfKeyBeingPressed.value = noteNumber;
-      store.dispatch("PLAY_PREVIEW_SOUND", { noteNumber });
-    };
-
-    const onMouseUp = () => {
-      if (noteNumberOfKeyBeingPressed.value != undefined) {
-        const noteNumber = noteNumberOfKeyBeingPressed.value;
-        noteNumberOfKeyBeingPressed.value = undefined;
-        store.dispatch("STOP_PREVIEW_SOUND", { noteNumber });
-      }
-    };
-
-    const onMouseEnter = (noteNumber: number) => {
-      if (
-        noteNumberOfKeyBeingPressed.value != undefined &&
-        noteNumberOfKeyBeingPressed.value !== noteNumber
-      ) {
-        store.dispatch("STOP_PREVIEW_SOUND", {
-          noteNumber: noteNumberOfKeyBeingPressed.value,
-        });
-        noteNumberOfKeyBeingPressed.value = noteNumber;
-        store.dispatch("PLAY_PREVIEW_SOUND", { noteNumber });
-      }
-    };
-
-    onMounted(() => {
-      const sequencerKeysElement = sequencerKeys.value;
-      if (!sequencerKeysElement) {
-        throw new Error("sequencerKeysElement is null.");
-      }
-      resizeObserver = new ResizeObserver((entries) => {
-        let inlineSize = 0;
-        for (const entry of entries) {
-          for (const borderBoxSize of entry.borderBoxSize) {
-            inlineSize = borderBoxSize.inlineSize;
-          }
-        }
-        if (inlineSize > 0 && inlineSize !== width.value) {
-          width.value = inlineSize;
-        }
-      });
-      resizeObserver.observe(sequencerKeysElement);
-
-      document.addEventListener("mouseup", onMouseUp);
-    });
-
-    onUnmounted(() => {
-      resizeObserver?.disconnect();
-      document.removeEventListener("mouseup", onMouseUp);
-    });
-
+const props = withDefaults(
+  defineProps<{
+    offset: number;
+    blackKeyWidth: number;
+  }>(),
+  {
+    offset: 0,
+    blackKeyWidth: 30,
+  }
+);
+const store = useStore();
+const width = ref(48);
+const zoomY = computed(() => store.state.sequencerZoomY);
+const keyBaseHeight = getKeyBaseHeight();
+const whiteKeyInfos = keyInfos.filter((value) => value.color === "white");
+const blackKeyInfos = keyInfos.filter((value) => value.color === "black");
+const whiteKeyBasePositions = whiteKeyInfos.map((value) => {
+  const noteNumber = value.noteNumber;
+  const n = noteNumber % 12;
+  const o = Math.floor(noteNumber / 12);
+  if (n < 5) {
+    return keyBaseHeight * (128 - 12 * o - (5 / 3) * (n / 2 + 1));
+  } else {
+    return keyBaseHeight * (128 - 12 * o - 5 - (7 / 4) * ((n - 5) / 2 + 1));
+  }
+});
+const blackKeyBasePositions = blackKeyInfos.map((value) => {
+  return keyBaseHeight * (127 - value.noteNumber);
+});
+const height = computed(() => {
+  return keyBaseHeight * 128 * zoomY.value + 1;
+});
+const whiteKeyRects = computed(() => {
+  return whiteKeyBasePositions.map((value, index, array) => {
+    const isLast = index === array.length - 1;
+    const nextValue = isLast ? keyBaseHeight * 128 : array[index + 1];
     return {
-      keyInfos,
-      zoomX,
-      zoomY,
-      width,
-      height,
-      whiteKeyInfos,
-      blackKeyInfos,
-      whiteKeyRects,
-      blackKeyRects,
-      sequencerKeys,
-      noteNumberOfKeyBeingPressed,
-      onMouseDown,
-      onMouseEnter,
+      x: -2,
+      y: Math.floor(value * zoomY.value),
+      width: width.value + 2,
+      height:
+        Math.floor(nextValue * zoomY.value) - Math.floor(value * zoomY.value),
     };
-  },
+  });
+});
+const blackKeyRects = computed(() => {
+  return blackKeyBasePositions.map((value) => {
+    return {
+      x: -2,
+      y: Math.floor(value * zoomY.value),
+      width: props.blackKeyWidth + 2,
+      height: Math.floor(keyBaseHeight * zoomY.value),
+    };
+  });
+});
+const noteNumberOfKeyBeingPressed = ref<number | undefined>();
+
+const sequencerKeys = ref<HTMLElement | null>(null);
+let resizeObserver: ResizeObserver | undefined;
+
+const onMouseDown = (noteNumber: number) => {
+  noteNumberOfKeyBeingPressed.value = noteNumber;
+  store.dispatch("PLAY_PREVIEW_SOUND", { noteNumber });
+};
+
+const onMouseUp = () => {
+  if (noteNumberOfKeyBeingPressed.value != undefined) {
+    const noteNumber = noteNumberOfKeyBeingPressed.value;
+    noteNumberOfKeyBeingPressed.value = undefined;
+    store.dispatch("STOP_PREVIEW_SOUND", { noteNumber });
+  }
+};
+
+const onMouseEnter = (noteNumber: number) => {
+  if (
+    noteNumberOfKeyBeingPressed.value != undefined &&
+    noteNumberOfKeyBeingPressed.value !== noteNumber
+  ) {
+    store.dispatch("STOP_PREVIEW_SOUND", {
+      noteNumber: noteNumberOfKeyBeingPressed.value,
+    });
+    noteNumberOfKeyBeingPressed.value = noteNumber;
+    store.dispatch("PLAY_PREVIEW_SOUND", { noteNumber });
+  }
+};
+
+onMounted(() => {
+  const sequencerKeysElement = sequencerKeys.value;
+  if (!sequencerKeysElement) {
+    throw new Error("sequencerKeysElement is null.");
+  }
+  resizeObserver = new ResizeObserver((entries) => {
+    let inlineSize = 0;
+    for (const entry of entries) {
+      for (const borderBoxSize of entry.borderBoxSize) {
+        inlineSize = borderBoxSize.inlineSize;
+      }
+    }
+    if (inlineSize > 0 && inlineSize !== width.value) {
+      width.value = inlineSize;
+    }
+  });
+  resizeObserver.observe(sequencerKeysElement);
+
+  document.addEventListener("mouseup", onMouseUp);
+});
+
+onUnmounted(() => {
+  resizeObserver?.disconnect();
+  document.removeEventListener("mouseup", onMouseUp);
 });
 </script>
 
