8266083dcbccb90aed9ae4b8caecb745eff5085c
Sig <sigprogrammer@gmail.com>
2024-02-18 18:44:25 +0900

    p1p2をpointApointBに変更

--- src/sing/graphics/shaders/lineStripVertexShader.glsl
+++ src/sing/graphics/shaders/lineStripVertexShader.glsl
@@ -1,18 +1,18 @@
 #version 300 es
 
 in vec3 pos; // 頂点の位置
-in vec2 p1; // 線分の始点の位置
-in vec2 p2; // 線分の終点の位置
+in vec2 pointA; // 線分の始点の位置
+in vec2 pointB; // 線分の終点の位置
 
 uniform mat3 translationMatrix;
 uniform mat3 projectionMatrix;
 
 void main() {
-  vec2 basisVecX = normalize(p2 - p1);
+  vec2 basisVecX = normalize(pointB - pointA);
   vec2 basisVecY = vec2(-basisVecX.y, basisVecX.x);
   vec2 rotatedPos = basisVecX * pos.x + basisVecY * pos.y;
-  // mix(p1, p2, pos.z) は (pos.z == 0) ? p1 : p2 と同じ
-  vec2 translatedPos = rotatedPos + mix(p1, p2, pos.z);
+  // mix(pointA, pointB, pos.z) は (pos.z == 0) ? pointA : pointB と同じ
+  vec2 translatedPos = rotatedPos + mix(pointA, pointB, pos.z);
   vec3 transformedPos = projectionMatrix * translationMatrix * vec3(translatedPos, 1.0);
   gl_Position = vec4(transformedPos.xy, 0.0, 1.0);
 }
