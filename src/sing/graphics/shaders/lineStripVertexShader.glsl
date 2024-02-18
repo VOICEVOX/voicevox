afb82321c958fafc3e0b7c1714e0ae21ac1c75d5
Sig <sigprogrammer@gmail.com>
2024-02-18 18:26:28 +0900

    頂点シェーダーのコードを少し変更

--- src/sing/graphics/shaders/lineStripVertexShader.glsl
+++ src/sing/graphics/shaders/lineStripVertexShader.glsl
@@ -1,6 +1,6 @@
 #version 300 es
 
-in vec3 pos; // 線分の頂点の位置
+in vec3 pos; // 頂点の位置
 in vec2 p1; // 線分の始点の位置
 in vec2 p2; // 線分の終点の位置
 
@@ -11,6 +11,8 @@ void main() {
   vec2 basisVecX = normalize(p2 - p1);
   vec2 basisVecY = vec2(-basisVecX.y, basisVecX.x);
   vec2 rotatedPos = basisVecX * pos.x + basisVecY * pos.y;
-  vec3 calculatedPos = vec3(mix(p1, p2, pos.z) + rotatedPos, 1.0);
-  gl_Position = vec4((projectionMatrix * translationMatrix * calculatedPos).xy, 0.0, 1.0);
+  // mix(p1, p2, pos.z) は (pos.z == 0) ? p1 : p2 と同じ
+  vec2 translatedPos = rotatedPos + mix(p1, p2, pos.z);
+  vec3 transformedPos = projectionMatrix * translationMatrix * vec3(translatedPos, 1.0);
+  gl_Position = vec4(transformedPos.xy, 0.0, 1.0);
 }
