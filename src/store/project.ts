e1fc11815fc628cb53e08267b3310c85ee99ae27
Hiroshiba <hihokaruta@gmail.com>
2024-03-08 01:45:16 +0900

    マイグレーション忘れ

--- src/store/project.ts
+++ src/store/project.ts
@@ -397,13 +397,9 @@ export const projectStore = createPartialStore<ProjectStoreTypes>({
           }
 
           if (
-            semver.satisfies(
-              projectAppVersion,
-              "<0.16.2",
-              semverSatisfiesOptions
-            )
+            semver.satisfies(projectAppVersion, "<0.17", semverSatisfiesOptions)
           ) {
-            // 0.16.2 未満のプロジェクトファイルはトークの情報のみ
+            // 0.17 未満のプロジェクトファイルはトークの情報のみ
             // なので全情報(audioKeys/audioItems)をtalkに移動する
             projectData.talk = {
               audioKeys: projectData.audioKeys,
@@ -412,7 +408,7 @@ export const projectStore = createPartialStore<ProjectStoreTypes>({
 
             // ソングの情報を初期化
             // generateSingingStoreInitialScoreが今後変わることがあるかもしれないので、
-            // 0.16.2時点のスコア情報を直接書く
+            // 0.17時点のスコア情報を直接書く
             projectData.song = {
               tpqn: DEFAULT_TPQN,
               tempos: [
@@ -432,7 +428,6 @@ export const projectStore = createPartialStore<ProjectStoreTypes>({
                 {
                   singer: undefined,
                   keyRangeAdjustment: 0,
-                  volumeRangeAdjustment: 0,
                   notes: [],
                 },
               ],
@@ -440,6 +435,19 @@ export const projectStore = createPartialStore<ProjectStoreTypes>({
 
             delete projectData.audioKeys;
             delete projectData.audioItems;
+          }
+
+          if (
+            semver.satisfies(
+              projectAppVersion,
+              "<0.17.1",
+              semverSatisfiesOptions
+            )
+          ) {
+            // volumeRangeAdjustmentの追加
+            for (const track of projectData.song.tracks) {
+              track.volumeRangeAdjustment = 0;
+            }
           }
 
           // Validation check
