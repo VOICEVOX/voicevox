49a5ce9f2214409e27532fd5c54167f37906a19b
Hiroshiba <hihokaruta@gmail.com>
2024-03-09 07:18:57 +0900

    「音量域調整」を「声量調整」に変更し、使い方も少し記述 (#1920)
    * 音量域→声量
    
    * 動かせる音域を28にし、龍星くんに対応
    
    * 説明文を追加

--- src/store/type.ts
+++ src/store/type.ts
@@ -752,7 +752,7 @@ export type Singer = z.infer<typeof singerSchema>;
 export const trackSchema = z.object({
   singer: singerSchema.optional(),
   keyRangeAdjustment: z.number(), // 音域調整量
-  volumeRangeAdjustment: z.number(), // 音量域調整量
+  volumeRangeAdjustment: z.number(), // 声量調整量
   notes: z.array(noteSchema),
 });
 export type Track = z.infer<typeof trackSchema>;
