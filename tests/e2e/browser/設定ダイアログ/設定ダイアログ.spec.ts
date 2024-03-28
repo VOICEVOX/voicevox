efb475d0d2aee9cfd1d7332758a1d744bbdc368a
Hiroshiba <hihokaruta@gmail.com>
2024-03-28 09:53:48 +0900

    fullPage [update snapshots]

--- tests/e2e/browser/設定ダイアログ/設定ダイアログ.spec.ts
+++ tests/e2e/browser/設定ダイアログ/設定ダイアログ.spec.ts
@@ -9,10 +9,5 @@ test("スクリーンショット", async ({ page }) => {
   await navigateToSettingDialog(page);
   await page.waitForTimeout(500);
 
-  // スクリーンショット撮影とスクロールを繰り返す
-  for (let i = 0; i < 5; i++) {
-    await expect(page).toHaveScreenshot(`スクリーンショット_${i}.png`);
-    await page.mouse.wheel(0, 500);
-    await page.waitForTimeout(300);
-  }
+  await expect(page).toHaveScreenshot("スクリーンショット", { fullPage: true });
 });
