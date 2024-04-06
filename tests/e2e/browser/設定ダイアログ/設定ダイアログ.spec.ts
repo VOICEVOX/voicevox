0f3df259e470d76b7cf187c0d5568942d5a3d656
Hiroshiba <hihokaruta@gmail.com>
2024-04-07 00:30:33 +0900

    Revert "fullPage [update snapshots]"
    This reverts commit efb475d0d2aee9cfd1d7332758a1d744bbdc368a.

--- tests/e2e/browser/設定ダイアログ/設定ダイアログ.spec.ts
+++ tests/e2e/browser/設定ダイアログ/設定ダイアログ.spec.ts
@@ -9,5 +9,10 @@ test("スクリーンショット", async ({ page }) => {
   await navigateToSettingDialog(page);
   await page.waitForTimeout(500);
 
-  await expect(page).toHaveScreenshot("スクリーンショット", { fullPage: true });
+  // スクリーンショット撮影とスクロールを繰り返す
+  for (let i = 0; i < 5; i++) {
+    await expect(page).toHaveScreenshot(`スクリーンショット_${i}.png`);
+    await page.mouse.wheel(0, 500);
+    await page.waitForTimeout(300);
+  }
 });
