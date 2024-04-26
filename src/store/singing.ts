bef2cc1425c812866ef3678616a2ed19516aeddd
y-chan <y-chan@y-chan.dev>
2024-04-27 00:27:48 +0900

    update comment

--- src/store/singing.ts
+++ src/store/singing.ts
@@ -1359,7 +1359,7 @@ export const singingStore = createPartialStore<SingingStoreTypes>({
                 styleId: singingTeacherStyleId,
                 engineId: singerAndFrameRate.singer.engineId,
               });
-              // TODO: 新しく推論した音量をどう保存するか検討する
+              // TODO: 新しく推論した音量をどうプロジェクトファイルへ保存するか検討する
               singingGuide.query.volume = volumes;
 
               const blob = await synthesize(
