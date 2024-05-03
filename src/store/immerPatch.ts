6d8af82422f452d6ca60ffc882e6d5bf38abba1d
White-Green <43771790+White-Green@users.noreply.github.com>
2024-05-03 15:19:22 +0900

    doc

--- src/store/immerPatch.ts
+++ src/store/immerPatch.ts
@@ -1,12 +1,26 @@
 import { Patch } from "immer";
 import { ExhaustiveError } from "@/type/utility";
 
+/**
+ * produceWithPatchesにより生成された複数のパッチをオブジェクトに適用します。
+ *
+ * @param {T} target パッチを適用する対象オブジェクト
+ * @param {Patch[]} patches 適用するパッチの配列
+ * @template T 対象オブジェクトの型(任意)
+ */
 export function applyPatches<T>(target: T, patches: Patch[]) {
   for (const patch of patches) {
     applyPatch(target, patch);
   }
 }
 
+/**
+ * produceWithPatchesにより生成された単一のパッチをオブジェクトに適用します。
+ *
+ * @param {T} target パッチを適用する対象オブジェクト
+ * @param {Patch} patch 適用するパッチ
+ * @template T 対象オブジェクトの型(任意)
+ */
 export function applyPatch<T>(target: T, patch: Patch) {
   const { path, value, op } = patch;
   for (const p of patch.path.slice(0, path.length - 1)) {
