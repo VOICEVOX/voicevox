d503d4f72138b28ed8ca90bf9e58313863ac2183
P0ngCh4ng <p0ngch4ng@gmail.com>
2024-05-05 18:37:24 +0900

    <0.20.0 のマイグレーションを作成

--- src/store/project.ts
+++ src/store/project.ts
@@ -464,7 +464,22 @@ export const projectStore = createPartialStore<ProjectStoreTypes>({
               track.pitchEditData = [];
             }
           }
-
+          if (
+            semver.satisfies(
+              projectAppVersion,
+              "<0.20.0",
+              semverSatisfiesOptions,
+            )
+          ) {
+            for (const audioItemsKey in projectData.audioItems) {
+              const audioItem = projectData.audioItems[audioItemsKey];
+              if (audioItem.query != null) {
+                audioItem.query.accentPhrases.forEach((accentPhrase: EditorAccentPhrase) => {
+                  accentPhrase.key = generateAccentPhraseKey();
+                })
+              }
+            }
+          }
           // Validation check
           // トークはvalidateTalkProjectで検証する
           // ソングはSET_SCOREの中の`isValidScore`関数で検証される
