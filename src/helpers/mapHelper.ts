f23d54886a56622e8a60fe597cc1c75f428d19ae
Hiroshiba <hihokaruta@gmail.com>
2024-05-14 03:43:08 +0900

    Apply suggestions from code review

--- src/helpers/mapHelper.ts
+++ src/helpers/mapHelper.ts
@@ -1,3 +1,6 @@
+/** Mapのヘルパー関数 */
+
+/** Mapから値を取得する。指定したキーが存在しない場合は例外を投げる */
 export const getOrThrow = <K, V>(map: Map<K, V>, key: K) => {
   if (!map.has(key)) {
     throw new Error(`Key not found: ${key}`);
@@ -5,6 +8,7 @@ export const getOrThrow = <K, V>(map: Map<K, V>, key: K) => {
   return map.get(key) as V;
 };
 
+/** Mapから値を削除する。指定したキーが存在しない場合は例外を投げる */
 export const deleteOrThrow = <K, V>(map: Map<K, V>, key: K) => {
   if (!map.has(key)) {
     throw new Error(`Key not found: ${key}`);
