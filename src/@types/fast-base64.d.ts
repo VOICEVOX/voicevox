10efeaa14e0daa795f57380ec38860af781c35df
sevenc-nanashi <sevenc7c@sevenc7c.com>
2024-05-27 18:24:39 +0900

    Add: コメントを追加

--- src/@types/fast-base64.d.ts
+++ src/@types/fast-base64.d.ts
@@ -1,3 +1,4 @@
+// fast-base64の型定義が壊れているので、ここで型定義を追加する。
 declare module "fast-base64" {
   export function toBytes(base64: string): Promise<Uint8Array>;
   export function toBase64(bytes: Uint8Array): Promise<string>;
