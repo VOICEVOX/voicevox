526a46ad9262d38cc26ea8b45ed2f72d283a7783
sevenc-nanashi <sevenc7c@sevenc7c.com>
2024-06-03 08:16:22 +0900

    Fix: パスを更新

--- tests/unit/lib/utaformatixProject/import.spec.ts
+++ tests/unit/lib/utaformatixProject/import.spec.ts
@@ -9,7 +9,7 @@ import { importUtaformatixProject } from "@/sing/utaformatixProject/import";
 // - synthv.mid：SynthVで作成（Synthesizer V Studio Pro 1.11.0、プロジェクトファイルは https://github.com/VOICEVOX/voicevox/pull/1982 を参照）
 // - timeSig.mid、bpm.mid：signalで作成（https://signal.vercel.app/edit）
 
-const midiRoot = "tests/unit/lib/ufProjectToSongState/";
+const midiRoot = "tests/unit/lib/utaformatixProject/";
 
 const convertMidi = async (filename: string) => {
   const midi = await fs.readFile(midiRoot + filename);
