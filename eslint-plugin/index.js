c00fef5818507cf68ec78d710a919d8eea83e4f6
Hiroshiba <hihokaruta@gmail.com>
2024-06-03 10:27:36 +0900

    最新版のeslint

--- eslint-plugin/index.js
+++ eslint-plugin/index.js
@@ -5,12 +5,10 @@ module.exports = {
       plugins: ["@voicevox"],
       rules: {
         "@voicevox/no-strict-nullable": "error",
-        "@voicevox/property-shorthand": "error",
       },
     },
   },
   rules: {
     "no-strict-nullable": require("./no-strict-nullable"),
-    "property-shorthand": require("./property-shorthand"),
   },
 };
