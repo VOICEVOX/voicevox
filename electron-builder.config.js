ebdb85423a81663158046dc7c92457767fd1b17a
Han Yeong-woo <han@yeongwoo.dev>
2024-06-05 00:59:51 +0900

    darwin-arm64プラットフォーム対応 (#2112)
    * darwin-arm64プラットフォーム対応
    
    * アクションのmacOSビルドでアドホック署名
    
    * macOSの`.app`ファイルが正しい構造を持つように修正
    
    Contents/MacOSフォルダには実行ファイルのみが存在するべきですが、
    その他のファイルも含まれていたため、正しい構造ではなく、
    コード署名が失敗しました。
    
    * テストからmacOS arm64を一時的に除外
    
    Co-Authored-By: Hiroshiba <hihokaruta@gmail.com>
    
    ---------
    
    Co-authored-by: Hiroshiba <hihokaruta@gmail.com>

--- electron-builder.config.js
+++ electron-builder.config.js
@@ -29,6 +29,8 @@ const WIN_SIGNING_HASH_ALGORITHMS = process.env.WIN_SIGNING_HASH_ALGORITHMS
 
 const isMac = process.platform === "darwin";
 
+const isArm64 = process.arch === "arm64";
+
 // electron-builderのextraFilesは、ファイルのコピー先としてVOICEVOX.app/Contents/を使用する。
 // しかし、実行ファイルはVOICEVOX.app/Contents/MacOS/にあるため、extraFilesをVOICEVOX.app/Contents/ディレクトリにコピーするのは正しくない。
 // VOICEVOX.app/Contents/MacOS/ディレクトリにコピーされるように修正する。
@@ -154,7 +156,7 @@ const builderOptions = {
     target: [
       {
         target: "dmg",
-        arch: ["x64"],
+        arch: [isArm64 ? "arm64" : "x64"],
       },
     ],
   },
