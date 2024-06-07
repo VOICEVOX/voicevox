aed060200ed4bd030315d37cec087ea504d6d4d9
X-20A <155217226+X-20A@users.noreply.github.com>
2024-06-08 04:25:49 +0900

    UIを変更 [update snapshots]

--- src/components/Talk/AudioInfo.vue
+++ src/components/Talk/AudioInfo.vue
@@ -520,7 +520,25 @@ const parameters = computed<Parameter[]>(() => {
       action: "COMMAND_MULTI_SET_AUDIO_POST_PHONEME_LENGTH",
       key: "postPhonemeLength",
     },
-    switchPauseLengthMode.value === "SCALE" ? plsParam : plParam,
+    {
+      label: "文内無音(秒)",
+      slider: previewSliderHelper({
+        modelValue: () => query.value?.pauseLength ?? null,
+        disable: () => uiLocked.value,
+        max: SLIDER_PARAMETERS.PAUSE_LENGTH.max,
+        min: SLIDER_PARAMETERS.PAUSE_LENGTH.min,
+        step: SLIDER_PARAMETERS.PAUSE_LENGTH.step,
+        scrollStep: SLIDER_PARAMETERS.PAUSE_LENGTH.scrollStep,
+        scrollMinStep: SLIDER_PARAMETERS.PAUSE_LENGTH.scrollMinStep,
+        onChange: (pauseLength: number) =>
+          store.dispatch("COMMAND_MULTI_SET_AUDIO_PAUSE_LENGTH", {
+            audioKeys: selectedAudioKeys.value,
+            pauseLength: pauseLength,
+          }),
+      }),
+      action: "COMMAND_MULTI_SET_AUDIO_PAUSE_LENGTH",
+      key: "pauseLength",
+    },
   ];
   // switchPauseLengthModeの変更に伴って更新
   const newParam = switchPauseLengthMode.value === "SCALE" ? plsParam : plParam;
