ea15b84a09473ca27684fcb592ed9891e4b59461
sevenc-nanashi <sevenc7c@sevenc7c.com>
2024-06-16 06:29:00 +0900

    Change: about:blankにする

--- src/backend/electron/main.ts
+++ src/backend/electron/main.ts
@@ -977,7 +977,8 @@ ipcMainHandle("VALIDATE_ENGINE_DIR", (_, { engineDir }) => {
 ipcMainHandle("RELOAD_APP", async (_, { isMultiEngineOffMode }) => {
   win.hide(); // FIXME: ダミーページ表示のほうが良い
 
-  win.reload();
+  // 一旦適当なURLに飛ばしてページをアンロードする
+  await win.loadURL("about:blank");
 
   log.info("Checking ENGINE status before reload app");
   const engineCleanupResult = cleanupEngines();
