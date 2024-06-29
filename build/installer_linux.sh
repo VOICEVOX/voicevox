d5ca313ffcc18267bf1b55c7ad82eedcc1e6f5a0
madosuki <yurikasamaaikatsu@gmail.com>
2024-06-29 22:04:50 +0900

    fix comment

--- build/installer_linux.sh
+++ build/installer_linux.sh
@@ -405,7 +405,7 @@ DESKTOP_FILE=$(find squashfs-root -maxdepth 1 -name '*.desktop' | head -1)
 chmod +x "${DESKTOP_FILE}"
 
 ESCAPED_APP_DIR=$(echo "$APP_DIR" | sed 's/\//\\\//g')
-# --no-sandboxをつけているのはセキュリティが強化されたUbuntu 24.04で動作させるため ref:https://github.com/electron/electron/issues/41066
+#ToDo: --no-sandboxをつけているのはセキュリティが強化されたUbuntu 24.04で動作させるため ref:https://github.com/electron/electron/issues/41066。外せたら外す。
 sed "s/Exec=.*/Exec=${ESCAPED_APP_DIR}\/${APPIMAGE} %U --no-sandbox/" "${DESKTOP_FILE}" > _
 mv _ "${DESKTOP_FILE}"
 
