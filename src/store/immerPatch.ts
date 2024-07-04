af31ff53d4dab8ff07bdc5f81951880e7ed0bac1
White-Green <43771790+White-Green@users.noreply.github.com>
2024-07-04 23:53:14 +0900

    Revert "use internal applyPatches for testing"
    This reverts commit 7bedef38f446915ea534a6170f956fac0d74449e.

--- src/store/immerPatch.ts
+++ src/store/immerPatch.ts
@@ -1,11 +1,5 @@
 import { Patch } from "immer";
-import { enablePatches as enablePatchesImpl } from "immer/src/plugins/patches";
-import { enableMapSet as enableMapSetImpl } from "immer/src/plugins/mapset";
-import { getPlugin } from "immer/src/utils/plugins";
-
-enablePatchesImpl();
-enableMapSetImpl();
-const applyPatchesImpl = getPlugin("Patches").applyPatches_;
+import { ExhaustiveError } from "@/type/utility";
 
 /**
  * produceWithPatchesにより生成された複数のパッチをオブジェクトに適用します。
@@ -20,6 +14,79 @@ export function applyPatches<T>(target: T, patches: Patch[]) {
   }
 }
 
+function isObject(value: unknown): value is object {
+  return typeof value === "object" && value != null;
+}
+
+// structuredCloneはfunction等を処理できないため、オブジェクトのコピーを自前実装する
+function clone<T>(value: T): T {
+  if (!isObject(value)) return value;
+
+  if (Array.isArray(value)) {
+    if (Object.getPrototypeOf(value) !== Array.prototype)
+      throw new Error("unsupported type");
+    return value.map((v) => clone(v)) as T;
+  }
+
+  if (value instanceof Map) {
+    if (Object.getPrototypeOf(value) !== Map.prototype)
+      throw new Error("unsupported type");
+    const result = new Map();
+    for (const [k, v] of value.entries()) {
+      result.set(clone(k), clone(v));
+    }
+    return result as T;
+  }
+
+  if (value instanceof Set) {
+    if (Object.getPrototypeOf(value) !== Set.prototype)
+      throw new Error("unsupported type");
+    const result = new Set();
+    for (const v of value.values()) {
+      result.add(clone(v));
+    }
+    return result as T;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const result: any = Object.create(Object.getPrototypeOf(value));
+  for (const [k, v] of Object.entries(value)) {
+    result[k] = clone(v);
+  }
+  return result as T;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function get(value: unknown, key: string | number): any {
+  if (value instanceof Map) {
+    return value.get(key);
+  }
+  // @ts-expect-error produceWithPatchesにより生成されたPatchを適用するため、valueはany型として扱う
+  return value[key];
+}
+
+function add(value: unknown, key: string | number, v: unknown): void {
+  if (value instanceof Map) {
+    value.set(key, v);
+  } else if (value instanceof Set) {
+    value.add(v);
+  } else {
+    // @ts-expect-error produceWithPatchesにより生成されたPatchを適用するため、valueはany型として扱う
+    value[key] = v;
+  }
+}
+
+function remove(value: unknown, key: string | number, v: unknown): void {
+  if (value instanceof Map) {
+    value.delete(key);
+  } else if (value instanceof Set) {
+    value.delete(v);
+  } else {
+    // @ts-expect-error produceWithPatchesにより生成されたPatchを適用するため、valueはany型として扱う
+    delete value[key];
+  }
+}
+
 /**
  * produceWithPatchesにより生成された単一のパッチをオブジェクトに適用します。
  *
@@ -28,5 +95,20 @@ export function applyPatches<T>(target: T, patches: Patch[]) {
  * @template T 対象オブジェクトの型(任意)
  */
 export function applyPatch<T>(target: T, patch: Patch) {
-  applyPatchesImpl(target, [patch]);
+  const { path, value, op } = patch;
+  for (const p of patch.path.slice(0, path.length - 1)) {
+    target = get(target, p);
+  }
+  const v = clone(value);
+  switch (op) {
+    case "add":
+    case "replace":
+      add(target, path[path.length - 1], v);
+      break;
+    case "remove":
+      remove(target, path[path.length - 1], v);
+      break;
+    default:
+      throw new ExhaustiveError(op);
+  }
 }
