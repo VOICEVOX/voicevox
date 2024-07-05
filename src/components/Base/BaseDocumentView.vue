82e1507b601b06042c2ec4b6eb09a90ae33638a4
Takusea <53995265+takusea@users.noreply.github.com>
2024-07-06 05:43:57 +0900

    [project-redisign-help-dialog] デザインの調整 (#2155)
    * 横幅が狭いときの表示崩れを修正
    
    * 改段落ができていない箇所に改段落を追加
    
    * スクロールバーの色を濃くする
    
    * table要素のスタイルを追加
    
    * DocumentViewの余白を調節
    
    * div要素を削除

--- src/components/Base/BaseDocumentView.vue
+++ src/components/Base/BaseDocumentView.vue
@@ -16,27 +16,33 @@
 
   :deep(h1) {
     @include mixin.headline-1;
+    margin-bottom: 2rem;
   }
 
   :deep(h2) {
     @include mixin.headline-2;
-    margin-top: 1.5rem;
+    padding-block: 0.5rem;
+    margin-top: 2rem;
+    margin-bottom: 1.5rem;
     border-bottom: 1px solid colors.$border;
   }
 
   :deep(h3) {
     @include mixin.headline-3;
-    margin-top: 1.5rem;
+    margin-top: 1.75rem;
+    margin-bottom: 1rem;
   }
 
   :deep(h4) {
     @include mixin.headline-4;
-    margin-top: 1.25rem;
+    margin-top: 1.5rem;
+    margin-bottom: 0.5rem;
   }
 
   :deep(h5) {
     @include mixin.headline-5;
     margin-top: 1rem;
+    margin-bottom: 0.25rem;
   }
 
   :deep(h6) {
@@ -46,12 +52,19 @@
 
   :deep(p) {
     line-height: 1.75;
-    margin-top: 1rem;
+    margin-bottom: 1rem;
   }
 
-  :deep(div > ul),
-  :deep(div > ol) {
-    margin-top: 1rem;
+  :deep(ul),
+  :deep(ol) {
+    padding-left: 1.5rem;
+  }
+
+  :deep(> ul),
+  :deep(> ol),
+  :deep(:where(div, details) > ul),
+  :deep(:where(div, details) > ol) {
+    margin-bottom: 1rem;
   }
 
   :deep(li) {
@@ -68,10 +81,16 @@
   }
 
   :deep(img) {
-    margin-top: 0.5rem;
     border: 1px solid colors.$border;
     border-radius: vars.$radius-1;
     vertical-align: middle;
+    max-width: 100%;
+  }
+
+  :deep(> img),
+  :deep(:where(div, details) > img) {
+    display: block;
+    margin-bottom: 1rem;
   }
 
   :deep(hr) {
@@ -84,7 +103,7 @@
 
   :deep(pre) {
     padding: vars.$padding-1;
-    margin-top: 1rem;
+    margin-bottom: 1rem;
     background-color: colors.$surface;
     border: 1px solid colors.$border;
     border-radius: vars.$radius-1;
@@ -97,25 +116,53 @@
     border-radius: 4px;
   }
 
+  :deep(table) {
+    border-collapse: separate;
+    border-spacing: 0;
+    border-radius: vars.$radius-1;
+    border: 1px solid colors.$border;
+  }
+
+  :deep(tr) {
+    height: vars.$size-control;
+  }
+
+  :deep(td),
+  :deep(th) {
+    padding-inline: vars.$padding-2;
+  }
+
+  :deep(td) {
+    border-top: 1px solid colors.$border;
+  }
+
   :deep(details) {
-    padding: vars.$padding-1;
+    margin-bottom: 1rem;
+    padding: vars.$padding-2;
     background-color: colors.$surface;
     border: 1px solid colors.$border;
     border-radius: vars.$radius-1;
   }
 
   :deep(summary) {
-    padding: vars.$padding-1;
-    margin: calc(#{vars.$padding-1} * -1);
+    padding: vars.$padding-2;
+    margin: calc(#{vars.$padding-2} * -1);
     cursor: pointer;
   }
 
+  :deep(details[open] > summary) {
+    margin-bottom: 0;
+  }
   :deep(summary::before) {
     content: "▶ ";
   }
 
   :deep(details[open] > summary::before) {
     content: "▼ ";
+  }
+
+  :deep(:last-child) {
+    margin-bottom: 0;
   }
 }
 </style>
