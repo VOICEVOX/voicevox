bbfeb2f62d4578f62c6503155df667ca0f9a9f57
Sig <sigprogrammer@gmail.com>
2024-07-20 16:08:11 +0900

    ドキュメントコメントを追加

--- src/sing/stateMachine/stateMachineBase.ts
+++ src/sing/stateMachine/stateMachineBase.ts
@@ -1,17 +1,48 @@
+/**
+ * ステートマシンのステートを表すインターフェース。
+ *
+ * @template State このインターフェースを実装するステートの型。
+ * @template Input ステートが処理する入力の型。
+ * @template Context ステート間で共有されるコンテキストの型。
+ */
 export interface IState<
   State extends IState<State, Input, Context>,
   Input,
   Context,
 > {
+  /**
+   * 入力を処理し、必要に応じて次のステートを設定する。
+   *
+   * @param payload `input`、`context`、`setNextState`関数を含むペイロード。
+   */
   process(payload: {
     input: Input;
     context: Context;
     setNextState: (nextState: State) => void;
   }): void;
+
+  /**
+   * ステートに入ったときに呼び出される。
+   *
+   * @param context ステート間で共有されるコンテキスト。
+   */
   onEnter(context: Context): void;
+
+  /**
+   * ステートから出るときに呼び出される。
+   *
+   * @param context ステート間で共有されるコンテキスト。
+   */
   onExit(context: Context): void;
 }
 
+/**
+ * ステートマシンを表すクラス。
+ *
+ * @template State ステートマシンのステートの型。
+ * @template Input ステートが処理する入力の型。
+ * @template Context ステート間で共有されるコンテキストの型。
+ */
 export class StateMachine<
   State extends IState<State, Input, Context>,
   Input,
@@ -23,6 +54,10 @@ export class StateMachine<
   private currentState: State;
   private nextState: State | undefined;
 
+  /**
+   * @param initialState ステートマシンの初期ステート。
+   * @param context ステート間で共有されるコンテキスト。
+   */
   constructor(initialState: State, context: Context) {
     this.context = context;
     this.setNextState = (nextState: State) => {
@@ -32,10 +67,20 @@ export class StateMachine<
     this.nextState = undefined;
   }
 
+  /**
+   * ステートマシンの現在のステートを返す。
+   *
+   * @returns 現在のステート。
+   */
   getCurrentState() {
     return this.currentState;
   }
 
+  /**
+   * 現在のステートを使用して入力を処理し、必要に応じてステートの遷移を行う。
+   *
+   * @param input 処理する入力。
+   */
   process(input: Input) {
     try {
       this.currentState.process({
