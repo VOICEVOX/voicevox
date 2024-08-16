b6c43232087d7769e9bd5ab8e192580547469443
Takusea <takusea826@gmail.com>
2024-08-16 23:34:59 +0900

    typecheckエラーを修正

--- src/components/Base/BaseDocumentView.stories.ts
+++ src/components/Base/BaseDocumentView.stories.ts
@@ -10,63 +10,6 @@ export default meta;
 type Story = StoryObj<typeof BaseDocumentView>;
 
 export const Default: Story = {
-  args: {
-    html: `
-      <h1>Heading 1</h1>
-      <h2>Heading 2</h2>
-      <h3>Heading 3</h3>
-      <h4>Heading 4</h4>
-      <h5>Heading 5</h5>
-      <h6>Heading 6</h6>
-      <p>ParagraphParagraph<a href="#">Link</a>ParagraphParagraph<code>code</code>ParagraphParagraph</p>
-      <ul>
-        <li>List</li>
-        <li>List</li>
-        <li>List</li>
-      </ul>
-      <ol>
-        <li>List</li>
-        <li>List</li>
-        <li>List</li>
-      </ol>
-      <pre>pre</pre>
-      <details>
-      <summary>summary</summary>
-      <p>Details</p>
-      </details>
-      <table>
-        <thead>
-          <tr>
-            <th>Table Header</th>
-            <th>Table Header</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Table</td>
-            <td>Table</td>
-          </tr>
-          <tr>
-            <td>Table</td>
-            <td>Table</td>
-          </tr>
-          <tr>
-            <td>Table</td>
-            <td>Table</td>
-          </tr>
-          <tr>
-            <td>Table</td>
-            <td>Table</td>
-          </tr>
-          <tr>
-            <td>Table</td>
-            <td>Table</td>
-          </tr>
-        </tbody>
-      </table>
-    `,
-  },
-
   render: (args) => ({
     components: { BaseDocumentView },
 
@@ -74,6 +17,60 @@ export const Default: Story = {
       return { args };
     },
 
-    template: '<BaseDocumentView v-html="args.html" />',
+    template: `
+      <BaseDocumentView>
+        <h1>Heading 1</h1>
+        <h2>Heading 2</h2>
+        <h3>Heading 3</h3>
+        <h4>Heading 4</h4>
+        <h5>Heading 5</h5>
+        <h6>Heading 6</h6>
+        <p>ParagraphParagraph<a href="#">Link</a>ParagraphParagraph<code>code</code>ParagraphParagraph</p>
+        <ul>
+          <li>List</li>
+          <li>List</li>
+          <li>List</li>
+        </ul>
+        <ol>
+          <li>List</li>
+          <li>List</li>
+          <li>List</li>
+        </ol>
+        <pre>pre</pre>
+        <details>
+        <summary>summary</summary>
+        <p>Details</p>
+        </details>
+        <table>
+          <thead>
+            <tr>
+              <th>Table Header</th>
+              <th>Table Header</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Table</td>
+              <td>Table</td>
+            </tr>
+            <tr>
+              <td>Table</td>
+              <td>Table</td>
+            </tr>
+            <tr>
+              <td>Table</td>
+              <td>Table</td>
+            </tr>
+            <tr>
+              <td>Table</td>
+              <td>Table</td>
+            </tr>
+            <tr>
+              <td>Table</td>
+              <td>Table</td>
+            </tr>
+          </tbody>
+        </table>
+      </BaseDocumentView>`,
   }),
 };
