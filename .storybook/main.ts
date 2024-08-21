7aa6e8681ab9c1786903e1393b43c7ea532207d4
Takusea <53995265+takusea@users.noreply.github.com>
2024-08-22 01:51:29 +0900

    BaseコンポーネントのStorybookファイルを作成 (#2227)
    * storybook/addon-themesを導入
    
    * Baseコンポーネントのstorybookファイルを追加
    
    * 文字色を付与
    
    * onLabel/offLabelをcheckedLabel/uncheckedLabelに改名
    
    * lintを適用
    
    * typecheckエラーを修正
    
    * test-storybookのtimeoutを倍の時間に変更
    
    * 冗長な記述を削除
    
    * clickableのStoryを追加
    
    * test-storybookのtimeoutを更に倍に変更

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -7,6 +7,7 @@ const config: StorybookConfig = {
     "@storybook/addon-essentials",
     "@chromatic-com/storybook",
     "@storybook/addon-interactions",
+    "@storybook/addon-themes",
   ],
   framework: {
     name: "@storybook/vue3-vite",
