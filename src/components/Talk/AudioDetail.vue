8351506e8304c751d1f65dd9f8134835000dd7e2
Segu_g <segusegment@gmail.com>
2024-08-24 18:04:47 +0900

    update AudioDetail.vue

--- src/components/Talk/AudioDetail.vue
+++ src/components/Talk/AudioDetail.vue
@@ -153,7 +153,7 @@ registerHotkeyWithCleanup({
       const audioKeys = store.state.experimentalSetting.enableMultiSelect
         ? store.getters.SELECTED_AUDIO_KEYS
         : [store.getters.ACTIVE_AUDIO_KEY];
-      void store.dispatch("COMMAND_MULTI_RESET_MORA_PITCH_AND_LENGTH", {
+      void store.actions.COMMAND_MULTI_RESET_MORA_PITCH_AND_LENGTH({
         audioKeys,
       });
     }
@@ -168,7 +168,7 @@ registerHotkeyWithCleanup({
       store.getters.ACTIVE_AUDIO_KEY &&
       store.getters.AUDIO_PLAY_START_POINT != undefined
     ) {
-      void store.dispatch("COMMAND_RESET_SELECTED_MORA_PITCH_AND_LENGTH", {
+      void store.actions.COMMAND_RESET_SELECTED_MORA_PITCH_AND_LENGTH({
         audioKey: store.getters.ACTIVE_AUDIO_KEY,
         accentPhraseIndex: store.getters.AUDIO_PLAY_START_POINT,
       });
@@ -211,7 +211,7 @@ const startPoint = computed({
     return store.getters.AUDIO_PLAY_START_POINT;
   },
   set: (startPoint) => {
-    void store.dispatch("SET_AUDIO_PLAY_START_POINT", { startPoint });
+    void store.actions.SET_AUDIO_PLAY_START_POINT({ startPoint });
   },
 });
 // アクティブ(再生されている状態)なアクセント句
@@ -243,12 +243,12 @@ watch(accentPhrases, async () => {
 // audio play
 const play = async () => {
   try {
-    await store.dispatch("PLAY_AUDIO", {
+    await store.actions.PLAY_AUDIO({
       audioKey: props.activeAudioKey,
     });
   } catch (e) {
     const msg = handlePossiblyNotMorphableError(e);
-    void store.dispatch("SHOW_ALERT_DIALOG", {
+    void store.actions.SHOW_ALERT_DIALOG({
       title: "再生に失敗しました",
       message: msg ?? "エンジンの再起動をお試しください。",
     });
@@ -256,7 +256,7 @@ const play = async () => {
 };
 
 const stop = () => {
-  void store.dispatch("STOP_AUDIO");
+  void store.actions.STOP_AUDIO();
 };
 
 const nowPlaying = computed(() => store.getters.NOW_PLAYING);
@@ -310,7 +310,7 @@ const scrollToActivePoint = () => {
 let requestId: number | undefined;
 watch(nowPlaying, async (newState) => {
   if (newState) {
-    const accentPhraseOffsets = await store.dispatch("GET_AUDIO_PLAY_OFFSETS", {
+    const accentPhraseOffsets = await store.actions.GET_AUDIO_PLAY_OFFSETS({
       audioKey: props.activeAudioKey,
     });
     // 現在再生されているaudio elementの再生時刻を描画毎に取得(監視)し、
