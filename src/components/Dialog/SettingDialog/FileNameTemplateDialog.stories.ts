d1599af0f77cd590e6144bdcaf4ef62263d1ca33
sevenc-nanashi <sevenc7c@sevenc7c.com>
2024-09-08 06:20:45 +0900

    Fix: fnを消す

--- src/components/Dialog/SettingDialog/FileNameTemplateDialog.stories.ts
+++ src/components/Dialog/SettingDialog/FileNameTemplateDialog.stories.ts
@@ -1,4 +1,4 @@
-import { userEvent, within, expect, fn } from "@storybook/test";
+import { userEvent, within, expect, fn, waitFor } from "@storybook/test";
 
 import { Meta, StoryObj } from "@storybook/vue3";
 import FileNameTemplateDialog from "./FileNameTemplateDialog.vue";
@@ -21,25 +21,12 @@ const meta: Meta<typeof FileNameTemplateDialog> = {
     defaultTemplate: DEFAULT_AUDIO_FILE_BASE_NAME_TEMPLATE,
     savedTemplate: "",
     fileNameBuilder: buildAudioFileNameFromRawData,
-    "onUpdate:template": fn(),
-    "onUpdate:openDialog": fn(),
   },
   tags: ["!autodocs"], // ダイアログ系はautodocsのプレビューが正しく表示されないので無効化
 };
 
 export default meta;
 type Story = StoryObj<typeof meta>;
-
-const getErrorMessage = (input: HTMLInputElement) => {
-  const element =
-    input.parentElement?.parentElement?.parentElement?.querySelector(
-      ".q-field__messages",
-    );
-  if (!element) {
-    throw new Error("エラーメッセージが見つかりませんでした");
-  }
-  return element.textContent;
-};
 
 export const Opened: Story = {
   name: "開いている",
@@ -58,23 +45,11 @@ const createInvalidInputPlay =
     if (inputValue) {
       await userEvent.type(input, inputValue);
     }
-    // expect.pollが追加されるまでコメントアウト
-    // https://github.com/storybookjs/storybook/issues/29060
-    // await expect.poll(() => getErrorMessage(input)).toMatch(expectedMessage);
-    for (let i = 0; i < 10; i++) {
-      const errorMessage = getErrorMessage(input);
-      if (typeof expectedMessage === "string") {
-        if (errorMessage === expectedMessage) {
-          return;
-        }
-      } else {
-        if (errorMessage?.match(expectedMessage)) {
-          return;
-        }
-      }
-      await new Promise((resolve) => setTimeout(resolve, 100));
+    if (expectedMessage) {
+      await waitFor(() =>
+        expect(canvas.getByText(expectedMessage)).toBeVisible(),
+      );
     }
-    await expect.unreachable();
   };
 
 export const EmptyInput: Story = {
@@ -117,7 +92,7 @@ export const MissingIndexInput: Story = {
 };
 
 export const Save: Story = {
-  name: "保存ボタンを押す",
+  name: "確定ボタンを押す",
   args: { ...Opened.args },
   play: async ({ args }) => {
     const canvas = within(document.body); // ダイアログなので例外的にdocument.bodyを使う
@@ -129,14 +104,14 @@ export const Save: Story = {
     const button = canvas.getByRole("button", { name: "確定" });
     await userEvent.click(button);
 
-    // 保存とダイアログを閉じるイベントが呼ばれる
+    // 確定とダイアログを閉じるイベントが呼ばれる
     await expect(args["onUpdate:template"]).toBeCalledWith("$連番$.wav");
     await expect(args["onUpdate:openDialog"]).toBeCalledWith(false);
   },
 };
 
 export const Unsaveable: Story = {
-  name: "無効な入力だと保存ボタンが押せない",
+  name: "無効な入力だと確定ボタンが押せない",
   args: { ...Opened.args },
   play: async () => {
     const canvas = within(document.body); // ダイアログなので例外的にdocument.bodyを使う
@@ -159,7 +134,7 @@ export const Close: Story = {
     const button = canvas.getByRole("button", { name: "キャンセル" });
     await userEvent.click(button);
 
-    // ダイアログを閉じるイベントが呼ばれる、保存イベントは呼ばれない
+    // ダイアログを閉じるイベントが呼ばれる、確定イベントは呼ばれない
     await expect(args["onUpdate:template"]).not.toBeCalled();
     await expect(args["onUpdate:openDialog"]).toBeCalledWith(false);
   },
