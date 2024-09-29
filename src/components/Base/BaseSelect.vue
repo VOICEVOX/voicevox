29088c0df1f00a177d58f263d059b8ebea76b0b7
Takusea <takusea826@gmail.com>
2024-09-29 17:18:49 +0900

    BaseSelect関連コンポーネントを追加

---
+++ src/components/Base/BaseSelect.vue
@@ -0,0 +1,121 @@
+<template>
+  <SelectRoot v-model="model" :defaultValue :disabled>
+    <SelectTrigger class="SelectTrigger">
+      <SelectValue class="SelectValue" :placeholder />
+      <SelectIcon class="SelectIcon">
+        <!-- 暫定でq-iconを使用 -->
+        <QIcon name="keyboard_arrow_down" size="sm" />
+      </SelectIcon>
+    </SelectTrigger>
+
+    <SelectPortal>
+      <SelectContent
+        class="SelectContent"
+        position="popper"
+        :sideOffset="4"
+        :collisionPadding="8"
+        :arrowPadding="16"
+        avoidCollisions
+        hideWhenDetached
+      >
+        <SelectViewport>
+          <slot />
+        </SelectViewport>
+        <SelectArrow class="SelectArrow" />
+      </SelectContent>
+    </SelectPortal>
+  </SelectRoot>
+</template>
+
+<script setup lang="ts">
+import {
+  SelectContent,
+  SelectPortal,
+  SelectRoot,
+  SelectIcon,
+  SelectArrow,
+  SelectTrigger,
+  SelectValue,
+  SelectViewport,
+} from "radix-vue";
+
+defineProps<{
+  placeholder?: string;
+  defaultValue?: string;
+  disabled?: boolean;
+}>();
+
+const model = defineModel<string>();
+</script>
+
+<style scoped lang="scss">
+@use "@/styles/v2/variables" as vars;
+@use "@/styles/v2/colors" as colors;
+@use "@/styles/v2/mixin" as mixin;
+
+.SelectTrigger {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  min-height: vars.$size-control;
+  border-radius: vars.$radius-1;
+  padding-inline: vars.$padding-2 vars.$padding-1;
+  gap: vars.$gap-1;
+  border: 1px solid;
+  text-align: start;
+  transition: background-color vars.$transition-duration;
+  cursor: pointer;
+  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.05);
+  color: colors.$display;
+  border-color: colors.$border;
+  background-color: colors.$control;
+
+  &:hover:not(:disabled) {
+    background-color: colors.$control-hovered;
+  }
+
+  &:active:not(:disabled) {
+    background-color: colors.$control-pressed;
+  }
+
+  &:active:not(:disabled) {
+    box-shadow: 0 0 0 transparent;
+  }
+
+  &:focus-visible {
+    @include mixin.on-focus;
+  }
+
+  &[data-placeholder] {
+    color: colors.$display-sub;
+  }
+}
+
+.SelectIcon {
+  color: colors.$display;
+}
+
+:global([data-radix-popper-content-wrapper]) {
+  z-index: vars.$z-index-dropdown !important;
+}
+
+:global(.SelectContent) {
+  overflow: hidden;
+  display: flex;
+  border-radius: vars.$radius-2;
+  padding: vars.$padding-1;
+  color: colors.$display;
+  background-color: colors.$background;
+  border: 1px solid colors.$border;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.05);
+  min-width: var(--radix-select-trigger-width);
+  max-width: var(--radix-select-content-available-width);
+  max-height: var(--radix-select-content-available-height);
+}
+
+:global(.SelectArrow) {
+  fill: colors.$background;
+  margin-top: -1px;
+  filter: drop-shadow(0 1px 0px colors.$border);
+}
+</style>
