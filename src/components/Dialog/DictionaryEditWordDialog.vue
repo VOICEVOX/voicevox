659cdf86cc1dc8b95b126b2ff8b396b3419d5c2f
jdkfx <amateurranch@gmail.com>
2024-09-30 23:09:52 +0900

    ダイアログの複製とテンプレート部分の切り出し

--- src/components/Dialog/DictionaryEditWordDialog.vue
+++ src/components/Dialog/DictionaryEditWordDialog.vue
@@ -1,132 +1,171 @@
 <template>
-  <QDialog
-    v-model="dictionaryManageDialogOpenedComputed"
-    maximized
-    transitionShow="jump-up"
-    transitionHide="jump-down"
-    class="setting-dialog transparent-backdrop"
-  >
-    <QLayout container view="hHh Lpr fFf" class="bg-background">
-      <QPageContainer>
-        <QHeader class="q-pa-sm">
-          <QToolbar>
-            <QToolbarTitle class="text-display"
-              >読み方＆アクセント辞書</QToolbarTitle
-            >
-            <QSpace />
-            <!-- close button -->
-            <QBtn
-              round
-              flat
-              icon="close"
-              color="display"
-              :disable="wordEditing"
-              @click="discardOrNotDialog(closeDialog)"
-            />
-          </QToolbar>
-        </QHeader>
-        <QPage class="row">
-          <div v-if="loadingDictState" class="loading-dict">
-            <div>
-              <QSpinner color="primary" size="2.5rem" />
-              <div class="q-mt-xs">
-                <template v-if="loadingDictState === 'loading'"
-                  >読み込み中・・・</template
-                >
-                <template v-if="loadingDictState === 'synchronizing'"
-                  >同期中・・・</template
-                >
-              </div>
-            </div>
-          </div>
-          <div class="col-4 word-list-col">
+  <div v-show="wordEditing" class="col-8 no-wrap text-no-wrap word-editor">
+    <div class="row q-pl-md q-mt-md">
+      <div class="text-h6">単語</div>
+      <QInput
+        ref="surfaceInput"
+        v-model="surface"
+        class="word-input"
+        dense
+        :disable="uiLocked"
+        @focus="clearSurfaceInputSelection()"
+        @blur="setSurface(surface)"
+        @keydown.enter="yomiFocus"
+      >
+        <ContextMenu
+          ref="surfaceContextMenu"
+          :header="surfaceContextMenuHeader"
+          :menudata="surfaceContextMenudata"
+          @beforeShow="startSurfaceContextMenuOperation()"
+          @beforeHide="endSurfaceContextMenuOperation()"
+        />
+      </QInput>
+    </div>
+    <div class="row q-pl-md q-pt-sm">
+      <div class="text-h6">読み</div>
+      <QInput
+        ref="yomiInput"
+        v-model="yomi"
+        class="word-input q-pb-none"
+        dense
+        :error="!isOnlyHiraOrKana"
+        :disable="uiLocked"
+        @focus="clearYomiInputSelection()"
+        @blur="setYomi(yomi)"
+        @keydown.enter="setYomiWhenEnter"
+      >
+        <template #error>
+          読みに使える文字はひらがなとカタカナのみです。
+        </template>
+        <ContextMenu
+          ref="yomiContextMenu"
+          :header="yomiContextMenuHeader"
+          :menudata="yomiContextMenudata"
+          @beforeShow="startYomiContextMenuOperation()"
+          @beforeHide="endYomiContextMenuOperation()"
+        />
+      </QInput>
+    </div>
+    <div class="row q-pl-md q-mt-lg text-h6">アクセント調整</div>
+    <div class="row q-pl-md desc-row">
+      語尾のアクセントを考慮するため、「が」が自動で挿入されます。
+    </div>
+    <div class="row q-px-md" style="height: 130px">
+      <div class="play-button">
+        <QBtn
+          v-if="!nowPlaying && !nowGenerating"
+          fab
+          color="primary"
+          textColor="display-on-primary"
+          icon="play_arrow"
+          @click="play"
+        />
+        <QBtn
+          v-else
+          fab
+          color="primary"
+          textColor="display-on-primary"
+          icon="stop"
+          :disable="nowGenerating"
+          @click="stop"
+        />
+      </div>
+      <div
+        ref="accentPhraseTable"
+        class="accent-phrase-table overflow-hidden-y"
+      >
+        <div v-if="accentPhrase" class="mora-table">
+          <AudioAccent
+            :accentPhrase
+            :accentPhraseIndex="0"
+            :uiLocked
+            :onChangeAccent="changeAccent"
+          />
+          <template
+            v-for="(mora, moraIndex) in accentPhrase.moras"
+            :key="moraIndex"
+          >
             <div
-              v-if="wordEditing"
-              class="word-list-disable-overlay"
-              @click="discardOrNotDialog(cancel)"
-            />
-            <div class="word-list-header text-no-wrap">
-              <div class="row word-list-title">
-                <span class="text-h5 col-8">単語一覧</span>
-                <QBtn
-                  outline
-                  textColor="display"
-                  class="text-no-wrap text-bold col"
-                  :disable="uiLocked"
-                  @click="newWord"
-                  >追加</QBtn
-                >
-              </div>
+              class="text-cell"
+              :style="{
+                gridColumn: `${moraIndex * 2 + 1} / span 1`,
+              }"
+            >
+              {{ mora.text }}
             </div>
-            <QList class="word-list">
-              <QItem
-                v-for="(value, key) in userDict"
-                :key
-                v-ripple
-                tag="label"
-                clickable
-                :active="selectedId === key"
-                activeClass="active-word"
-                @click="selectWord(key)"
-                @dblclick="editWord"
-                @mouseover="hoveredKey = key"
-                @mouseleave="hoveredKey = undefined"
-              >
-                <QItemSection>
-                  <QItemLabel lines="1" class="text-display">{{
-                    value.surface
-                  }}</QItemLabel>
-                  <QItemLabel lines="1" caption>{{ value.yomi }}</QItemLabel>
-                </QItemSection>
-
-                <QItemSection
-                  v-if="!uiLocked && (hoveredKey === key || selectedId === key)"
-                  side
-                >
-                  <div class="q-gutter-xs">
-                    <QBtn
-                      size="12px"
-                      flat
-                      dense
-                      round
-                      icon="edit"
-                      @click.stop="
-                        selectWord(key);
-                        editWord();
-                      "
-                    >
-                      <QTooltip :delay="500">編集</QTooltip>
-                    </QBtn>
-                    <QBtn
-                      size="12px"
-                      flat
-                      dense
-                      round
-                      icon="delete_outline"
-                      @click.stop="
-                        selectWord(key);
-                        deleteWord();
-                      "
-                    >
-                      <QTooltip :delay="500">削除</QTooltip>
-                    </QBtn>
-                  </div>
-                </QItemSection>
-              </QItem>
-            </QList>
-          </div>
-
-          <DictionaryEditWordDialog></DictionaryEditWordDialog>
-        </QPage>
-      </QPageContainer>
-    </QLayout>
-  </QDialog>
+            <div
+              v-if="moraIndex < accentPhrase.moras.length - 1"
+              class="splitter-cell"
+              :style="{
+                gridColumn: `${moraIndex * 2 + 2} / span 1`,
+              }"
+            />
+          </template>
+        </div>
+      </div>
+    </div>
+    <div class="row q-pl-md q-pt-lg text-h6">単語優先度</div>
+    <div class="row q-pl-md desc-row">
+      単語を登録しても反映されない場合は優先度を高くしてください。
+    </div>
+    <div
+      class="row q-px-md"
+      :style="{
+        justifyContent: 'center',
+      }"
+    >
+      <QSlider
+        v-model="wordPriority"
+        snap
+        dense
+        color="primary"
+        markers
+        :min="0"
+        :max="10"
+        :step="1"
+        :markerLabels="wordPriorityLabels"
+        :style="{
+          width: '80%',
+        }"
+      />
+    </div>
+    <div class="row q-px-md save-delete-reset-buttons">
+      <QSpace />
+      <QBtn
+        v-show="!!selectedId"
+        outline
+        textColor="display"
+        class="text-no-wrap text-bold q-mr-sm"
+        :disable="uiLocked || !isWordChanged"
+        @click="resetWord(selectedId)"
+        >リセット</QBtn
+      >
+      <QBtn
+        outline
+        textColor="display"
+        class="text-no-wrap text-bold q-mr-sm"
+        :disable="uiLocked"
+        @click="discardOrNotDialog(cancel)"
+        >キャンセル</QBtn
+      >
+      <QBtn
+        outline
+        textColor="display"
+        class="text-no-wrap text-bold q-mr-sm"
+        :disable="uiLocked || !isWordChanged"
+        @click="saveWord"
+        >保存</QBtn
+      >
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { computed, ref, watch } from "vue";
 import { QInput } from "quasar";
-import DictionaryEditWordDialog from "./DictionaryEditWordDialog.vue";
+import AudioAccent from "@/components/Talk/AudioAccent.vue";
+import ContextMenu from "@/components/Menu/ContextMenu.vue";
+import { useRightClickContextMenu } from "@/composables/useRightClickContextMenu";
 import { useStore } from "@/store";
 import type { FetchAudioResult } from "@/store/type";
 import { AccentPhrase, UserDictWord } from "@/openapi";
@@ -531,68 +570,108 @@ const toWordEditingState = () => {
 const toDialogClosedState = () => {
   dictionaryManageDialogOpenedComputed.value = false;
 };
+
+const surfaceContextMenu = ref<InstanceType<typeof ContextMenu>>();
+const yomiContextMenu = ref<InstanceType<typeof ContextMenu>>();
+
+const {
+  contextMenuHeader: surfaceContextMenuHeader,
+  contextMenudata: surfaceContextMenudata,
+  startContextMenuOperation: startSurfaceContextMenuOperation,
+  clearInputSelection: clearSurfaceInputSelection,
+  endContextMenuOperation: endSurfaceContextMenuOperation,
+} = useRightClickContextMenu(surfaceContextMenu, surfaceInput, surface);
+
+const {
+  contextMenuHeader: yomiContextMenuHeader,
+  contextMenudata: yomiContextMenudata,
+  startContextMenuOperation: startYomiContextMenuOperation,
+  clearInputSelection: clearYomiInputSelection,
+  endContextMenuOperation: endYomiContextMenuOperation,
+} = useRightClickContextMenu(yomiContextMenu, yomiInput, yomi);
 </script>
 
 <style lang="scss" scoped>
 @use "@/styles/colors" as colors;
 @use "@/styles/variables" as vars;
 
-.word-list-col {
-  border-right: solid 1px colors.$surface;
-  position: relative; // オーバーレイのため
-  overflow-x: hidden;
-}
-
-.word-list-header {
-  margin: 1rem;
-
-  gap: 0.5rem;
-  align-items: center;
-  justify-content: space-between;
-  .word-list-title {
-    flex-grow: 1;
-  }
-}
-
-.word-list {
-  // menubar-height + toolbar-height + window-border-width +
-  // 36(title & buttons) + 30(margin 15x2)
+.word-editor {
+  display: flex;
+  flex-flow: column;
   height: calc(
     100vh - #{vars.$menubar-height + vars.$toolbar-height +
-      vars.$window-border-width + 36px + 30px}
-  );
-  width: 100%;
-  overflow-y: auto;
-  padding-bottom: 16px;
+      vars.$window-border-width}
+  ) !important;
+  overflow: auto;
 }
 
-.active-word {
-  background: rgba(colors.$primary-rgb, 0.4);
-}
+.word-input {
+  padding-left: 10px;
+  width: calc(66vw - 80px);
 
-.loading-dict {
-  background-color: rgba(colors.$display-rgb, 0.15);
-  position: absolute;
-  inset: 0;
-  z-index: 10;
-  display: flex;
-  text-align: center;
-  align-items: center;
-  justify-content: center;
+  :deep(.q-field__control) {
+    height: 2rem;
+  }
 
-  > div {
-    color: colors.$display;
-    background: colors.$background;
-    border-radius: 6px;
-    padding: 14px;
+  :deep(.q-placeholder) {
+    padding: 0;
+    font-size: 20px;
+  }
+
+  :deep(.q-field__after) {
+    height: 2rem;
   }
 }
 
-.word-list-disable-overlay {
-  background-color: rgba($color: #000000, $alpha: 0.4);
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  z-index: 10;
+.desc-row {
+  color: rgba(colors.$display-rgb, 0.5);
+  font-size: 12px;
+}
+
+.play-button {
+  margin: auto 0;
+  padding-right: 16px;
+}
+
+.accent-phrase-table {
+  flex-grow: 1;
+  align-self: stretch;
+
+  display: flex;
+  height: 130px;
+  overflow-x: scroll;
+  width: calc(66vw - 140px);
+
+  .mora-table {
+    display: inline-grid;
+    align-self: stretch;
+    grid-template-rows: 1fr 60px 30px;
+
+    .text-cell {
+      padding: 0;
+      min-width: 20px;
+      max-width: 20px;
+      grid-row-start: 3;
+      text-align: center;
+      white-space: nowrap;
+      color: colors.$display;
+      position: relative;
+    }
+
+    .splitter-cell {
+      min-width: 20px;
+      max-width: 20px;
+      grid-row: 3 / span 1;
+      z-index: vars.$detail-view-splitter-cell-z-index;
+    }
+  }
+}
+
+.save-delete-reset-buttons {
+  padding: 20px;
+
+  display: flex;
+  flex: 1;
+  align-items: flex-end;
 }
 </style>
