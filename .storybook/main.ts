af60b5408b436eba4d0917fbb5ad28ffe4fc4ffb
sevenc-nanashi <sevenc7c@sevenc7c.com>
2024-10-09 19:56:41 +0900

    Migrate: storybookのmigrationを実行

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,6 +2,7 @@ import type { StorybookConfig } from "@storybook/vue3-vite";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
@@ -9,12 +10,15 @@ const config: StorybookConfig = {
     "@storybook/addon-interactions",
     "@storybook/addon-themes",
   ],
+
   framework: {
     name: "@storybook/vue3-vite",
     options: {
       docgen: "vue-component-meta",
     },
   },
+
+  docs: {}
 };
 
 export default config;
