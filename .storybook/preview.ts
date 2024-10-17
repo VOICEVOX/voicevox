d44ea2e81fb159ce30dd9c0d9cd4cb42056ac2ff
sevenc-nanashi <sevenc7c@sevenc7c.com>
2024-10-17 18:29:27 +0900

    Add: Storybookにテーマが適用されるように

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -4,10 +4,13 @@ import iconSet from "quasar/icon-set/material-icons";
 import { withThemeByDataAttribute } from "@storybook/addon-themes";
 import { addActionsWithEmits } from "./utils/argTypesEnhancers";
 import { markdownItPlugin } from "@/plugins/markdownItPlugin";
+import { api as browserSandbox } from "@/backend/browser/sandbox";
 
 import "@quasar/extras/material-icons/material-icons.css";
 import "quasar/dist/quasar.sass";
 import "../src/styles/_index.scss";
+import { UnreachableError } from "@/type/utility";
+import { setThemeToCss } from "@/domain/dom";
 
 setup((app) => {
   app.use(Quasar, {
@@ -61,6 +64,45 @@ const preview: Preview = {
       defaultTheme: "light",
       attributeName: "is-dark-theme",
     }),
+    () => {
+      let observer: MutationObserver | undefined = undefined;
+      return {
+        async mounted() {
+          const root = document.documentElement;
+          const themes = await browserSandbox.getAvailableThemes();
+          observer = new MutationObserver(() => {
+            if (!observer)
+              throw new UnreachableError("assert: observer !== undefined");
+
+            const isDark = root.getAttribute("is-dark-theme") === "true";
+            const theme = themes.find((theme) => theme.isDark === isDark);
+            if (!theme)
+              throw new UnreachableError("assert: theme !== undefined");
+
+            observer.disconnect();
+
+            setThemeToCss(theme);
+
+            observer.observe(root, {
+              attributes: true,
+              attributeFilter: ["is-dark-theme"],
+            });
+          });
+
+          observer.observe(root, {
+            attributes: true,
+            attributeFilter: ["is-dark-theme"],
+          });
+        },
+        unmounted() {
+          if (observer) {
+            observer.disconnect();
+          }
+        },
+
+        template: `<story />`
+      };
+    },
   ],
   argTypesEnhancers: [addActionsWithEmits],
 };
