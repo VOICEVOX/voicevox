f108c9950866f125ca2da0b31491013e6e843a71
sevenc-nanashi <sevenc7c@sevenc7c.com>
2024-10-22 21:07:02 +0900

    Change: とりあえず分離

---
+++ src/components/Sing/SequencerRuler/Container.vue
@@ -0,0 +1,15 @@
+<template>
+  <Presentation />
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+import Presentation from "./Presentation.vue";
+import { useStore } from "@/store";
+
+defineOptions({
+  name: "SequencerGrid",
+});
+
+const store = useStore();
+</script>
