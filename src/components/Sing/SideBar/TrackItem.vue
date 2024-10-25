5671f0a9e2b7cb711079569d9e78b3c8aec7cb89
Hiroshiba <hihokaruta@gmail.com>
2024-10-26 03:05:38 +0900

    選択されていないトラックのトラック名の表示をQInputの見た目に合わせる

--- src/components/Sing/SideBar/TrackItem.vue
+++ src/components/Sing/SideBar/TrackItem.vue
@@ -67,7 +67,9 @@
             :disable="uiLocked"
             @blur="updateTrackName"
           />
-          <div v-else>{{ track.name }}</div>
+          <div v-else class="walkaround-unselected-track-name">
+            {{ track.name }}
+          </div>
         </QItemLabel>
         <QItemLabel v-if="trackCharacter" caption class="singer-name">
           <!-- ミュート中はアイコンを表示 -->
@@ -409,5 +411,12 @@ const singerName = computed(() => {
   white-space: nowrap;
   overflow: hidden;
   text-overflow: ellipsis;
+}
+
+// 選択されていないトラックのトラック名の表示をQInputの見た目に合わせる
+.walkaround-unselected-track-name {
+  margin-bottom: 2px;
+  margin-top: 3px;
+  letter-spacing: 0.00937em;
 }
 </style>
