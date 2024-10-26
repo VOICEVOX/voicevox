3d5b90e181d20f73bd611febc63b102f344ab504
Segu-g <segusegment@gmail.com>
2024-10-27 02:56:01 +0900

    project.ts: dot notation

--- src/store/project.ts
+++ src/store/project.ts
@@ -399,9 +399,9 @@ export const projectStore = createPartialStore<ProjectStoreTypes>({
   },
 
   CLEAR_UNDO_HISTORY: {
-    action({ commit }) {
-      commit("RESET_SAVED_LAST_COMMAND_IDS");
-      commit("CLEAR_COMMANDS");
+    action({ mutations }) {
+      mutations.RESET_SAVED_LAST_COMMAND_IDS();
+      mutations.CLEAR_COMMANDS();
     },
   },
 });
