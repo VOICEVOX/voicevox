325893879e56155be52960181c11795a138f7a98
sevenc-nanashi <sevenc7c@sevenc7c.com>
2024-11-18 20:23:51 +0900

    Code: コメントを追加

--- src/helpers/path.ts
+++ src/helpers/path.ts
@@ -2,6 +2,8 @@ import * as windowsPath from "@std/path/windows";
 import * as posixPath from "@std/path/posix";
 import { isWindows } from "./platform";
 
+// OSにあったパス操作関数をエクスポートする。
+// NOTE: @std/pathのWindows判定は差し替えられないので、ここで判定する
 const path = isWindows ? windowsPath : posixPath;
 
 export default path;
