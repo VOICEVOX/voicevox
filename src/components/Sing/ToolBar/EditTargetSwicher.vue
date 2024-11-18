a8d9f50bed3748e607d413b5f77f2d24751f421e
sevenc-nanashi <sevenc7c@sevenc7c.com>
2024-11-18 21:05:44 +0900

    Code: nr fmt

--- src/components/Sing/ToolBar/EditTargetSwicher.vue
+++ src/components/Sing/ToolBar/EditTargetSwicher.vue
@@ -42,7 +42,7 @@
 
 <script setup lang="ts">
 import { SequencerEditTarget } from "@/store/type";
-import { isMac } from "@/helpers/platform"
+import { isMac } from "@/helpers/platform";
 
 defineProps<{
   editTarget: SequencerEditTarget;
