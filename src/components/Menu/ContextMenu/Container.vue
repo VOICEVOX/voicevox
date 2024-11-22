4b5f6fe048d4dd20afc13e9029b663c720c19363
sevenc-nanashi <sevenc7c@sevenc7c.com>
2024-11-22 18:11:24 +0900

    Migrate: ContextMenuをContainer/Presentationに分離する

---
+++ src/components/Menu/ContextMenu/Container.vue
@@ -0,0 +1,32 @@
+<template>
+  <Presentation ref="contextMenu" :header :menudata :uiLocked />
+</template>
+
+<script setup lang="ts">
+import { computed, useTemplateRef } from "vue";
+import type { ComponentExposed } from "vue-component-type-helpers";
+import Presentation, { ContextMenuItemData } from "./Presentation.vue";
+import { useStore } from "@/store";
+
+defineOptions({
+  name: "ContextMenu",
+});
+
+defineProps<{
+  header?: string;
+  menudata: ContextMenuItemData[];
+}>();
+defineExpose({
+  hide: () => {
+    contextMenu.value?.hide();
+  },
+});
+
+const store = useStore();
+const uiLocked = computed(() => store.getters.UI_LOCKED);
+const contextMenu =
+  useTemplateRef<ComponentExposed<typeof Presentation>>("contextMenu");
+</script>
+<script lang="ts">
+export type { ContextMenuItemData } from "./Presentation.vue";
+</script>
