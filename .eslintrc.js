daa0863b57976bbeadb7db112ea5eb478c64d77c
sevenc-nanashi <sevenc7c@sevenc7c.com>
2024-12-14 19:37:03 +0900

    Fix: インポート時のトラック順を修正

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,9 @@ const tsEslintRules = {
   // TODO: いずれは有効化する
   "@typescript-eslint/require-await": "off",
 
+  // ミスが起こりやすいため有効化
+  "@typescript-eslint/require-array-sort-compare": "error",
+
   "@typescript-eslint/no-misused-promises": [
     "error",
     {
