0cf6605beb097a0d55bad8029945807d82ec7096
jdkfx <htazoe@jdkfx.com>
2024-12-22 18:57:13 +0900

    テストの修正

--- tests/e2e/browser/アクセント.spec.ts
+++ tests/e2e/browser/アクセント.spec.ts
@@ -10,10 +10,10 @@ test("アクセント分割したらアクセント区間が増える", async ({
   await page.locator(".audio-cell input").first().fill("こんにちは");
   await page.locator(".audio-cell input").first().press("Enter");
   await page.waitForTimeout(500);
-  expect(await page.locator(".mora-table").count()).toBe(1);
+  expect(await page.locator(".accent-phrase").count()).toBe(1);
   await (await page.locator(".splitter-cell").all())[1].click();
   await page.waitForTimeout(500);
-  expect(await page.locator(".mora-table").count()).toBe(2);
+  expect(await page.locator(".accent-phrase").count()).toBe(2);
 });
 
 test("アクセントの読み部分をクリックすると読みを変更できる", async ({
