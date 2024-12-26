24ceffe9166a475bc8cda56ef260afa729e2ace5
sabonerune <102559104+sabonerune@users.noreply.github.com>
2024-12-26 21:54:12 +0900

    fix: YMM4のカスタムボイス機能が使えない問題の対策

--- src/backend/electron/fileHelper.ts
+++ src/backend/electron/fileHelper.ts
@@ -9,6 +9,21 @@ export function writeFileSafely(
   path: string,
   data: string | NodeJS.ArrayBufferView,
 ) {
+  try {
+    // 上書きでないのなら直接書き込む
+    fs.writeFileSync(path, data, { flag: "wx" });
+    return;
+  } catch (error) {
+    const e = error as NodeJS.ErrnoException;
+    if (e.code !== "EEXIST") {
+      throw e;
+    }
+    const stat = fs.statSync(path, { throwIfNoEntry: false });
+    if (stat != undefined && !stat.isFile()) {
+      // 書き込み先に既にフォルダ等があり上書きできない場合
+      throw e;
+    }
+  }
   const tmpPath = `${path}.tmp`;
   fs.writeFileSync(tmpPath, data);
 
