a956b2885dfae6297b31ae926cbaa6a64dc23160
Takusea <takusea826@gmail.com>
2024-12-29 16:03:19 +0900

    ParameterSliderコンポーネントを追加

---
+++ src/components/Talk/ParameterSlider.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+    <div class="parameter-header">
+      <label :for="`parameter-${sliderKey}`" class="parameter-label">
+        {{ label }}
+      </label>
+      <input
+        :id="`parameter-${sliderKey}`"
+        v-model="inputValue"
+        class="parameter-input"
+        type="text"
+        @change="onUpdate"
+        @blur="onUpdate"
+      />
+    </div>
+    <BaseSlider
+      :modelValue
+      :min
+      :max
+      :step
+      :scrollStep
+      @update:modelValue="$emit('update:modelValue', $event)"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, watchEffect } from "vue";
+import BaseSlider from "@/components/Base/BaseSlider.vue";
+
+const props = defineProps<{
+  sliderKey: string;
+  min: number;
+  max: number;
+  step: number;
+  scrollStep: number;
+  label: string;
+  modelValue: number;
+}>();
+
+const inputValue = ref(props.modelValue.toFixed(2));
+watchEffect(() => {
+  inputValue.value = props.modelValue.toFixed(2);
+});
+
+const emit = defineEmits<{
+  "update:modelValue": [value: number];
+}>();
+
+const onUpdate = (event: Event) => {
+  if (!(event.target instanceof HTMLInputElement)) {
+    return;
+  }
+
+  const value = Number(event.target.value);
+  if (!Number.isNaN(value)) {
+    emit("update:modelValue", Math.min(Math.max(value, props.min), props.max));
+  }
+
+  inputValue.value = props.modelValue.toFixed(2);
+};
+</script>
+
+<style scoped lang="scss">
+@use "@/styles/v2/variables" as vars;
+@use "@/styles/v2/mixin" as mixin;
+
+.parameter-header {
+  display: flex;
+  justify-content: space-between;
+}
+
+.parameter-input {
+  border: none;
+  width: 64px;
+  background-color: transparent;
+  text-align: right;
+  border-radius: vars.$radius-1;
+
+  &:focus-visible {
+    @include mixin.on-focus;
+  }
+}
+</style>
