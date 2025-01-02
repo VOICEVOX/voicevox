1842ae6a758166224d1a349199a8fbaacec189be
Hiroshiba <hihokaruta@gmail.com>
2025-01-03 00:16:16 +0900

    Update src/domain/defaultEngine/envEngineInfo.ts
    Co-authored-by: Nanashi. <sevenc7c@sevenc7c.com>

--- src/domain/defaultEngine/envEngineInfo.ts
+++ src/domain/defaultEngine/envEngineInfo.ts
@@ -38,9 +38,7 @@ export function loadEnvEngineInfos(): EnvEngineInfoType[] {
   // electronのときはプロセスの環境変数を優先する。
   // NOTE: electronテスト環境を切り替えるため。テスト環境が１本化されればimport.meta.envを使う。
   const defaultEngineInfosEnv =
-    (isElectron && process?.env?.VITE_DEFAULT_ENGINE_INFOS
-      ? process.env.VITE_DEFAULT_ENGINE_INFOS
-      : import.meta.env.VITE_DEFAULT_ENGINE_INFOS) ?? "[]";
+    ((isElectron && process?.env?.VITE_DEFAULT_ENGINE_INFOS) || import.meta.env.VITE_DEFAULT_ENGINE_INFOS) ?? "[]";
 
   // FIXME: 「.envを書き換えてください」というログを出したい
   // NOTE: domainディレクトリなのでログを出す方法がなく、Errorオプションのcauseを用いてもelectron-logがcauseのログを出してくれない
