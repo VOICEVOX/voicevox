8d1c1c99bf7465e42592c7a1820c11c76822de7d
Hiroshiba <hihokaruta@gmail.com>
2025-01-17 03:08:55 +0900

    refactor: createLoggerを少しリファクタリング

--- src/domain/frontend/log.ts
+++ src/domain/frontend/log.ts
@@ -1,22 +1,28 @@
 /** ログ出力用の関数を生成する。ブラウザ専用。 */
 // TODO: window.backendをDIできるようにする
 export function createLogger(scope: string) {
-  const createInner =
-    (
-      method: "logInfo" | "logWarn" | "logError",
-      fallbackMethod: "info" | "warn" | "error",
-    ) =>
-    (...args: unknown[]) => {
-      if (window.backend == undefined) {
-        // eslint-disable-next-line no-console
-        console[fallbackMethod](...args);
+  return {
+    info: createLogFunction("info"),
+    warn: createLogFunction("warn"),
+    error: createLogFunction("error"),
+  };
+
+  function createLogFunction(logType: "info" | "warn" | "error") {
+    return (...args: unknown[]) => {
+      if (window.backend != undefined) {
+        const method = (
+          {
+            info: "logInfo",
+            warn: "logWarn",
+            error: "logError",
+          } as const
+        )[logType];
+        window.backend[method](`[${scope}]`, ...args);
         return;
       }
-      window.backend[method](`[${scope}]`, ...args);
+
+      // eslint-disable-next-line no-console
+      console[logType](...args);
     };
-  return {
-    info: createInner("logInfo", "info"),
-    warn: createInner("logWarn", "warn"),
-    error: createInner("logError", "error"),
-  };
+  }
 }
