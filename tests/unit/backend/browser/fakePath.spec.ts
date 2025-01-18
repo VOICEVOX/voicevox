53600c9dd5751a5879f2c24cee4643b0943a3bf8
Hiroshiba <hihokaruta@gmail.com>
2025-01-18 23:23:11 +0900

    test: fakePathのテストケースを更新し、ファイル名の例を改善

--- tests/unit/backend/browser/fakePath.spec.ts
+++ tests/unit/backend/browser/fakePath.spec.ts
@@ -1,9 +1,10 @@
 import { isFakePath, createFakePath } from "@/backend/browser/fakePath";
 
-it.each(["dummy", "日本語", "拡張子付き日本語.wav"])(
-  "FakePathを作れて検証もできる: %s",
-  (input) => {
-    const fakePath = createFakePath(input);
-    expect(isFakePath(fakePath)).toBe(true);
-  },
-);
+it.each([
+  "spaced file name",
+  "filename-with-extension.wav",
+  "日本語ファイル名",
+])("FakePathを作れて検証もできる: %s", (input) => {
+  const fakePath = createFakePath(input);
+  expect(isFakePath(fakePath)).toBe(true);
+});
