8c4f742cd1bece795f77535bf895618fdb58c0f3
Sig <62321214+sigprogramming@users.noreply.github.com>
2025-01-24 02:32:24 +0900

    feat: ソング：音素タイミングの表示エリアを追加 (#2472)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Romot <mail@romot.co>
    Co-authored-by: Hiroshiba <hihokaruta@gmail.com>

---
+++ src/components/Sing/SequencerGridSpacer.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="sequencer-grid-spacer"></div>
+</template>
+
+<script setup lang="ts">
+import { inject } from "vue";
+import { gridInfoInjectionKey } from "./ScoreSequencer.vue";
+
+const injectedValue = inject(gridInfoInjectionKey);
+if (injectedValue == undefined) {
+  throw new Error("injectedValue is undefined.");
+}
+
+const { gridWidth, gridHeight } = injectedValue;
+</script>
+
+<style scoped lang="scss">
+@use "@/styles/variables" as vars;
+@use "@/styles/colors" as colors;
+
+.sequencer-grid-spacer {
+  display: block;
+  width: v-bind("`${gridWidth}px`");
+  height: v-bind("`${gridHeight}px`");
+  pointer-events: none;
+}
+</style>
