f0809d740120a4ab283031591a90b74a18d30c77
Hiroshiba <hihokaruta@gmail.com>
2025-02-08 23:46:29 +0900

    [update snapshots]

--- tests/e2e/browser/音声書き出し.spec.ts
+++ tests/e2e/browser/音声書き出し.spec.ts
@@ -1,12 +1,27 @@
-import { test, expect } from "@playwright/test";
+import { test, expect, Page } from "@playwright/test";
 import { gotoHome, navigateToMain } from "../navigators";
 import { getQuasarMenu } from "../locators";
 import { mockShowExportFileDialog, mockWriteFile } from "./utility";
 
 test.beforeEach(gotoHome);
 
+/** 選択音声を書き出し、その音声ファイルのバイナリをスナップショットテストする */
+async function exportSelectedAudioAndSnapshot(page: Page, name: string) {
+  const { getFileIds } = await mockShowExportFileDialog(page);
+  const { getWritedFileBuffers } = await mockWriteFile(page);
+
+  await page.getByRole("button", { name: "ファイル" }).click();
+  await getQuasarMenu(page, "選択音声を書き出し").click();
+  await expect(page.getByText("音声を書き出しました")).toBeVisible();
+
+  const fileId = (await getFileIds())[0];
+  const buffer = (await getWritedFileBuffers())[fileId];
+  expect(buffer).toMatchSnapshot(`${name}.wav`);
+}
+
 test.describe("音声書き出し", () => {
   test.skip(process.platform !== "win32", "Windows以外のためスキップします");
+
   test.beforeEach(async ({ page }) => {
     await navigateToMain(page);
 
@@ -14,21 +29,37 @@ test.describe("音声書き出し", () => {
     const accentPhrase = page.locator(".accent-phrase");
 
     await audioCell.click();
-    await audioCell.fill("こんにちは、これはテスト音声です");
+    await audioCell.fill("こんにちは、テストです");
     await audioCell.press("Enter");
     await expect(accentPhrase).not.toHaveCount(0);
   });
 
-  test("デフォルト", async ({ page }, { title }) => {
-    const { getFileIds } = await mockShowExportFileDialog(page);
-    const { getWritedFileBuffers } = await mockWriteFile(page);
+  test("各パラメータを変更して音声書き出し", async ({ page }) => {
+    await exportSelectedAudioAndSnapshot(page, "デフォルト");
 
-    await page.getByRole("button", { name: "ファイル" }).click();
-    await getQuasarMenu(page, "選択音声を書き出し").click();
-    await expect(page.getByText("音声を書き出しました")).toBeVisible();
+    const parameters = [
+      ["話速", "1.5"],
+      ["音高", "0.5"],
+      ["抑揚", "1.5"],
+      ["音量", "1.5"],
+      ["間の長さ", "1.5"],
+      ["開始無音", "0.3"],
+      ["終了無音", "0.3"],
+    ] as const;
 
-    const fileId = (await getFileIds())[0];
-    const buffer = (await getWritedFileBuffers())[fileId];
-    expect(buffer).toMatchSnapshot(`${title}.wav`);
+    for (const [name, newValue] of parameters) {
+      await test.step(`${name}変更`, async () => {
+        const input = page.getByLabel(name);
+        const originalValue = await input.inputValue();
+
+        await input.fill(newValue);
+        await input.press("Enter");
+
+        await exportSelectedAudioAndSnapshot(page, `${name}変更`);
+
+        await input.fill(originalValue);
+        await input.press("Enter");
+      });
+    }
   });
 });
