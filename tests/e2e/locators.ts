b02d3f35d9dbe986387cc6ae4cfb8e933c9dcb92
Hiroshiba <hihokaruta@gmail.com>
2025-03-01 22:49:33 +0900

    fix: ensureNotNullishのインポートをlocators.tsに追加

--- tests/e2e/locators.ts
+++ tests/e2e/locators.ts
@@ -1,5 +1,5 @@
-import { ensureNotNullish } from "@/helpers/errorHelper";
 import { Locator, Page } from "@playwright/test";
+import { ensureNotNullish } from "@/helpers/errorHelper";
 
 /**
  * 最新のquasarダイアログのlocatorを取得する
