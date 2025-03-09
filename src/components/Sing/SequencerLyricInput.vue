1e0d2a54f0c6cb4bfae4798e64cfa13c9694909f
KTERAPO\gapck <gameblog60@gmail.com>
2025-03-09 18:08:08 +0900

    Tabキーで自動スクロールしない問題を修正

--- src/components/Sing/SequencerLyricInput.vue
+++ src/components/Sing/SequencerLyricInput.vue
@@ -67,6 +67,7 @@ watch(
     void nextTick(() => {
       lyricInput.value?.focus();
       lyricInput.value?.select();
+      lyricInput.value?.scrollIntoView({block: "nearest", inline: "nearest"});
     });
   },
   { immediate: true },
