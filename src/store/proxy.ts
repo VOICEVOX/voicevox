4ebc4314bca7149750ebc197773191beba7270e9
sevenc-nanashi <sevenc7c@sevenc7c.com>
2025-03-17 23:40:13 +0900

    feat: 実行時にEngineの型チェックを行うように

--- src/store/proxy.ts
+++ src/store/proxy.ts
@@ -1,3 +1,5 @@
+import { cloneWithUnwrapProxy } from "@/helpers/cloneWithUnwrapProxy";
+import openapi from "../../openapi.json";
 import { ProxyStoreState, ProxyStoreTypes, EditorAudioQuery } from "./type";
 import { createPartialStore } from "./vuex";
 import { createEngineUrl } from "@/domain/url";
@@ -7,10 +9,99 @@ import {
   OpenAPIEngineAndMockConnectorFactory,
   OpenAPIEngineConnectorFactory,
 } from "@/infrastructures/EngineConnector";
-import { AudioQuery } from "@/openapi";
+import { AudioQuery, DefaultApiInterface } from "@/openapi";
 import { EngineInfo } from "@/type/preload";
+import Ajv, { ValidateFunction } from "ajv";
 
 export const proxyStoreState: ProxyStoreState = {};
+
+const validateOpenApiResponse = createValidateOpenApiResponse();
+
+function toCamelCase(str: string) {
+  return str.replace(/_./g, (s) => s.charAt(1).toUpperCase());
+}
+
+/** OpenAPIのレスポンスを検証する */
+function createValidateOpenApiResponse() {
+  const ajv = new Ajv().addSchema({
+    $id: "openapi.json",
+    definitions: patchOpenApiJson(openapi).components.schemas,
+  });
+  const validatorCache = new Map<string, ValidateFunction>();
+
+  for (const path of Object.values(openapi.paths)) {
+    for (const method of Object.values(path)) {
+      const schema =
+        method.responses["200"]?.content?.["application/json"]?.schema;
+      if (schema == null) {
+        continue;
+      }
+      validatorCache.set(
+        toCamelCase(method.operationId),
+        ajv.compile(patchOpenApiJson(schema)),
+      );
+    }
+  }
+
+  return <K extends keyof DefaultApiInterface>(
+    key: K,
+    response: ReturnType<DefaultApiInterface[K]>,
+  ): ReturnType<DefaultApiInterface[K]> => {
+    return response.then((res) => {
+      const maybeValidator = validatorCache.get(key);
+      if (maybeValidator == null) {
+        return res;
+      }
+
+      if (!maybeValidator(res)) {
+        throw new Error(
+          `Response validation error in ${key}: ${ajv.errorsText(maybeValidator.errors)}`,
+        );
+      }
+
+      return res;
+    }) as ReturnType<DefaultApiInterface[K]>;
+  };
+}
+
+function patchOpenApiJson<T extends Record<string, unknown>>(schema: T): T {
+  return inner(cloneWithUnwrapProxy(schema)) as T;
+
+  function inner(schema: Record<string, unknown>): Record<string, unknown> {
+    if (schema["$ref"] != null) {
+      const ref = schema["$ref"];
+      if (typeof ref === "string") {
+        schema["$ref"] = ref.replace(
+          "#/components/schemas/",
+          "openapi.json#/definitions/",
+        );
+      }
+    }
+
+    if (
+      schema["type"] === "object" &&
+      typeof schema["properties"] === "object" &&
+      schema["properties"] != null &&
+      Array.isArray(schema["required"])
+    ) {
+      schema["properties"] = Object.fromEntries(
+        Object.entries(schema["properties"]).map(([key, value]) => [
+          toCamelCase(key),
+          inner(value as Record<string, unknown>),
+        ]),
+      );
+
+      schema["required"] = schema["required"].map((key) => toCamelCase(key));
+    }
+
+    for (const key in schema) {
+      if (typeof schema[key] === "object" && schema[key] != null) {
+        inner(schema[key] as Record<string, unknown>);
+      }
+    }
+    return schema;
+  }
+}
 
 const proxyStoreCreator = (_engineFactory: IEngineConnectorFactory) => {
   const proxyStore = createPartialStore<ProxyStoreTypes>({
@@ -35,11 +126,11 @@ const proxyStoreCreator = (_engineFactory: IEngineConnectorFactory) => {
         );
         return Promise.resolve({
           invoke: (v) => (arg) =>
-            // FIXME: anyを使わないようにする
-            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-            // @ts-ignore
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-return
-            instance[v](arg) as any,
+            validateOpenApiResponse(
+              v,
+              // @ts-expect-error 動いているので無視
+              instance[v](arg),
+            ),
         });
       },
     },
