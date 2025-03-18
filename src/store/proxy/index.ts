1b77b4a219215e8944e2f87c0f01df9cfeeb1aa3
sevenc-nanashi <sevenc7c@sevenc7c.com>
2025-03-18 12:01:52 +0900

    chore: openapi.tsに切り出し

---
+++ src/store/proxy/index.ts
@@ -0,0 +1,82 @@
+import { ProxyStoreState, ProxyStoreTypes, EditorAudioQuery } from "../type";
+import { createPartialStore } from "../vuex";
+import { validateOpenApiResponse } from "./openapi";
+import { createEngineUrl } from "@/domain/url";
+import { isElectron, isProduction } from "@/helpers/platform";
+import {
+  IEngineConnectorFactory,
+  OpenAPIEngineAndMockConnectorFactory,
+  OpenAPIEngineConnectorFactory,
+} from "@/infrastructures/EngineConnector";
+import { AudioQuery } from "@/openapi";
+import { EngineInfo } from "@/type/preload";
+
+export const proxyStoreState: ProxyStoreState = {};
+
+const proxyStoreCreator = (_engineFactory: IEngineConnectorFactory) => {
+  const proxyStore = createPartialStore<ProxyStoreTypes>({
+    INSTANTIATE_ENGINE_CONNECTOR: {
+      action({ state }, payload) {
+        const engineId = payload.engineId;
+        const engineInfo: EngineInfo | undefined = state.engineInfos[engineId];
+        if (engineInfo == undefined)
+          return Promise.reject(
+            new Error(`No such engineInfo registered: engineId == ${engineId}`),
+          );
+
+        const altPort: string | undefined = state.altPortInfos[engineId];
+        const port = altPort ?? engineInfo.defaultPort;
+        const instance = _engineFactory.instance(
+          createEngineUrl({
+            protocol: engineInfo.protocol,
+            hostname: engineInfo.hostname,
+            port,
+            pathname: engineInfo.pathname,
+          }),
+        );
+        return Promise.resolve({
+          invoke: (v) => (arg) =>
+            validateOpenApiResponse(
+              v,
+              // @ts-expect-error 動いているので無視
+              instance[v](arg),
+            ),
+        });
+      },
+    },
+  });
+  return proxyStore;
+};
+
+/** AudioQueryをエンジン用に変換する */
+export const convertAudioQueryFromEditorToEngine = (
+  editorAudioQuery: EditorAudioQuery,
+  defaultOutputSamplingRate: number,
+): AudioQuery => {
+  return {
+    ...editorAudioQuery,
+    outputSamplingRate:
+      editorAudioQuery.outputSamplingRate == "engineDefault"
+        ? defaultOutputSamplingRate
+        : editorAudioQuery.outputSamplingRate,
+  };
+};
+
+/** AudioQueryをエディタ用に変換する */
+export const convertAudioQueryFromEngineToEditor = (
+  engineAudioQuery: AudioQuery,
+): EditorAudioQuery => {
+  return {
+    ...engineAudioQuery,
+    pauseLengthScale: engineAudioQuery.pauseLengthScale ?? 1,
+  };
+};
+
+// 製品PC版は通常エンジンのみを、それ以外はモックエンジンも使えるようする
+const getConnectorFactory = () => {
+  if (isElectron && isProduction) {
+    return OpenAPIEngineConnectorFactory;
+  }
+  return OpenAPIEngineAndMockConnectorFactory;
+};
+export const proxyStore = proxyStoreCreator(getConnectorFactory());
--- src/store/proxy/openapi.ts
+++ src/store/proxy/openapi.ts
@@ -1,21 +1,9 @@
 import Ajv, { JSONSchemaType, ValidateFunction } from "ajv";
-import openapi from "../../openapi.json";
-import { ProxyStoreState, ProxyStoreTypes, EditorAudioQuery } from "./type";
-import { createPartialStore } from "./vuex";
+import openapi from "../../../openapi.json";
 import { cloneWithUnwrapProxy } from "@/helpers/cloneWithUnwrapProxy";
-import { createEngineUrl } from "@/domain/url";
-import { isElectron, isProduction } from "@/helpers/platform";
-import {
-  IEngineConnectorFactory,
-  OpenAPIEngineAndMockConnectorFactory,
-  OpenAPIEngineConnectorFactory,
-} from "@/infrastructures/EngineConnector";
-import { AudioQuery, DefaultApiInterface } from "@/openapi";
-import { EngineInfo } from "@/type/preload";
+import { DefaultApiInterface } from "@/openapi";
 
-export const proxyStoreState: ProxyStoreState = {};
-
-const validateOpenApiResponse = createValidateOpenApiResponse();
+export const validateOpenApiResponse = createValidateOpenApiResponse();
 
 function toCamelCase(str: string) {
   return str.replace(/_./g, (s) => s.charAt(1).toUpperCase());
@@ -118,71 +106,3 @@ function patchOpenApiJson<T extends Record<string, unknown>>(schema: T): T {
     return schema;
   }
 }
-
-const proxyStoreCreator = (_engineFactory: IEngineConnectorFactory) => {
-  const proxyStore = createPartialStore<ProxyStoreTypes>({
-    INSTANTIATE_ENGINE_CONNECTOR: {
-      action({ state }, payload) {
-        const engineId = payload.engineId;
-        const engineInfo: EngineInfo | undefined = state.engineInfos[engineId];
-        if (engineInfo == undefined)
-          return Promise.reject(
-            new Error(`No such engineInfo registered: engineId == ${engineId}`),
-          );
-
-        const altPort: string | undefined = state.altPortInfos[engineId];
-        const port = altPort ?? engineInfo.defaultPort;
-        const instance = _engineFactory.instance(
-          createEngineUrl({
-            protocol: engineInfo.protocol,
-            hostname: engineInfo.hostname,
-            port,
-            pathname: engineInfo.pathname,
-          }),
-        );
-        return Promise.resolve({
-          invoke: (v) => (arg) =>
-            validateOpenApiResponse(
-              v,
-              // @ts-expect-error 動いているので無視
-              instance[v](arg),
-            ),
-        });
-      },
-    },
-  });
-  return proxyStore;
-};
-
-/** AudioQueryをエンジン用に変換する */
-export const convertAudioQueryFromEditorToEngine = (
-  editorAudioQuery: EditorAudioQuery,
-  defaultOutputSamplingRate: number,
-): AudioQuery => {
-  return {
-    ...editorAudioQuery,
-    outputSamplingRate:
-      editorAudioQuery.outputSamplingRate == "engineDefault"
-        ? defaultOutputSamplingRate
-        : editorAudioQuery.outputSamplingRate,
-  };
-};
-
-/** AudioQueryをエディタ用に変換する */
-export const convertAudioQueryFromEngineToEditor = (
-  engineAudioQuery: AudioQuery,
-): EditorAudioQuery => {
-  return {
-    ...engineAudioQuery,
-    pauseLengthScale: engineAudioQuery.pauseLengthScale ?? 1,
-  };
-};
-
-// 製品PC版は通常エンジンのみを、それ以外はモックエンジンも使えるようする
-const getConnectorFactory = () => {
-  if (isElectron && isProduction) {
-    return OpenAPIEngineConnectorFactory;
-  }
-  return OpenAPIEngineAndMockConnectorFactory;
-};
-export const proxyStore = proxyStoreCreator(getConnectorFactory());
