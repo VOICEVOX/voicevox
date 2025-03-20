a11c81adb08396963e94ba24b8ee277833402e55
madosuki <yurikasamaaikatsu@gmail.com>
2025-03-20 23:46:19 +0900

    change

--- src/components/Dialog/HelpDialog/HelpDialog.vue
+++ src/components/Dialog/HelpDialog/HelpDialog.vue
@@ -32,7 +32,7 @@
               icon="close"
               color="display"
               aria-label="ヘルプを閉じる"
-              @click="closeDialog"
+              @click="onDialogOK"
             />
           </QToolbar>
         </QHeader>
@@ -103,10 +103,6 @@ type PageData = PageItem | PageSeparator;
 
 const { dialogRef, onDialogOK } = useDialogPluginComponent();
 defineEmits([...useDialogPluginComponent.emits]);
-
-const closeDialog = () => {
-  onDialogOK();
-};
 
 // エディタのアップデート確認
 const store = useStore();
