35bad54a482a15392736a04ede62800d7f0d0679
Romot <mail@romot.co>
2025-03-29 18:51:34 +0900

    [update snapshots] スナップショットテストの更新

--- src/mock/engineMock/synthesisMock.ts
+++ src/mock/engineMock/synthesisMock.ts
@@ -9,7 +9,7 @@ import { generateWavFileData } from "@/helpers/fileDataGenerator";
 import { applyGaussianFilter } from "@/sing/utility";
 
 /** 0~1を返す疑似乱数生成器 */
-function Random(seed: number = 0) {
+function Random(seed: number = 123456789) {
   // 線形合同法
   const a = 1664525;
   const c = 1013904223;
@@ -17,7 +17,7 @@ function Random(seed: number = 0) {
 
   return () => {
     seed = (a * seed + c) % m;
-    return seed / m;
+    return Math.round(seed / m * 1000) / 1000;
   };
 }
 
@@ -37,8 +37,7 @@ function generateWave(
   const samplesPerOriginal = sampleRate / frameRate;
   const wave = new Float32Array(sampleRate * duration);
 
-  const seed =
-    Math.round(f0.concat(volume).reduce((acc, v) => acc + v, 0)) % 2 ** 31; // そこそこ被らないシード値
+  const seed = 123456789;
   const random = Random(seed);
   let phase = 0;
   for (let frameIndex = 0; frameIndex < f0.length; frameIndex++) {
@@ -48,21 +47,22 @@ function generateWave(
 
     for (let i = 0; i < samplesPerOriginal; i++) {
       const sampleIndex = frameIndex * samplesPerOriginal + i;
+      let value = 0;
       switch (type) {
         case "sine":
-          wave[sampleIndex] = Math.sin(phase);
+          value = Math.sin(phase);
           break;
         case "square":
-          wave[sampleIndex] = (phase / Math.PI) % 2 < 1 ? 1 : -1;
+          value = (phase / Math.PI) % 2 < 1 ? 1 : -1;
           break;
         case "noise":
-          wave[sampleIndex] = random() * 2 - 1;
+          value = random() * 2 - 1;
           break;
         case "silence":
-          wave[sampleIndex] = 0;
+          value = 0;
           break;
       }
-      wave[sampleIndex] *= vol;
+      wave[sampleIndex] = Math.round(value * vol * 1000) / 1000;
 
       phase += omega;
       if (phase > 2 * Math.PI) {
