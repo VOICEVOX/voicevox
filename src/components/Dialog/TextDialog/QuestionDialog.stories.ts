d45079521a607ac4190ecc19900ce3a4d2bb7966
Takusea <takusea826@gmail.com>
2025-05-22 00:12:01 +0900

    テストの要素選択を修正

--- src/components/Dialog/TextDialog/QuestionDialog.stories.ts
+++ src/components/Dialog/TextDialog/QuestionDialog.stories.ts
@@ -68,7 +68,7 @@ export const ClickBackdropWithoutCancel: Story = {
   name: "persistent: trueで背景を押してもキャンセル扱いにならない",
   args: { ...Opened.args },
   play: async ({ args }) => {
-    const backdrop = document.body.querySelector(".q-dialog__backdrop");
+    const backdrop = document.body.querySelector(".DialogOverlay");
     if (!backdrop) throw new UnreachableError();
     await userEvent.click(backdrop);
 
@@ -81,7 +81,7 @@ export const ClickBackdropWithCancel: Story = {
   name: "persistent: falseで背景を押すとキャンセル扱いになる",
   args: { ...Opened.args, buttons: ["A", "キャンセル"], persistent: false },
   play: async ({ args }) => {
-    const backdrop = document.body.querySelector(".q-dialog__backdrop");
+    const backdrop = document.body.querySelector(".DialogOverlay");
     if (!backdrop) throw new UnreachableError();
     await userEvent.click(backdrop);
 
