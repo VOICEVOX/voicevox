27268365b9811a8f0d02b6df3230a4bf344e18c7
Sig <62321214+sigprogramming@users.noreply.github.com>
2025-05-24 16:34:58 +0900

    TODOコメントを追加

--- src/store/singing.ts
+++ src/store/singing.ts
@@ -136,6 +136,7 @@ import {
   synthesizeSingingVoice,
   EngineSongApi,
   // 一時的なインポート：SongTrackRenderer の統合が完了するまで、oldSongTrackRendering モジュールを使用する
+  // TODO: SongTrackRenderer の統合が完了したら、このインポートを削除する
 } from "@/sing/oldSongTrackRendering";
 
 const logger = createLogger("store/singing");
