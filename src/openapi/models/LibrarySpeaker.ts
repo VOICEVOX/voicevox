c9ad0d5fba9b684b1013ee89dc8519410b821ad6
sevenc-nanashi <sevenc7c@sevenc7c.com>
2025-06-11 22:00:34 +0900

    fix: masterで実行する

--- src/openapi/models/LibrarySpeaker.ts
+++ src/openapi/models/LibrarySpeaker.ts
@@ -1,8 +1,8 @@
 /* tslint:disable */
 /* eslint-disable */
 /**
- * DUMMY Engine
- * DUMMY の音声合成エンジンです。
+ * VOICEVOX ENGINE OSS
+ * VOICEVOX OSS の音声合成エンジンです。
  *
  * The version of the OpenAPI document: latest
  * 
@@ -27,7 +27,7 @@ import {
 } from './SpeakerInfo';
 
 /**
- * 音声ライブラリに含まれるキャラクターの情報
+ * 音声ライブラリに含まれるキャラクターの情報。
  * @export
  * @interface LibrarySpeaker
  */
