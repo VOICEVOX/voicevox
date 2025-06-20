35a375e222e5b08c7b0fb82b32c9134939d5935d
sevenc-nanashi <sevenc7c@sevenc7c.com>
2025-06-20 11:59:44 +0900

    test: テストを追加

---
+++ tests/unit/lib/reorder.spec.ts
@@ -0,0 +1,32 @@
+import { expect, test } from "vitest";
+import { reorder } from "@/helpers/reorderHelper";
+
+test("1要素の順序を前方向に変更できる", () => {
+  expect(reorder(["a", "b", "c", "d"], new Set(["b"]), 1, 2)).toEqual([
+    "a",
+    "c",
+    "b",
+    "d",
+  ]);
+});
+
+test("1要素の順序を後ろ方向に変更できる", () => {
+  expect(reorder(["a", "b", "c", "d"], new Set(["b"]), 1, 0)).toEqual([
+    "b",
+    "a",
+    "c",
+    "d",
+  ]);
+});
+
+test("複数要素の順序を前方向に変更できる", () => {
+  expect(
+    reorder(["a", "b", "c", "d", "e", "f"], new Set(["b", "d"]), 1, 4),
+  ).toEqual(["a", "c", "e", "b", "d", "f"]);
+});
+
+test("複数要素の順序を後ろ方向に変更できる", () => {
+  expect(
+    reorder(["a", "b", "c", "d", "e", "f"], new Set(["b", "d"]), 1, 0),
+  ).toEqual(["b", "d", "a", "c", "e", "f"]);
+});
