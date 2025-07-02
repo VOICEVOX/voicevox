408aeba0987aa89b2b4d8a16e0e5d73101f176d5
Romot <mail@romot.co>
2025-07-02 13:46:28 +0900

    透明度を調整 [update snapshots]

--- src/components/Sing/SequencerRuler/LoopLane/Presentation.vue
+++ src/components/Sing/SequencerRuler/LoopLane/Presentation.vue
@@ -205,7 +205,7 @@ const handleLoopRangeMouseLeave = () => {
   border: 1px solid transparent;
   box-shadow: inset 0 0 0 0 transparent;
 
-  // ホバー時は半透明のオーバーレイと境界線でループエリアを示す
+  // ホバー時は半透明のオーバーレイでループエリアを示す
   &:hover {
     background-color: color-mix(
       in oklch,
