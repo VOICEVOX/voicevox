bba05ee2417bb090fb289a1b9e2abadf66699d1b
sabonerune <102559104+sabonerune@users.noreply.github.com>
2025-10-16 07:52:17 +0900

    feat: GPUサポートチェックの実装をsysteminformationからElectronのAPIに変更

--- src/backend/electron/device.ts
+++ src/backend/electron/device.ts
@@ -1,20 +1,27 @@
-import si from "systeminformation";
+import { app } from "electron";
+
+// hasSupportedGpuで使用する最小限のGPUInfo
+type MinimumGPUInfo = {
+  gpuDevice: { vendorId: number }[];
+};
 
 /**
  * GPUモードがサポートされているかどうか。
  * windowsの場合は全GPUに対応。
  * linuxの場合はNVIDIAのみ対応。
  */
-export function hasSupportedGpu(platform: string): Promise<boolean> {
-  return si
-    .graphics()
-    .then((data) =>
-      data.controllers.some(
-        (datum) =>
-          platform === "win32" ||
-          (platform === "linux" &&
-            datum.vendor.toUpperCase().includes("NVIDIA")),
-      ),
-    )
+export async function hasSupportedGpu(platform: string): Promise<boolean> {
+  return app
+    .getGPUInfo("basic")
+    .then((GPUInfo) => {
+      const info = GPUInfo as MinimumGPUInfo;
+      if (platform === "win32") {
+        // VendorID 0x1414はMicrosoft Basic Render Driverなので除外する
+        return info.gpuDevice.some((device) => device.vendorId !== 0x1414);
+      } else if (platform === "linux") {
+        return info.gpuDevice.some((device) => device.vendorId === 0x10de);
+      }
+      return false;
+    })
     .catch(() => false);
 }
