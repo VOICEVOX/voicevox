64621b90d178319f669dae96d033e052b6e688d4
akikukeo <165561768+akikukeo@users.noreply.github.com>
2025-12-27 18:10:41 +0900

    fix: 秒数が0にならない、秒数が表示されない

--- src/store/audioGenerate.ts
+++ src/store/audioGenerate.ts
@@ -177,6 +177,8 @@ export function handlePossiblyNotMorphableError(e: unknown) {
  * AudioQueryから音声の長さを計算する(秒)
  */
 export function calculateAudioLength(audioQuery: EditorAudioQuery) {
+  if (audioQuery.accentPhrases.length === 0) return 0;
+
   let length = 0;
   length += audioQuery.prePhonemeLength;
   audioQuery.accentPhrases.forEach((accentPhrase) => {
