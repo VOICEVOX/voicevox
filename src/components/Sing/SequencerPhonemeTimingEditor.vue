7164399d26677ae0149448840d01f61e42c0e83c
Sig <62321214+sigprogramming@users.noreply.github.com>
2026-01-08 22:47:52 +0900

    パラメーターパネルで編集対象を切り替えられるようにした

---
+++ src/components/Sing/SequencerPhonemeTimingEditor.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="phoneme-timing-editor">
+    <div class="axis-area"></div>
+    <div class="parameter-area">音素タイミング編集</div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.phoneme-timing-editor {
+  position: relative;
+  width: 100%;
+  height: 100%;
+
+  overflow: hidden;
+  display: grid;
+  grid-template-columns: 48px 1fr;
+}
+
+.axis-area {
+  grid-column: 1;
+  grid-row: 1;
+  border-right: solid 1px var(--scheme-color-sing-piano-keys-right-border);
+}
+
+.parameter-area {
+  grid-column: 2;
+  grid-row: 1;
+
+  overflow: hidden;
+}
+</style>
